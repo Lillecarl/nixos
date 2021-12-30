f3cd01754542e6fb56af475dcf50535c7148fb5a
LilleCarl <git@lillecarl.com>
2021-12-30 19:13:58 +0100

    Set XDG freedesktop standard environment variables
    With this we should be able to unclutter the homedir quite a bit. And
    it's always nice to be compliant.

--- configuration.nix
+++ configuration.nix
@@ -174,6 +174,21 @@ rec
     #};
   };
 
+  # XDG Base Directory Specification
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "\${HOME}/.cache";
+    XDG_CONFIG_HOME = "\${HOME}/.config";
+    XDG_BIN_HOME    = "\${HOME}/.local/bin";
+    XDG_DATA_HOME   = "\${HOME}/.local/share";
+    XDG_STATE_HOME  = "\${HOME}/.local/state";
+
+    PATH = [ 
+      "\${XDG_BIN_HOME}"
+    ];
+
+    POWERSHELL_TELEMETRY_OPTOUT = "yes";
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
