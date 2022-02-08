bbbb5f4554b1ad773a72a25fbe3253ab35f89409
Carl Hjerpe <git@hjerpe.xyz>
2022-02-08 13:49:01 +0100

    Use Slack from master channel until it's fixed

--- common/default.nix
+++ common/default.nix
@@ -95,7 +95,7 @@ rec
     # Chat apps
     element-desktop # Element Slack app
     teams # Microsoft Teams collaboration suite (Electron)
-    slack # Team collaboration chat (Electron)
+    master.slack # Team collaboration chat (Electron)
     discord # Gaming chat application
     zoom # Meetings application
     signal-desktop # Secure messenger
