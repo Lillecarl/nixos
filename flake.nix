c592726167d828b889ff4dc6cdb06143c095bb09
Carl Hjerpe <git@hjerpe.xyz>
2022-02-08 13:48:45 +0100

    Add master channel overlay
    This language is fucked, I have no idea what I'm doing.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,42 @@
 {
   inputs = {
-    unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs-master.url = github:NixOS/nixpkgs/master;
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
   };
 
-  outputs = { self, unstable, nixos-hardware, ... } @inputs:
+  outputs = { self, nixpkgs-unstable, nixpkgs-master, nixos-hardware, ... } @inputs:
     let
-      nixpkgsConfig = {
-        config = { allowUnfree = true; };
+      system = "x86_64-linux"; # I guess this works as long as all my systems are x86_64-linux
+      overlay-master = final: prev: {
+        master = import nixpkgs-master {
+          inherit system;
+          config.allowUnfree = true;
+        };
       };
+
+      overlayMagic = (
+        { config, pkgs, ... }:
+        { nixpkgs.overlays = [ overlay-master ]; }
+      );
     in
     {
       nixosConfigurations = rec {
-        shitbox = unstable.lib.nixosSystem {
+        shitbox = nixpkgs-unstable.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             ./shitbox
+            overlayMagic
             nixos-hardware.nixosModules.common-cpu-intel
             nixos-hardware.nixosModules.common-pc-ssd
             nixos-hardware.nixosModules.common-pc
           ];
         };
-        lemur = unstable.lib.nixosSystem {
+        lemur = nixpkgs-unstable.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            ./lemur
+            ./lemur 
+            overlayMagic
             nixos-hardware.nixosModules.common-cpu-intel
             nixos-hardware.nixosModules.common-pc-laptop
             nixos-hardware.nixosModules.common-pc-ssd
