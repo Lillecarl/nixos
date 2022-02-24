95c520faf6608bbe75cc2c49abc9a42ca03f1587
Carl Hjerpe <git@hjerpe.xyz>
2022-02-24 17:11:14 +0100

    Add some mac packages that are core to my setup

--- C02YF1KLJHD2/default.nix
+++ C02YF1KLJHD2/default.nix
@@ -14,11 +14,20 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [ 
+  environment.systemPackages = with pkgs; [
+    vim
     starship
+    zellij
+    chezmoi
+    gitui
+    ## Packages below are broken in Nix and installed with brew
+    #brave
+    #wezterm
+    #iterm2
   ];
 
   programs.zsh.enable = true;
+
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 }
