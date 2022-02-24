00dbcc6697c4ddb57734d64e2ce5a109e04e2292
Carl Hjerpe <git@hjerpe.xyz>
2022-02-24 11:19:15 +0100

    Add nix-darwin, NixOS VM on Mac isn't good enough

---
+++ C02YF1KLJHD2/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+{
+  nixpkgs = {
+    # Allow proprietary software to be installed
+    config.allowUnfree = true;
+  };
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [ 
+    vim
+  ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
     nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
     nixpkgs-master.url = github:NixOS/nixpkgs/master;
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
+
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
   };
 
-  outputs = { self, nixpkgs-unstable, nixpkgs-master, nixos-hardware, ... } @inputs:
+  outputs = { self, nixpkgs-unstable, nixpkgs-master, nixos-hardware, nixpkgs-darwin, darwin, ... } @inputs:
     let
       system = "x86_64-linux"; # I guess this works as long as all my systems are x86_64-linux
 
@@ -60,6 +64,16 @@
             nixos-hardware.nixosModules.common-pc-ssd
             nixos-hardware.nixosModules.common-pc
           ];
+        };
+      };
+      darwinConfigurations."C02YF1KLJHD2" = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        modules = [
+          ./C02YF1KLJHD2
+        ];
+        inputs = {
+          inherit darwin;
+          nixpkgs = nixpkgs-darwin;
         };
       };
     };
