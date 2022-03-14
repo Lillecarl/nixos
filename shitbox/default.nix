4722d82e33c2db01f39d99b98c26349d7c332dfa
Carl Hjerpe <git@hjerpe.xyz>
2022-03-15 00:40:37 +0100

    Add KWinKill service to shitbox

--- shitbox/default.nix
+++ shitbox/default.nix
@@ -11,6 +11,23 @@
     ./boot.nix
   ];
 
+  # Kills KDE if it hangs on shutdown
+  systemd.services.KWinKill = {
+    wantedBy = [ "multi-user.target" ];
+
+    script = "${pkgs.bashInteractive}/bin/sh -c \"true\"";
+    preStop = "${pkgs.procps}/bin/pkill -KILL kwin_x11";
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    stopIfChanged = false;
+    restartIfChanged = false;
+    reloadIfChanged = false;
+  };
+
 
   nixpkgs = {
     # Allow proprietary software to be installed
