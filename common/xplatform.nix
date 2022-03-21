467e351f214de1ac76aaaf9d61f91da907440218
Carl Hjerpe <git@hjerpe.xyz>
2022-03-21 11:42:40 +0100

    Move Mac CLI tools to xplaform file
    Merge Linux CLI tools with this one in the future

---
+++ common/xplatform.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, ... }:
+{
+  # These tools are common across MacOS and Linux
+  environment.systemPackages = with pkgs; [
+    vim # Editor
+    starship # Prompt
+    mcfly # Improved history search
+    zellij # Terminal multiplexer
+    chezmoi # Dotfile manager
+    gitui # Git TUI
+    direnv # Env vars from directories
+    kubectl # Kubernetes admin tool
+    kubectx # Kubernetes session helper
+    kubernetes-helm # Kubernetes package manager
+    kustomize # Kubernetes YAML configurations
+    octant # Kubernetes GUI
+    tldr # tldr manpages
+    ripgrep # Nice grep
+    powershell # scripting shell from MS
+    cli53 # Route53 CLI
+    unixtools.watch # watch command
+    git-open # Open Git repos from cli in web
+    git # VCS
+    pijul # Cooler VCS
+    tfswitch # Switch terraform versions
+    asciinema # Record terminal sessions
+    nodejs # Stupid language runtime
+    nodePackages.npm # Stupid language package manager
+    nodePackages.yarn # Stupid language less stupid package manager
+    gource # VCS repo history visualizer
+    jq # JSON CLI Tool
+    yq # YAML CLI Tool
+    thefuck # Command fixing tool
+  ];
+   
+}
