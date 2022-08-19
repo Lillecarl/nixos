dc6582bf813c22481e41892b925b7978c41ef97a
Carl Hjerpe <git@hjerpe.xyz>
2022-08-19 15:21:53 +0200

    Configure swap

--- nub/hardware-configuration.nix
+++ nub/hardware-configuration.nix
@@ -25,7 +25,11 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
