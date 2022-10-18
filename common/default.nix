701d16c05d6e43ccd385b8fb68f432a54be1930b
Carl Hjerpe <git@hjerpe.xyz>
2022-10-18 22:01:44 +0200

    Add commands that run qdbus for window mgmt

--- common/default.nix
+++ common/default.nix
@@ -249,10 +249,66 @@ rec
     enable = true;
 
     bindings = [
-      { keys = [ 119 ]; events = [ "key" ];
-	command = "${pkgs.systemd}/bin/machinectl shell --setenv=WAYLAND_DISPLAY=wayland-0 --setenv=XDG_SESSION_TYPE=wayland lillecarl@ ${pkgs.spectacle}/bin/spectacle"; }
-      { keys = [ 105 ]; events = [ "key" ];
-	command = "${pkgs.coreutils-full}/bin/date > /home/lillecarl/actkbd"; }
+      {
+        keys = [ 56 58 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.krunner /App org.kde.krunner.App.query \"window: \"";
+      }
+      {
+        keys = [ 57 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.krunner /App org.kde.krunner.App.query \"\"";
+      }
+      {
+        keys = [ 105 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Left\"";
+      }
+      {
+        keys = [ 106 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Right\"";
+      }
+      {
+        keys = [ 103 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Top\"";
+      }
+      {
+        keys = [ 108 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Bottom\"";
+      }
+      {
+        keys = [ 105 103 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Top Left\"";
+      }
+      {
+        keys = [ 106 103 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Top Right\"";
+      }
+      {
+        keys = [ 105 108 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Bottom Left\"";
+      }
+      {
+        keys = [ 106 108 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Quick Tile Bottom Right\"";
+      }
+      {
+        keys = [ 33 125 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell lillecarl@ /run/current-system/sw/bin/qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.Component.invokeShortcut \"Window Maximize\"";
+      }
+      {
+        keys = [ 119 ];
+	events = [ "key" ];
+	command = "${pkgs.systemd}/bin/machinectl shell --setenv=WAYLAND_DISPLAY=wayland-0 --setenv=XDG_SESSION_TYPE=wayland lillecarl@ ${pkgs.spectacle}/bin/spectacle";
+      }
     ];
   };
 
