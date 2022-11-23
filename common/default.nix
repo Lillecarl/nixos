19e89d2adf6dc1369870d4287ca2e726d8c45dfb
Carl Hjerpe <git@hjerpe.xyz>
2022-11-23 19:21:11 +0100

    fmt

--- common/default.nix
+++ common/default.nix
@@ -30,25 +30,25 @@ let
 
   python3Packages = pkgs.python310.pkgs;
 
-  xonsh-direnv = pkgs.callPackage ../pkgs/xonsh-direnv {};
+  xonsh-direnv = pkgs.callPackage ../pkgs/xonsh-direnv { };
 
-    xontrib-argcomplete = python3Packages.buildPythonPackage rec {
-  pname = "xontrib-argcomplete";
-  version = "0.3.2";
-  src = python3Packages.fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-jn1NHh/PTTgSX0seOvOZTpRv4PxAQ4PbDiXOSb4/jrU=";
-  };
+  xontrib-argcomplete = python3Packages.buildPythonPackage rec {
+    pname = "xontrib-argcomplete";
+    version = "0.3.2";
+    src = python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-jn1NHh/PTTgSX0seOvOZTpRv4PxAQ4PbDiXOSb4/jrU=";
+    };
 
-  propagatedBuildInputs = with pkgs.python3Packages; [
-    argcomplete
-  ];
+    propagatedBuildInputs = with pkgs.python3Packages; [
+      argcomplete
+    ];
 
-  meta = {
-    description = "Argcomplete support for python and xonsh scripts in xonsh shell.";
-    homepage = "https://github.com/anki-code/${pname}";
-    license = lib.licenses.mit;
-  };
+    meta = {
+      description = "Argcomplete support for python and xonsh scripts in xonsh shell.";
+      homepage = "https://github.com/anki-code/${pname}";
+      license = lib.licenses.mit;
+    };
   };
 
   tokenize-output = python3Packages.buildPythonPackage rec {
