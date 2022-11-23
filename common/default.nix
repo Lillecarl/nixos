5ed43404527414ca58c90c9490faaf8e406e14e9
Carl Hjerpe <git@hjerpe.xyz>
2022-11-23 19:45:33 +0100

    Package xontrib-output-search "properly"

--- common/default.nix
+++ common/default.nix
@@ -35,25 +35,7 @@ let
   xontrib-sh = pkgs.callPackage ../pkgs/xontrib-sh { };
   xontrib-argcomplete = pkgs.callPackage ../pkgs/xontrib-argcomplete { };
   tokenize-output = pkgs.callPackage ../pkgs/tokenize-output { };
-
-  xontrib-output-search = python3Packages.buildPythonPackage rec {
-    pname = "xontrib-output-search";
-    version = "0.6.2";
-    src = python3Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-Zh4DXs5qajZ3bR2YVJ+uLE2u1TVJcmdzH3x9nX6jJDI=";
-    };
-
-    propagatedBuildInputs = with pkgs.python3Packages; [
-      tokenize-output
-    ];
-
-    meta = {
-      description = "Get identifiers, paths, URLs and words from the previous command output and use them for the next command in xonsh shell.";
-      homepage = "https://github.com/anki-code/${pname}";
-      license = lib.licenses.mit;
-    };
-  };
+  xontrib-output-search = pkgs.callPackage ../pkgs/xontrib-output-search { inherit tokenize-output; };
 
   repassh = python3Packages.buildPythonPackage rec {
     pname = "repassh";
---
+++ pkgs/xontrib-output-search/default.nix
@@ -0,0 +1,23 @@
+{ lib
+, python3Packages
+, tokenize-output
+}:
+
+python3Packages.buildPythonPackage rec {
+  pname = "xontrib-output-search";
+  version = "0.6.2";
+  src = python3Packages.fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-Zh4DXs5qajZ3bR2YVJ+uLE2u1TVJcmdzH3x9nX6jJDI=";
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    tokenize-output
+  ];
+
+  meta = {
+    description = "Get identifiers, paths, URLs and words from the previous command output and use them for the next command in xonsh shell.";
+    homepage = "https://github.com/anki-code/${pname}";
+    license = lib.licenses.mit;
+  };
+}
