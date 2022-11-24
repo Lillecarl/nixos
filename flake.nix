e082a228e4e0d41fa6f8736f2fddda0a2cea53a5
Carl Hjerpe <git@hjerpe.xyz>
2022-11-24 22:53:53 +0100

    Formatting

--- flake.nix
+++ flake.nix
@@ -12,65 +12,65 @@
   };
 
   outputs = { self, nixos-unstable, nixos-hardware, home-manager, ... } @inputs:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixos-unstable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
-  in
-  {
+    let
+      system = "x86_64-linux";
+      pkgs = import nixos-unstable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
+    in
+    {
 
-    nixosConfigurations = rec {
-      shitbox = nixos-unstable.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./shitbox
-          ./common
-          ./common/flatpak.nix
-          ./common/keybindings.nix
-          ./common/killservice.nix
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-pc
-        ];
-        specialArgs = inputs;
+      nixosConfigurations = rec {
+        shitbox = nixos-unstable.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./shitbox
+            ./common
+            ./common/flatpak.nix
+            ./common/keybindings.nix
+            ./common/killservice.nix
+            nixos-hardware.nixosModules.common-cpu-amd
+            nixos-hardware.nixosModules.common-pc-ssd
+            nixos-hardware.nixosModules.common-pc
+          ];
+          specialArgs = inputs;
+        };
+        nub = nixos-unstable.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./nub
+            ./common
+            ./common/flatpak.nix
+            ./common/keybindings.nix
+            ./common/killservice.nix
+            ./common/xplatform.nix
+            nixos-hardware.nixosModules.lenovo-thinkpad-t14s
+            nixos-hardware.nixosModules.common-cpu-amd
+            nixos-hardware.nixosModules.common-gpu-amd
+            nixos-hardware.nixosModules.common-pc-laptop
+            nixos-hardware.nixosModules.common-pc-laptop-acpi_call
+            nixos-hardware.nixosModules.common-pc-ssd
+            nixos-hardware.nixosModules.common-pc
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "old"; # Move non-hm files if they're in the way
+              home-manager.users.lillecarl = import ./lillecarl;
+
+              # Optionally, use home-manager.extraSpecialArgs to pass
+              # arguments to home.nix
+            }
+          ];
+          specialArgs = inputs;
+        };
       };
-      nub = nixos-unstable.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./nub
-          ./common
-          ./common/flatpak.nix
-          ./common/keybindings.nix
-          ./common/killservice.nix
-          ./common/xplatform.nix
-          nixos-hardware.nixosModules.lenovo-thinkpad-t14s
-          nixos-hardware.nixosModules.common-cpu-amd
-          nixos-hardware.nixosModules.common-gpu-amd
-          nixos-hardware.nixosModules.common-pc-laptop
-          nixos-hardware.nixosModules.common-pc-laptop-acpi_call
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-pc
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "old"; # Move non-hm files if they're in the way
-            home-manager.users.lillecarl = import ./lillecarl;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-        specialArgs = inputs;
+      homeConfigurations = {
+        lillecarl = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./lillecarl
+          ];
+        };
       };
     };
-    homeConfigurations = {
-      lillecarl = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./lillecarl
-        ];
-      };
-    };
-  };
 }
