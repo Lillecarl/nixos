d696ea12a2ea69e5670f37e8dc7ec589c6c6be7b
Carl Hjerpe <git@hjerpe.xyz>
2022-11-24 09:59:48 +0100

    Remove old cruft

--- nub/default.nix
+++ nub/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 let
   kubeEnable = false;
@@ -21,7 +17,6 @@ rec
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelParams = [ "resume=/dev/vg1/swap" "mem_sleep_default=deep" ];
-  #boot.kernelPackages = with pkgs.linuxKernel.packages; linux_xanmod_latest;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   services.tp-auto-kbbl = {
