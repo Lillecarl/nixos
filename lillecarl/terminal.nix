cfd6ab85e8d4d5915821233dbe8fbba81f83ba11
Carl Hjerpe <git@hjerpe.xyz>
2022-11-24 23:22:16 +0100

    formatting

--- lillecarl/terminal.nix
+++ lillecarl/terminal.nix
@@ -1,7 +1,7 @@
-
 { config, pkgs, ... }:
 {
-  home.file = { # Replace this list once https://github.com/nix-community/home-manager/pull/3235 is ready and merged
+  home.file = {
+    # Replace this list once https://github.com/nix-community/home-manager/pull/3235 is ready and merged
     ".config/xonsh/rc.xsh".source = ./dotfiles/.config/xonsh/rc.xsh;
     ".config/xonsh/rc.d/aliases.xsh".source = ./dotfiles/.config/xonsh/rc.d/aliases.xsh;
     ".config/xonsh/rc.d/keybindings.xsh".source = ./dotfiles/.config/xonsh/rc.d/keybindings.xsh;
