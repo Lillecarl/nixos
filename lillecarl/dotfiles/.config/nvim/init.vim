6c1055c2f0418dde5d93dfc30f27903275177f40
Carl Hjerpe <git@hjerpe.xyz>
2022-11-24 19:03:57 +0100

    Add nvim init file, should probably be rebuilt using HM modules

---
+++ lillecarl/dotfiles/.config/nvim/init.vim
@@ -0,0 +1,8 @@
+set encoding=utf-8
+set number
+set mouse=
+
+call plug#begin()
+" code completion in vim
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+call plug#end()
--- lillecarl/terminal.nix
+++ lillecarl/terminal.nix
@@ -9,6 +9,7 @@
     ".config/starship.toml".source = ./dotfiles/.config/starship.toml;
     ".config/tealdeer/config.toml".source = ./dotfiles/.config/tealdeer/config.toml;
     ".config/powershell/Microsoft.PowerShell_profile.ps1".source = ./dotfiles/.config/powershell/Microsoft.PowerShell_profile.ps1;
+    ".config/nvim/init.vim".source = ./dotfiles/.config/nvim/init.vim;
   };
 
   programs.git = {
