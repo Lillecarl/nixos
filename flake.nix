bf685a19e3c086b6b43a11b20d36d0bc12c380c8
root@rvlpsalt02.viaplay.cloud <root@rvlpsalt02.viaplay.cloud>
2022-12-19 12:54:43 +0000

    Lock flake to stable nixos (22.11 as of writing)
    We could probably keep using unstable, it just means more bleeding-edge packages. Which doesn't really matter since we'll be upgrading on our own terms anyways, but for now we'll keep it here.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Home Manager configuration of Jane Doe";
+  description = "Viaplay central nix flake repository";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    # Specify the source of Home Manager and nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
