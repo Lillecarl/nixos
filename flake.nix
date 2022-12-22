fe5ebf57f7267a867eede39b0a00469501ee1d54
Carl Hjerpe <git@hjerpe.xyz>
2022-12-22 05:00:53 +0100

    Add plasma-manager (not to nixos hm module yet though)

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     nixos-unstable-channel.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixos-unstable";
+    };
+
+    plasma-manager = {
+      url = github:pjones/plasma-manager;
+      inputs.nixpkgs.follows = "nixos-unstable";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
@@ -17,7 +23,6 @@
       pkgs = import nixos-unstable { system = "x86_64-linux"; config = { allowUnfree = true; }; };
     in
     {
-
       nixosConfigurations = rec {
         shitbox = nixos-unstable.lib.nixosSystem {
           inherit system;
@@ -28,26 +33,6 @@
             ./common/keybindings.nix
             ./common/killservice.nix
             nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-pc-ssd
-            nixos-hardware.nixosModules.common-pc
-          ];
-          specialArgs = inputs;
-        };
-        nub = nixos-unstable.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./nub
-            ./common
-            ./common/flatpak.nix
-            ./common/keybindings.nix
-            ./common/killservice.nix
-            ./common/xplatform.nix
-            nixos-hardware.nixosModules.lenovo-thinkpad-t14s
-            nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-cpu-amd-pstate
-            nixos-hardware.nixosModules.common-gpu-amd
-            nixos-hardware.nixosModules.common-pc-laptop
-            nixos-hardware.nixosModules.common-pc-laptop-acpi_call
             nixos-hardware.nixosModules.common-pc-ssd
             nixos-hardware.nixosModules.common-pc
 
@@ -64,12 +49,45 @@
           ];
           specialArgs = inputs;
         };
+        nub = nixos-unstable.lib.nixosSystem
+          {
+            inherit system;
+            modules = [
+              ./nub
+              ./common
+              ./common/flatpak.nix
+              ./common/keybindings.nix
+              ./common/killservice.nix
+              ./common/xplatform.nix
+              nixos-hardware.nixosModules.lenovo-thinkpad-t14s
+              nixos-hardware.nixosModules.common-cpu-amd
+              nixos-hardware.nixosModules.common-cpu-amd-pstate
+              nixos-hardware.nixosModules.common-gpu-amd
+              nixos-hardware.nixosModules.common-pc-laptop
+              nixos-hardware.nixosModules.common-pc-laptop-acpi_call
+              nixos-hardware.nixosModules.common-pc-ssd
+              nixos-hardware.nixosModules.common-pc
+
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.backupFileExtension = "old"; # Move non-hm files if they're in the way
+                home-manager.users.lillecarl = import ./lillecarl;
+
+                # Optionally, use home-manager.extraSpecialArgs to pass
+                # arguments to home.nix
+              }
+            ];
+            specialArgs = inputs;
+          };
       };
       homeConfigurations = {
         lillecarl = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
             ./lillecarl
+            inputs.plasma-manager.homeManagerModules.plasma-manager
           ];
         };
       };
