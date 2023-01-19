8b0c305a2032c22f5d3f9d79c72b4ebc82d6c4ad
Carl Hjerpe <git@hjerpe.xyz>
2023-01-19 18:03:51 +0100

    symlink all xonsh dependencies into one derivation
    This speeds up xonsh launch time by not having to walk the longest
    PYTHONPATH on earth.

--- overlays/xonsh-overlay/default.nix
+++ overlays/xonsh-overlay/default.nix
@@ -1,18 +1,26 @@
-final: prev: {
-  xonsh = prev.writeShellScriptBin "xonsh" ''
-    export PYTHONPATH=${prev.python3.pkgs.makePythonPath [
-      final.xonsh-direnv
+final: prev:
+let
+  # Join all xonsh dependencies into one derivation
+  xonsh-joined = prev.symlinkJoin {
+    name = "xonsh-joined";
+    # recurse all listed dependencies
+    paths = (prev.python3.pkgs.requiredPythonModules [
       final.xontrib-argcomplete
       final.xontrib-output-search
       final.xontrib-fzf-widgets
       final.xontrib-sh
       final.xontrib-jump-to-dir
       final.lazyasd
-      prev.xonsh
       prev.python3.pkgs.pyyaml
       prev.python3.pkgs.psutil
       prev.python3.pkgs.jinja2
-    ]};
+    ]);
+  };
+in
+{
+  # write xonsh wrapper script
+  xonsh = prev.writeShellScriptBin "xonsh" ''
+    export PYTHONPATH=${xonsh-joined}/lib/python3.10/site-packages:$PYTHONPATH
 
     ${prev.xonsh}/bin/xonsh $@
   '';
