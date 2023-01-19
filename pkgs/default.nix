e7d6e7ca94cc6ecd7ad54f9e97b2a8b93e1209d9
Carl Hjerpe <git@hjerpe.xyz>
2023-01-19 18:04:07 +0100

    Remove xonsh-direnv since it's bundled with xonsh.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
 final: prev: {
-  xonsh-direnv = prev.callPackage ../pkgs/xonsh-direnv { };
   xontrib-fzf-widgets = prev.callPackage ../pkgs/xontrib-fzf-widgets { };
   xontrib-sh = prev.callPackage ../pkgs/xontrib-sh { };
   xontrib-argcomplete = prev.callPackage ../pkgs/xontrib-argcomplete { };
