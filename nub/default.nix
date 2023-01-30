fffc155ee66fd913720cd944b8bec6c59e736a90
Carl Hjerpe <git@hjerpe.xyz>
2023-01-30 20:38:49 +0100

    Remove GoogleDNS config

--- nub/default.nix
+++ nub/default.nix
@@ -188,7 +188,16 @@ rec
     };
   };
   # Enable systemd-resolved, takes care of splitting DNS across interfaces n stuff
-  services.resolved.enable = true;
+  services.resolved = {
+    enable = true;
+
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+    ];
+  };
   systemd.network = {
     enable = true;
 
