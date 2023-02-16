5b52bc243b487b4b7fdffc11cd9a2467451ac8d5
Carl Hjerpe <git@hjerpe.xyz>
2023-02-16 23:48:54 +0100

    Fix desktop items

--- pkgs/desktopItemOverrides/slack.nix
+++ pkgs/desktopItemOverrides/slack.nix
@@ -5,7 +5,7 @@
 pkgs.makeDesktopItem {
   name = "slack";
   desktopName = "Slack";
-  icon = "${pkgs.slack-dark}/share/pixmaps/slack.png";
+  icon = "${pkgs.slack}/share/pixmaps/slack.png";
   mimeTypes = lib.splitString ";" "x-scheme-handler/slack";
-  exec = "${pkgs.slack-dark}/bin/slack --ozone-platform=wayland %U";
+  exec = "${pkgs.slack}/bin/slack --ozone-platform=wayland %U";
 }
