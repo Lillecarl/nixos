26fdf3a1b40c35b3e25fe41e64d6407b9443f000
Carl Hjerpe <git@hjerpe.xyz>
2023-02-26 13:48:36 +0100

    Disable running tests when building acme-dns.
    I assume joohoi does this already for us.

--- pkgs/acme-dns/default.nix
+++ pkgs/acme-dns/default.nix
@@ -13,6 +13,8 @@ buildGoModule rec {
   src = fetchFromGitHub versiondata;
   vendorSha256 = "sha256-q/P+cH2OihvPxPj2XWeLsTBHzQQABp0zjnof+Ys/qKo=";
 
+  doCheck = false;
+
   meta = {
     description = "Limited DNS server with RESTful HTTP API to handle ACME DNS challenges easily and securely.";
     homepage = "https://github.com/joohoi/acme-dns";
