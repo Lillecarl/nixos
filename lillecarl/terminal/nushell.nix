e7d021f06fe8da6af6cb2a2f7d66d5548bf992a7
Carl Hjerpe <git@hjerpe.xyz>
2023-03-15 13:20:34 +0100

    Add direnv to nushell

--- lillecarl/terminal/nushell.nix
+++ lillecarl/terminal/nushell.nix
@@ -6,6 +6,16 @@
     configFile.text = ''
       let-env config = {
         edit_mode: vi
+        # direnv integration
+        hooks: {
+          pre_prompt: [{
+            code: "
+              let direnv = (direnv export json | from json)
+              let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
+              $direnv | load-env
+            "
+          }]
+        }
       }
     '';
   };
