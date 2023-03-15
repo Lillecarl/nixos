e93c1dbd8637ae8c1551989966f543e21d3c90c5
Carl Hjerpe <git@hjerpe.xyz>
2023-03-15 13:49:03 +0100

    Fix xonsh wrapper PATH

--- pkgs/xonsh-wrapper/default.nix
+++ pkgs/xonsh-wrapper/default.nix
@@ -4,7 +4,7 @@
 }:
 writeShellScriptBin "xonsh" ''
   export PYTHONPATH="${xonsh-joined}/lib/python3.10/site-packages:$PYTHONPATH"
-  export PATH="${xonsh-joined}/lib/python3.10/site-packages:$PYTHONPATH"
+  export PATH="${xonsh-joined}/lib/python3.10/site-packages:$PATH"
 
-  exec ${xonsh}/bin/.xonsh-wrapped $@
+  exec ${xonsh}/bin/..xonsh-wrapped-wrapped $@
 ''
