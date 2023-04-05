bd8d31a3dabf4bba3f069bdc3c04814af685c66b
Carl Hjerpe <git@hjerpe.xyz>
2023-04-05 15:44:25 +0200

    Disable xwayland?

--- common/default.nix
+++ common/default.nix
@@ -22,7 +22,7 @@
 
   programs.dconf.enable = true;
   # Enable xwayland, not everything is wayland yet.
-  programs.xwayland.enable = true;
+  #programs.xwayland.enable = true;
   # Enable the X11 windowing system.
   services.xserver.enable = true; # Not sure why this is required.
   # Enable the Plasma 5 Desktop Environment.
