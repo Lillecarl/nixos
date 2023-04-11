e116ada47f9b51c4bf4ee1bc9801b585edf6bd01
Carl Hjerpe <git@hjerpe.xyz>
2023-04-11 03:43:27 +0200

    Move nub to flake-parts

--- flake.nix
+++ flake.nix
@@ -79,6 +79,7 @@
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
         ./shitbox/flake-module.nix
+        ./nub/flake-module.nix
         ./lillecarl/flake-module.nix
         ./terraform/flake-module.nix
         ./system-manager/flake-module.nix
@@ -86,32 +87,6 @@
       systems = [ "x86_64-linux" "x86_64-darwin" ];
       flake = rec {
         nixosConfigurations = rec {
-          nub = inputs.nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-              ./nub
-              ./common
-              ./common/flatpak.nix
-              ./common/verycommon.nix
-              ./common/keybindings.nix
-              ./common/killservice.nix
-              ./common/xplatform.nix
-              ./modules/ifupdown2
-              inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s
-              inputs.nixos-hardware.nixosModules.common-cpu-amd
-              inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-              inputs.nixos-hardware.nixosModules.common-gpu-amd
-              inputs.nixos-hardware.nixosModules.common-pc-laptop
-              inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call
-              inputs.nixos-hardware.nixosModules.common-pc-ssd
-              inputs.nixos-hardware.nixosModules.common-pc
-              inputs.dwarffs.nixosModules.dwarffs
-            ];
-            specialArgs = {
-              inherit inputs;
-              programs-sqlite-db = inputs.flake-programs-sqlite.packages."x86_64-linux".programs-sqlite;
-            };
-          };
           nubvm = inputs.nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
---
+++ nub/flake-module.nix
@@ -0,0 +1,31 @@
+{ inputs, lib, ... }:
+{
+  flake = {
+    nixosConfigurations.nub = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./default.nix
+        ../common
+        ../common/flatpak.nix
+        ../common/verycommon.nix
+        ../common/keybindings.nix
+        ../common/killservice.nix
+        ../common/xplatform.nix
+        ../modules/ifupdown2
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14s
+        inputs.nixos-hardware.nixosModules.common-cpu-amd
+        inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+        inputs.nixos-hardware.nixosModules.common-gpu-amd
+        inputs.nixos-hardware.nixosModules.common-pc-laptop
+        inputs.nixos-hardware.nixosModules.common-pc-laptop-acpi_call
+        inputs.nixos-hardware.nixosModules.common-pc-ssd
+        inputs.nixos-hardware.nixosModules.common-pc
+        inputs.dwarffs.nixosModules.dwarffs
+      ];
+      specialArgs = {
+        inherit inputs;
+        programs-sqlite-db = inputs.flake-programs-sqlite.packages."x86_64-linux".programs-sqlite;
+      };
+    };
+  };
+}
