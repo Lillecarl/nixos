7c6163c87cba65c22725e36839b092e422953363
Carl Hjerpe <git@hjerpe.xyz>
2023-04-29 20:25:40 +0200

    Fix sourcing of nix stuff

--- modules/hm/xonsh/default.nix
+++ modules/hm/xonsh/default.nix
@@ -43,27 +43,13 @@ in
         text = lib.concatStringsSep "\n" ([
           ''
             #! ${cfg.package}/bin/xonsh
-            if not __xonsh__.env.get('__NIXOS_SET_ENVIRONMENT_DONE'):
-              # The NixOS environment and thereby also $PATH
-              # haven't been fully set up at this point. But
-              # `source-bash` below requires `bash` to be on $PATH,
-              # so add an entry with bash's location:
-              $PATH.add('${pkgs.bash}')
-            
-              # Stash xonsh's ls alias, so that we don't get a collision
-              # with Bash's ls alias from environment.shellAliases:
-              _ls_alias = aliases.pop('ls', None)
-            
-              # Source the NixOS environment config.
-              source-bash "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
-            
-              # Restore xonsh's ls alias, overriding that from Bash (if any).
-              if _ls_alias is not None:
-                  aliases['ls'] = _ls_alias
-              del _ls_alias
+            $PATH.add('${pkgs.bash}')
+
+            # Source the HM environment config.
+            source-bash --suppress-skip-message "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
           ''
           (lib.optionalString cfg.enableBashCompletion ''
-            source-bash ${pkgs.bash-completion}/etc/profile.d/bash_completion.sh"
+            source-bash --suppress-skip-message "${pkgs.bash-completion}/etc/profile.d/bash_completion.sh"
           '')
           cfg.extraConfig
         ]);
