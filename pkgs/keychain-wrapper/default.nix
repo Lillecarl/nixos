632e050d28c6318e8e87eda31d29d0b4fc2287b2
Carl Hjerpe <git@hjerpe.xyz>
2023-04-29 17:57:51 +0200

    Fix keychain wrapper errors when things run as su

--- pkgs/keychain-wrapper/default.nix
+++ pkgs/keychain-wrapper/default.nix
@@ -2,6 +2,12 @@
 , keychain
 }:
 writeShellScriptBin "keychain" ''
-  mkdir -p "$XDG_RUNTIME_DIR"/keychain
-  exec ${keychain}/bin/keychain --dir "$XDG_RUNTIME_DIR"/keychain $@
+  # Only create keychain dir if XDG specification is set
+  if ! [[ -z $XDG_RUNTIME_DIR ]]; then
+    mkdir -p "$XDG_RUNTIME_DIR"/keychain
+    exec ${keychain}/bin/keychain --dir "$XDG_RUNTIME_DIR"/keychain $@
+  fi
+
+  # Execute normal keychain just as is
+  exec ${keychain}/bin/keychain $@
 ''
