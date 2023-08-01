7532eaa98e3fdd3346d2484dd059b1a5b283ca5f
Carl Hjerpe <github@lillecarl.com>
2023-08-01 22:46:44 +0000

    Add flakes configuration to ISO

--- nixos-installer/default.nix
+++ nixos-installer/default.nix
@@ -7,8 +7,19 @@
 }:
 {
   environment.systemPackages = [
+    # Add disko command
     inputs.disko.packages.${pkgs.system}.default
   ];
+
+  nix = {
+    # Enable flakes
+    extraOptions = ''
+      experimental-features = nix-command flakes repl-flake
+    '';
+    # Add flake inputs to flake registry
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  };
 
   system.stateVersion = "23.11";
 }
