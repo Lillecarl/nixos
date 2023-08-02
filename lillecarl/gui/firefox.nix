4cc095147734cce8edde81504690c3dd4e6d1dec
Carl Hjerpe <git@hjerpe.xyz>
2023-08-02 05:23:42 +0200

    More firefox configuration

--- lillecarl/gui/firefox.nix
+++ lillecarl/gui/firefox.nix
@@ -63,20 +63,70 @@
         ];
 
         settings = {
+          # A lot of settings are taken from here: https://github.com/yokoffing/Betterfox/blob/main/user.js
+
+          # Fastfox
+          "browser.startup.preXulSkeletonUI" = false;
+          "content.notify.interval" = 100000;
+          "nglayout.initialpaint.delay" = 0;
+          "nglayout.initialpaint.delay_in_oopif" = 0;
+          # Speculative connections
+          "browser.places.speculativeConnect.enabled" = false;
+          "browser.urlbar.speculativeConnect.enabled" = false;
+          "network.dns.disablePrefetch" = true;
+          "network.http.speculative-parallel-limit" = 0;
+          "network.predictor.enable-prefetch" = false;
+          "network.predictor.enabled" = false;
+          "network.prefetch-next" = false;
+          # Passwords and autofill
+          "editor.truncate_user_pastes" = false;
+          "signon.autofillForms" = false;
+          "signon.formlessCapture.enabled" = false;
+          "signon.privateBrowsingCapture.enabled" = false;
+          "signon.rememberSignons" = false;
+          # Address + CC manager
+          "browser.formfill.enable" = false;
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
+          "extensions.formautofill.heuristics.enabled" = false;
+          # Telemetry
+          "app.normandy.api_url" = "";
+          "app.normandy.enabled" = false;
+          "app.shield.optoutstudies.enabled" = false;
+          "breakpad.reportURL" = "";
+          "browser.crashReports.unsubmittedCheck.autoSubmit2" = false;
+          "browser.discovery.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "browser.tabs.crashReporting.sendReport" = false;
+          "captivedetect.canonicalURL" = "";
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "default-browser-agent.enabled" = false;
+          "network.captive-portal-service.enabled" = false;
+          "network.connectivity-service.enabled" = false;
+          "toolkit.coverage.opt-out" = true;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.coverage.opt-out" = true;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.server" = "data:,";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
+          # Misc
           "app.update.auto" = false; # don't auto update
           "browser.aboutConfig.showWarning" = false; # to show warning prompt when accessing about:config page.
           "browser.ctrlTab.sortByRecentlyUsed" = true; # ctrl + tab sort tabs by recently used
-          "browser.newtabpage.activity-stream.feeds.telemetry" = false; # disable telemetry
-          "browser.newtabpage.activity-stream.telemetry" = false; # disable telemetry
           "browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint" = ""; # Set telemetry server to empty string
-          "browser.ping-centre.telemetry" = false; # some kind of telemetry
           "browser.shell.checkDefaultBrowser" = false; # don't ask about default browser
           "browser.startup.homepage" = "about:blank"; # set startup page to empty page
           "browser.startup.page" = 3; # restore sessions on startup
-          "datareporting.healthreport.service.enabled" = false; # setting Firefox Health Report functionality (https://wiki.mozilla.org/Firefox_Health_Report)
-          "datareporting.healthreport.uploadEnabled" = false; # setting Firefox Health Report uploading functionality.
-          "datareporting.policy.dataSubmissionEnabled" = false; # this is the data submission master kill switch. (https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/internals/preferences.html)
           "dom.successive_dialog_time_limit" = 1; # don't allow websites to abuse prompts.
+          "extensions.pocket.enabled" = false; # Disable pocket
           "extensions.update.autoUpdateDefault" = false; # don't auto update extensions
           "general.autoScroll" = true; # press scroll wheel scroll
           "privacy.annotate_channels.strict_list.enabled" = true; # strict browsing
@@ -86,19 +136,10 @@
           "privacy.trackingprotection.enabled" = true; # tracking protection
           "privacy.trackingprotection.socialtracking.enabled" = true; # social tracking protection
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # enable userChrome
-          "toolkit.telemetry.archive.enabled" = false; # disable telemetry
-          "toolkit.telemetry.bhrPing.enabled" = false; # disable telemetry
-          "toolkit.telemetry.enabled" = false; # disable telemetry
-          "toolkit.telemetry.firstShutdownPing.enabled" = false; # disable telemetry
-          "toolkit.telemetry.newProfilePing.enabled" = false; # don't ping things
           "toolkit.telemetry.pioneer-new-studies-available" = false; # don't ping things
           "toolkit.telemetry.prompted" = 2; # we've been asked about telemetry
           "toolkit.telemetry.rejected" = true; # we rejected telemetry
-          "toolkit.telemetry.server" = ""; # unconfigure telemetry server
-          "toolkit.telemetry.shutdownPingSender.enabled" = false; # don't ping things
-          "toolkit.telemetry.unified" = false; # not sure
           "toolkit.telemetry.unifiedIsOptIn" = false; # don't opt in
-          "toolkit.telemetry.updatePing.enabled" = false; # don't ping things
         };
 
         userChrome = ''
