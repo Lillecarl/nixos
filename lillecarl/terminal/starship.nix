218d7bd738cd1ca80a1c7fa6391cad12e43a7ecd
Carl Hjerpe <git@hjerpe.xyz>
2023-08-02 17:19:42 +0200

    formatting

--- lillecarl/terminal/starship.nix
+++ lillecarl/terminal/starship.nix
@@ -145,49 +145,47 @@ in
     enable = true;
     enableNushellIntegration = true;
 
-    settings =
-      builtins.fromTOML icons
-      // {
-        # We don't use terraform workspaces so don't consume the space
-        terraform = {
-          disabled = true;
-        };
-
-        # Directory config, truncation_length is subpath count not char count
-        # don't truncate to git repo (not sure how i feel about this one yet)
-        directory = {
-          truncate_to_repo = false;
-          truncation_length = 10;
-        };
-
-        # Show exit codes
-        status = {
-          disabled = false;
-        };
-
-        username = {
-          format = "[$user]($style) on";
-        };
-
-        time = {
-          disabled = false;
-        };
-
-        kubernetes = {
-          disabled = false;
-        };
-
-        sudo = {
-          disabled = false;
-        };
-
-        # Display which shell we're in
-        # Do we actually need this? We use xonsh all the time.
-        env_var.STARSHIP_SHELL = {
-          format = "🐚 [$env_value]($style) ";
-          style = "fg:green";
-        };
+    settings = builtins.fromTOML icons // {
+      # We don't use terraform workspaces so don't consume the space
+      terraform = {
+        disabled = true;
       };
+
+      # Directory config, truncation_length is subpath count not char count
+      # don't truncate to git repo (not sure how i feel about this one yet)
+      directory = {
+        truncate_to_repo = false;
+        truncation_length = 10;
+      };
+
+      # Show exit codes
+      status = {
+        disabled = false;
+      };
+
+      username = {
+        format = "[$user]($style) on";
+      };
+
+      time = {
+        disabled = false;
+      };
+
+      kubernetes = {
+        disabled = false;
+      };
+
+      sudo = {
+        disabled = false;
+      };
+
+      # Display which shell we're in
+      # Do we actually need this? We use xonsh all the time.
+      env_var.STARSHIP_SHELL = {
+        format = "🐚 [$env_value]($style) ";
+        style = "fg:green";
+      };
+    };
   };
   programs.keychain.enableNushellIntegration = true;
 }
