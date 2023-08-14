c19068438ef8720eda765c4fd1684829d45a9846
Carl Hjerpe <git@hjerpe.xyz>
2023-08-14 15:00:02 +0200

    Updates + fmt

--- nub/default.nix
+++ nub/default.nix
@@ -64,8 +64,8 @@ in
     enable = true;
 
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC="performance";
-      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     };
   };
 
