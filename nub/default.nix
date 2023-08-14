5660cd00aa3be0c2254855544dffe8dc796b4151
Carl Hjerpe <git@hjerpe.xyz>
2023-08-14 21:56:32 +0200

    configure battery threshold through TLP

--- nub/default.nix
+++ nub/default.nix
@@ -66,6 +66,8 @@ in
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      START_CHARGE_THRESH_BAT0 = 80;
+      STOP_CHARGE_THRESH_BAT0 = 85;
     };
   };
 
