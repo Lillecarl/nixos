9f8400bbf75f55707b98cff04efc0c5893e9a131
Carl Hjerpe <git@hjerpe.xyz>
2023-08-16 04:39:08 +0200

    Implement sleeping in wayidle with python script

--- lillecarl/gui/swaytools.nix
+++ lillecarl/gui/swaytools.nix
@@ -4,6 +4,21 @@
 }:
 let
   swaylock = "${pkgs.swaylock}/bin/swaylock";
+  swaysleep = (pkgs.writers.writePython3 "swaysleep" { libraries = [ pkgs.python3.pkgs.plumbum ]; } ''
+    from pathlib import Path
+    from plumbum import local
+
+    systemctl = local["${pkgs.systemd}/bin/systemctl"]
+
+    # Contains status about our laptop battery
+    batstat = Path("/sys/class/power_supply/BAT0/status").read_text()
+
+    if batstat.find("Discharging") != -1:
+        print("Suspending")
+        systemctl(["suspend"])
+    else:
+        print("Not suspending, we're not discharging")
+  '');
 in
 {
   programs.swaylock = {
@@ -28,6 +43,10 @@ in
 
     timeouts = [
       { timeout = 300; command = swaylock; }
+      # I can be very forgetful at times
+      { timeout = 1800; command = "${swaysleep}"; }
+      { timeout = 3600; command = "${swaysleep}"; }
+      { timeout = 7200; command = "${swaysleep}"; }
     ];
   };
 }
