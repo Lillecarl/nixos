6b3e6b708689c87f139cd618c7762402d3585bfc
Carl Hjerpe <git@hjerpe.xyz>
2023-08-17 19:17:46 +0200

    Change some power settings

--- nub/default.nix
+++ nub/default.nix
@@ -64,8 +64,10 @@ in
     enable = true;
 
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_power";
       START_CHARGE_THRESH_BAT0 = 80;
       STOP_CHARGE_THRESH_BAT0 = 85;
     };
