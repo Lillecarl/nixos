8c3793e72abcd28cc9b049561687f892e0317e66
Carl Hjerpe <git@hjerpe.xyz>
2023-08-21 19:33:19 +0200

    Configure immutable users

--- common/users.nix
+++ common/users.nix
@@ -1,40 +1,50 @@
 { pkgs
 , ...
 }:
-rec {
-  # Allow root to map to LilleCarl user in LXD container
-  users.users.root = {
-    subUidRanges = [
-      {
-        count = 1;
-        startUid = users.users.lillecarl.uid;
-      }
-    ];
-    subGidRanges = [
-      {
-        count = 1;
-        startGid = 1000;
-      }
-    ];
-  };
+let
+  firstUid = 1000;
+in
+{
+  users = {
+    defaultUserShell = pkgs.zsh;
+    enforceIdUniqueness = true;
+    mutableUsers = false;
 
-  users.defaultUserShell = pkgs.zsh;
-  users.users.lillecarl = {
-    uid = 1000;
-    #shell = "${pkgs.xonsh}/bin/xonsh";
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "libvirtd"
-      "networkmanager"
-      "lxd"
-      "flatpak"
-      "adbusers"
-      "podman"
-      "wireshark"
-      "i2c"
-      "video"
-    ];
-    autoSubUidGidRange = true;
+    users = {
+      root = {
+        subUidRanges = [
+          {
+            count = 1;
+            startUid = firstUid;
+          }
+        ];
+        subGidRanges = [
+          {
+            count = 1;
+            startGid = 1000;
+          }
+        ];
+      };
+
+      lillecarl = {
+        uid = firstUid;
+        hashedPassword = "$y$j9T$U4zBBS9RMV9YMttHauO8k0$V.KT/P/AdBTXXT8f6p9EIlCsZV5UnaPDgEVtUvUJU3C";
+        createHome = true;
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "libvirtd"
+          "networkmanager"
+          "lxd"
+          "flatpak"
+          "adbusers"
+          "podman"
+          "wireshark"
+          "i2c"
+          "video"
+        ];
+        autoSubUidGidRange = true;
+      };
+    };
   };
 }
