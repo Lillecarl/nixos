21856c298645f4cea74bd0d0f66011cbccfc9687
Carl Hjerpe <git@hjerpe.xyz>
2023-08-21 17:02:13 +0200

    Disable presentation notification

--- lillecarl/gui/mako.nix
+++ lillecarl/gui/mako.nix
@@ -4,5 +4,10 @@
     enable = true;
 
     anchor = "center";
+
+    extraConfig = ''
+      [app-name="Firefox" body="Klicka här för att återgå till videosamtalet när du vill sluta presentera"]
+      invisible=1
+    '';
   };
 }
