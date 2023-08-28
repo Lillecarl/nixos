fc41299fdb7bd541d85efc8143d97002e00877ff
Carl Hjerpe <git@hjerpe.xyz>
2023-08-29 00:34:06 +0200

    Configure powershell to set itself as $SHELL

--- lillecarl/dotfiles/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ lillecarl/dotfiles/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -2,11 +2,11 @@ function Invoke-MySudo {
   & /usr/bin/env sudo -E pwsh -command "$args"
 }
 
-set-alias sudo Invoke-MySudo
+$env:SHELL = "pwsh"
+
+Set-Alias sudo Invoke-MySudo
 
 # Load starship
-Get-Command -ErrorAction SilentlyContinue -Name kubectl | Out-Null && kubectl completion powershell | Out-String | Invoke-Expression
-Get-Command -ErrorAction SilentlyContinue -Name starship | Out-Null && starship init powershell | Out-String | Invoke-Expression
-#Invoke-Expression (&starship init powershell)
+Invoke-Expression (&starship init powershell)
 
 #Clear-Host
