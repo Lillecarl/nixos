a3e428a43ba77b1c79d744354a9d96f6cb765b7b
Carl Hjerpe <git@hjerpe.xyz>
2023-09-08 10:17:51 +0200

    Fix filepicker portal missing
    https://wiki.archlinux.org/title/XDG_Desktop_Portal#List_of_backends_and_interfaces

--- common/xdg.nix
+++ common/xdg.nix
@@ -3,12 +3,17 @@
 , ...
 }:
 {
+  environment.systemPackages = [
+    pkgs.xdg-utils
+  ];
+
   xdg = {
     portal = {
       wlr.enable = false;
 
       extraPortals = lib.mkForce [
         pkgs.gnome.gnome-keyring
+        pkgs.xdg-desktop-portal-gtk
         pkgs.xdg-desktop-portal-hyprland
       ];
     };
