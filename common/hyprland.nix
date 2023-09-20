cf0f5bc7efc928dae615a766ad2a849542c1b412
Carl Hjerpe <git@hjerpe.xyz>
2023-09-20 04:30:49 +0200

    Revert "Remove hyprland from NixOS"
    This reverts commit 1733b69e995bfe74948b8ab669775e8ec9caee85.
    
    For some fucking reason this causes hyprland to crash on startup.

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,14 +1,5 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  # Taken from https://github.com/hyprwm/Hyprland/blob/main/nix/module.nix
-  # TODO: xdg.portal should be implemented as a HM module.
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  };
-  programs.dconf.enable = true;
-  security.polkit.enable = true;
-
-  # nm-applet is a NixOS module for some reason. TODO: Make HM module
+  programs.hyprland.enable = true;
   programs.nm-applet.enable = true;
 }
