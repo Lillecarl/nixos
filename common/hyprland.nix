1733b69e995bfe74948b8ab669775e8ec9caee85
Carl Hjerpe <git@hjerpe.xyz>
2023-09-20 02:15:35 +0200

    Remove hyprland from NixOS
    The module is a bit shit and we don't rely on it's features in any way.

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.hyprland.enable = true;
+  # Taken from https://github.com/hyprwm/Hyprland/blob/main/nix/module.nix
+  # TODO: xdg.portal should be implemented as a HM module.
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+  programs.dconf.enable = true;
+  security.polkit.enable = true;
+
+  # nm-applet is a NixOS module for some reason. TODO: Make HM module
   programs.nm-applet.enable = true;
 }
