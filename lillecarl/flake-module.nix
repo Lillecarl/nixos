c56e180bb2d1a2508d4880a868ab360c05ec7785
Carl Hjerpe <git@hjerpe.xyz>
2023-09-22 14:02:54 +0200

    Make computer specific monitor configurations

--- lillecarl/flake-module.nix
+++ lillecarl/flake-module.nix
@@ -38,12 +38,21 @@ in
           extraSpecialArgs = {
             keyboardName = "daskeyboard";
             bluetooth = false;
+            monitorConfig = ''
+              monitor=DP-1,2560x1440@164.802002,1080x240,1.0
+              monitor=DVI-D-2,1920x1080@143.996002,0x0,1.0
+              monitor=DVI-D-2,transform,3
+            '';
           };
         });
         "lillecarl@nub" = mkHome "x86_64-linux" (guibase // {
           extraSpecialArgs = {
             keyboardName = "at-translated-set-2-keyboard";
             bluetooth = true;
+            monitorConfig = ''
+              monitor=DP-2,3440x1440@60,0x0,1.0
+              monitor=eDP-1,1920x1200@60,760x1440,1.0
+            '';
           };
         });
         lillecarl-term = mkHome "x86_64-linux" {
--- lillecarl/gui/hyprland.nix
+++ lillecarl/gui/hyprland.nix
@@ -4,6 +4,7 @@
 , flakeloc
 , keyboardName
 , bluetooth
+, monitorConfig
 , ...
 }:
 let
@@ -45,6 +46,50 @@ let
   };
 
   wallpaper = "${inputs.nixos-artwork}/wallpapers/nix-wallpaper-watersplash.png";
+
+  extraConfig = ''
+    # Source from home-manager file that can be live edited through out of store symlinks.
+    source = ${config.xdg.configHome}/hypr/linked.conf
+
+    $mainMod = SUPER
+
+    exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
+  '' +
+  (if bluetooth then ''
+    exec-once = ${pkgs.blueman}/bin/blueman-applet
+  '' else ""
+  )
+  + ''
+    # Per machine configured home configuration
+    ${monitorConfig}
+
+    # Launch terminal
+    bind  = $mainMod          , Q       , exec, ${pkgs.wezterm}/bin/wezterm-gui
+    # Awesome locker
+    bind  = Ctrl_L Alt_L      , delete  , exec, ${pkgs.swaylock}/bin/swaylock
+    # Media buttons
+    bindl =                   , code:121, exec, ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle
+    bindl =                   , code:122, exec, ${pkgs.avizo}/bin/volumectl down
+    bindl =                   , code:123, exec, ${pkgs.avizo}/bin/volumectl up
+    bindl =                   , code:198, exec, ${pkgs.mictoggle}
+    # Increase and decrease screen backlight
+    bindl =                   , code:232, exec, ${pkgs.avizo}/bin/lightctl down
+    bindl =                   , code:233, exec, ${pkgs.avizo}/bin/lightctl up
+    # drun app launcher
+    bind  = $mainMod          , R       , exec, ${pkgs.rofi-wayland}/bin/rofi -show drun
+    # search application window titles
+    bind  = $mainMod          , tab     , exec, ${pkgs.rofi-wayland}/bin/rofi -show window
+    # Switch to US layout
+    bindl = $mainMod          , E       , exec, ${hyprctl} switchxkblayout ${keyboardName} 0
+    # Switch to SE layout
+    bindl = $mainMod          , S       , exec, ${hyprctl} switchxkblayout ${keyboardName} 1
+    # Switch to EU layout
+    bindl = $mainMod          , Y       , exec, ${hyprctl} switchxkblayout ${keyboardName} 2
+    bind  =                   , Print   , exec, ${printScript} screen --edit --upload
+    bind  = $mainMod          , Print   , exec, ${printScript} window --edit --upload
+    bind  = $mainMod Shift_L  , Print   , exec, ${printScript} region --edit --upload
+    bind  = Ctrl_L Alt_L      , V       , exec, ${pkgs.clipman}/bin/clipman pick --tool=rofi
+  '';
 in
 {
   home.packages = [
@@ -77,7 +122,8 @@ in
   };
 
   home.file.".config/hypr/linked.conf".source = config.lib.file.mkOutOfStoreSymlink "${flakeloc}/lillecarl/gui/hyprland.conf";
-  home.file.".config/hypr/hyprlandd.conf".source = "${config.xdg.configHome}/hypr/hyprland.conf";
+  #home.file.".config/hypr/hyprlandd.conf" = config.xdg.configFile."hypr/hyprland.conf";
+  xdg.configFile."hypr/hyprlandd.conf".text = config.xdg.configFile."hypr/hyprland.conf".text;
 
   home.file.".config/hypr/hyprpaper.conf".text = ''
     preload = ${wallpaper}
@@ -100,48 +146,7 @@ in
     systemdIntegration = true;
     disableAutoreload = true;
 
-    xwayland.enable = false;
-
-    extraConfig = ''
-      # Source from home-manager file that can be live edited through out of store symlinks.
-      source = ${config.xdg.configHome}/hypr/linked.conf
-
-      $mainMod = SUPER
-
-      exec-once = ${pkgs.hyprpaper}/bin/hyprpaper
-      exec-once = ${pkgs.ulauncher}/bin/ulauncher --hide-window
-    '' +
-    (if bluetooth then ''
-      exec-once = ${pkgs.blueman}/bin/blueman-applet
-    '' else ""
-    )
-    + ''
-      # Launch terminal
-      bind  = $mainMod          , Q       , exec, ${pkgs.wezterm}/bin/wezterm-gui
-      # Awesome locker
-      bind  = Ctrl_L Alt_L      , delete  , exec, ${pkgs.swaylock}/bin/swaylock
-      # Media buttons
-      bindl =                   , code:121, exec, ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle
-      bindl =                   , code:122, exec, ${pkgs.avizo}/bin/volumectl down
-      bindl =                   , code:123, exec, ${pkgs.avizo}/bin/volumectl up
-      bindl =                   , code:198, exec, ${pkgs.mictoggle}
-      # Increase and decrease screen backlight
-      bindl =                   , code:232, exec, ${pkgs.avizo}/bin/lightctl down
-      bindl =                   , code:233, exec, ${pkgs.avizo}/bin/lightctl up
-      # drun app launcher
-      bind  = $mainMod          , R       , exec, ${pkgs.rofi-wayland}/bin/rofi -show drun
-      # search application window titles
-      bind  = $mainMod          , tab     , exec, ${pkgs.rofi-wayland}/bin/rofi -show window
-      # Switch to US layout
-      bindl = $mainMod          , E       , exec, ${hyprctl} switchxkblayout ${keyboardName} 0
-      # Switch to SE layout
-      bindl = $mainMod          , S       , exec, ${hyprctl} switchxkblayout ${keyboardName} 1
-      # Switch to EU layout
-      bindl = $mainMod          , Y       , exec, ${hyprctl} switchxkblayout ${keyboardName} 2
-      bind  =                   , Print   , exec, ${printScript} screen --edit --upload
-      bind  = $mainMod          , Print   , exec, ${printScript} window --edit --upload
-      bind  = $mainMod Shift_L  , Print   , exec, ${printScript} region --edit --upload
-      bind  = Ctrl_L Alt_L      , V       , exec, ${pkgs.clipman}/bin/clipman pick --tool=rofi
-    '';
+    xwayland.enable = true;
+    inherit extraConfig;
   };
 }
