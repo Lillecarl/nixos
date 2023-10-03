126ed1b1da566061adb13525bafce2dee51bee20
Carl Hjerpe <git@hjerpe.xyz>
2023-10-04 01:29:37 +0200

    Cleaning of rebuild script

--- rebuild.py
+++ rebuild.py
@@ -10,11 +10,9 @@ ping = local["ping"]
 sudo = local["sudo"]
 echo = local["echo"]
 virsh = local["virsh"]
-nixos_rebuild = local["nixos-rebuild"]
-home_manager = local["home-manager"]
+nh = local["nh"]
 
 hostname = socket.gethostname()
-commonargs = ["--flake", environ["FLAKELOC"], "--keep-failed", "-v", "--impure"]
 
 def check_connection(address):
     try:
@@ -31,13 +29,13 @@ def block_until_internet(address):
     print("No internet, retrying (indefinitely)")
     sleep(1)
 
-sudo[echo["Building nixos"]] & FG  # type: ignore
+sudo[echo["Building nixos"]].run_fg()
 if hostname == "shitbox":
     print("Dumping Windows VM XML")
     xml = sudo[virsh["dumpxml", "win10"]]()
     local.path("shitbox/win10.xml").write(xml)
 block_until_internet("1.1.1.1")
-nixos_rebuild["switch", "--use-remote-sudo", commonargs] & FG  # type: ignore
+nh["os", "switch"] & FG  # type: ignore
 print("Building home")
 block_until_internet("1.1.1.1")
-home_manager["switch", "-b", "old", commonargs] & FG  # type: ignore
+nh["home", "switch", "--", "--impure" ].run_fg()
