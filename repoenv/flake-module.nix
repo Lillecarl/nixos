1192723bd7ed7a3910bf485634d299ce969a63cc
Carl Hjerpe <git@hjerpe.xyz>
2023-10-12 16:02:35 +0200

    Add msr-tools to repo

--- repoenv/flake-module.nix
+++ repoenv/flake-module.nix
@@ -13,6 +13,7 @@
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.pokemonsay
+          pkgs.msr-tools
           (pkgs.python3.withPackages (ps: with ps; [
             PyGithub
             levenshtein
