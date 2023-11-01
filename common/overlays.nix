e640f8187f1bb850e6b6573f39b3687093cbc3d5
Carl Hjerpe <git@hjerpe.xyz>
2023-11-01 12:40:21 +0100

    Don't use nixpkgs-wayland overlay
    Keep the flake input if we want packages, but don't use their overlay.

--- common/overlays.nix
+++ common/overlays.nix
@@ -6,7 +6,7 @@ in
   nixpkgs.overlays = [
     inputs.hyprland.overlays.default
     inputs.nix-vscode-extensions.overlays.default
-    inputs.nixpkgs-wayland.overlay
+    #inputs.nixpkgs-wayland.overlay
     inputs.nur.overlay
     #inputs.waybar.overlays.default
     pkgs-overlay
