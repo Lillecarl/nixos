aa6cfc7877650b90d07e21ab5bfd85ed66527a98
Carl Hjerpe <git@hjerpe.xyz>
2023-11-01 16:19:17 +0100

    Add paste bindings to vim

--- lillecarl/terminal/neovim/default.nix
+++ lillecarl/terminal/neovim/default.nix
@@ -64,6 +64,18 @@
       set encoding=utf-8
       set ignorecase
       set smartcase
+
+      " " Copy to clipboard
+      vnoremap  <leader>y  "+y
+      nnoremap  <leader>Y  "+yg_
+      nnoremap  <leader>y  "+y
+      nnoremap  <leader>yy  "+yy
+
+      " " Paste from clipboard
+      nnoremap <leader>p "+p
+      nnoremap <leader>P "+P
+      vnoremap <leader>p "+p
+      vnoremap <leader>P "+P
     '';
 
     extraLuaConfig = ''
