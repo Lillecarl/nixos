bbc0d9f2355b9345556a11bc91e4060ba59c9389
Carl Hjerpe <git@hjerpe.xyz>
2023-11-03 15:04:27 +0100

    Linux monitors

--- lillecarl/flake-module.nix
+++ lillecarl/flake-module.nix
@@ -41,8 +41,10 @@ in
             bluetooth = false;
             monitorConfig = ''
               monitor=DP-1,2560x1440@164.802002,1080x240,1.0
-              monitor=HDMI-A-2,1920x1080@143.996002,0x0,1.0
-              monitor=HDMI-A-2,transform,3
+              monitor=DP-2,1920x1080@143.996002,0x0,1.0
+              monitor=DP-2,transform,3
+              monitor=DP-3,1920x1080@143.996002,3640x0,1.0
+              monitor=DP-3,transform,1
             '';
             systemConfig = self.nixosConfigurations.shitbox.config;
           };
