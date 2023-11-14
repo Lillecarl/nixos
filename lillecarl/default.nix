6af0ed6eca74b9f51759809ec97dc5cc3eb00ad6
Carl Hjerpe <git@hjerpe.xyz>
2023-11-14 18:18:27 +0100

    Use sd-switch to restart user units

--- lillecarl/default.nix
+++ lillecarl/default.nix
@@ -15,4 +15,10 @@
   home.enableNixpkgsReleaseCheck = true;
   news.display = "silent";
   programs.home-manager.enable = true;
+
+  # Use experimental sd-switch to determine which systemd user units
+  # to restart, do it automatically.
+  systemd.user.startServices = "sd-switch";
+  # No user services require more than 10 sec to start.
+  systemd.user.servicesStartTimeoutMs = 10000;
 }
