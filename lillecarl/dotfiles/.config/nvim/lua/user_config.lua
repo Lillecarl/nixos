0b15c8fe83c305f142fde7977a04f1b5242a8949
Carl Hjerpe <git@hjerpe.xyz>
2023-11-15 17:01:07 +0100

    nvim-tree

--- lillecarl/dotfiles/.config/nvim/lua/user_config.lua
+++ lillecarl/dotfiles/.config/nvim/lua/user_config.lua
@@ -1,7 +1,34 @@
+-- Setup nvim-tree
+local nvim_tree_api = require "nvim-tree.api"
+
+local function nvim_tree_attach(bufnr)
+  local nvim_tree_api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  nvim_tree_api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings (Within nvim-tree buffer)
+  vim.keymap.set('n', '<C-t>', nvim_tree_api.tree.toggle, {})
+  vim.keymap.set('n', '?',     nvim_tree_api.tree.toggle_help,                  opts('Help'))
+end
+require("nvim-tree").setup({
+  on_attach = nvim_tree_attach,
+})
+
+-- Setup nvim-tree bindings
+vim.keymap.set('n', '<C-t>', nvim_tree_api.tree.toggle, {})
+
+
 -- Configure NERDTree bindings
-vim.cmd [[
+--[[
+--vim.cmd [[
 nnoremap <leader>n :NERDTreeFocus<CR>
 nnoremap <C-n> :NERDTree<CR>
 nnoremap <C-t> :NERDTreeToggle<CR>
 nnoremap <C-f> :NERDTreeFind<CR>
 ]]
+--]]
