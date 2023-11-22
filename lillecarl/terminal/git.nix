2b9dbc3aafb6708e972a446a2281bf57782f582e
Carl Hjerpe <git@hjerpe.xyz>
2023-11-22 12:53:53 +0100

    git: aliases
    git mergepush pushes to gitlab with some predefined push options
    git root prints the root of the current git repo you're in

--- lillecarl/terminal/git.nix
+++ lillecarl/terminal/git.nix
@@ -1,5 +1,4 @@
-{ config
-, pkgs
+{ pkgs
 , inputs
 , ...
 }: {
@@ -12,6 +11,23 @@
     signing = {
       key = "3916387439FCDA33";
       signByDefault = false;
+    };
+
+    aliases = {
+      root = "rev-parse --show-toplevel";
+      # $1 = which branch to merge into
+      # $N = extra arguments
+      # leading exclamation mark(!) makes git run shell commands
+      mergepush = builtins.replaceStrings ["\n"] [" "] ''
+        !test $# -gt 0 || exit 1;
+        git push
+        ''${@:2}
+        -o merge_request.create
+        -o merge_request.target=$1
+        -o merge_request.merge_when_pipeline_succeeds
+        -o merge_request.remove_source_branch
+        #
+      ''; # The final hash prevents git from appending arguments to the command
     };
 
     extraConfig = {
