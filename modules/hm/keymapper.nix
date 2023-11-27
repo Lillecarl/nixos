17f8fe10ee1a50fb459bbe1241e013ed6a11c166
Carl Hjerpe <git@hjerpe.xyz>
2023-11-27 11:34:51 +0100

    Implement HM module for keymapper

---
+++ modules/hm/keymapper.nix
@@ -0,0 +1,50 @@
+{ config
+, lib
+, pkgs
+, ... }:
+let
+  cfg = config.services.keymapper;
+in
+{
+  options = {
+    services.keymapper = {
+      enable = lib.mkEnableOption "keymapper";
+
+      systemdTarget = lib.mkOption {
+        type = lib.types.str;
+        default = "graphical-session.target";
+        example = "sway-session.target";
+        description = ''
+          Systemd target to bind to.
+        '';
+      };
+
+      extraConfig = lib.mkOption {
+        type = lib.types.str;
+        default = "";
+        description = "Keymapper configuration";
+      };
+    };
+  };
+
+  config = {
+    systemd.user.services.keymapper = {
+      Unit = {
+        Description = "Keymapper";
+      };
+      Service = {
+        ExecStart = "${pkgs.keymapper}/bin/keymapper -v -c ${config.xdg.configHome + "/keymapper/keymapper.conf"}";
+        Restart = "always";
+        RestartSec = "5";
+      };
+      Install = {
+        WantedBy = [ cfg.systemdTarget ];
+      };
+    };
+
+    xdg.configFile."keymapper/keymapper.conf".text = cfg.extraConfig;
+    #xdg.configFile."keymapper/keymapper.conf".text = ''
+    #  ${lib.concatStringsSep "\n" cfg.extraConfig}
+    #'';
+  };
+}
