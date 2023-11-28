a44b5c5865736f95baee1ae2e06975c5b5b1690b
Carl Hjerpe <git@hjerpe.xyz>
2023-11-28 17:52:35 +0100

    Add keyutils for qutebrowser

--- lillecarl/gui/qutebrowser.nix
+++ lillecarl/gui/qutebrowser.nix
@@ -1,5 +1,19 @@
-{ ... }: {
+{ pkgs
+, ...
+}:
+{
+  home.packages = [
+    pkgs.keyutils
+  ];
   programs.qutebrowser = {
     enable = true;
+
+    package = pkgs.qutebrowser.overrideAttrs (oldAttrs: {
+      propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+      ];
+      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+        pkgs.keyutils
+      ];
+    });
   };
 }
