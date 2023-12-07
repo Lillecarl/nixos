fcb60a8eb8dd427dd8b430f3863184d1033e926f
Carl Hjerpe <git@hjerpe.xyz>
2023-12-07 02:33:30 +0100

    Remove packages
    I don't use xonsh anymore and I don't think I will. It's too buggy.
    ExaBGP is packages in nixpkgs now.
    pyzmq was only used for Salt, which I don't work with anymore.

--- pkgs/pkgs.nix
+++ pkgs/pkgs.nix
@@ -2,22 +2,6 @@ final: prev: flake:
 let
   # python3 packages
   python3Packages = {
-    xonsh-direnv = prev.callPackage ../pkgs/python3Packages/xonsh-direnv { };
-    xontrib-abbrevs = prev.callPackage ../pkgs/python3Packages/xontrib-abbrevs { };
-    xontrib-back2dir = prev.callPackage ../pkgs/python3Packages/xontrib-back2dir { };
-    xontrib-argcomplete = prev.callPackage ../pkgs/python3Packages/xontrib-argcomplete { };
-    xontrib-autoxsh = prev.callPackage ../pkgs/python3Packages/xontrib-autoxsh { };
-    xontrib-fzf-widgets = prev.callPackage ../pkgs/python3Packages/xontrib-fzf-widgets { };
-    xontrib-jump-to-dir = prev.callPackage ../pkgs/python3Packages/xontrib-jump-to-dir { };
-    xontrib-onepath = prev.callPackage ../pkgs/python3Packages/xontrib-onepath { };
-    xontrib-output-search = prev.callPackage ../pkgs/python3Packages/xontrib-output-search { };
-    xontrib-sh = prev.callPackage ../pkgs/python3Packages/xontrib-sh { };
-    tokenize-output = prev.callPackage ../pkgs/python3Packages/tokenize-output { };
-    lazyasd = prev.callPackage ../pkgs/python3Packages/lazyasd { };
-
-    exabgp = prev.callPackage ../pkgs/python3Packages/exabgp { };
-
-    from_ssv = prev.python3Packages.callPackage ../pkgs/python3Packages/from_ssv { };
     pyping = prev.python3Packages.callPackage ../pkgs/python3Packages/pyping { };
   };
   nodePackages = prev.callPackages ./node-packages { };
@@ -35,9 +19,6 @@ prev.lib.filterAttrs
     # Filter out package sets if we're called from a flake.
     (n != "python3Packages" && n != "nodePackages" && n != "firefoxAddons" && n != "grafanaPlugins"))
   {
-    xonsh-joined = prev.callPackage ../pkgs/xonsh-joined { };
-    xonsh-wrapper = final.callPackage ../pkgs/xonsh-wrapper { };
-
     # Inject python3 packages
     python3Packages = python3Packages // prev.python3Packages;
     python3 = prev.python3.override {
@@ -49,32 +30,6 @@ prev.lib.filterAttrs
     grafanaPlugins = grafanaPlugins // prev.grafanaPlugins;
     # firefox addons
     firefoxAddons = prev.callPackage ./firefoxAddons { };
-
-    obs-studio_beta = prev.obs-studio.overrideAttrs (finalAttrs: previousAttrs: rec {
-      version = "30.0.0-beta2";
-      src = prev.fetchFromGitHub {
-        owner = "obsproject";
-        repo = "obs-studio";
-        rev = version;
-        sha256 = "sha256-OhsPKLNzA88PecIduB8GsxvyzRwIrdxYQbJVJIspfuQ=";
-        fetchSubmodules = true;
-      };
-
-      patches = [
-        (builtins.fetchurl {
-          url = "https://raw.githubusercontent.com/NixOS/nixpkgs/2f9286912cb215969ece465147badf6d07aa43fe/pkgs/applications/video/obs-studio/fix-nix-plugin-path.patch";
-          sha256 = "sha256:0dn1lrw77f3322bizagdpxh79ars53nj6yardw2fhdzgk50fcjna";
-        })
-      ];
-
-      buildInputs = previousAttrs.buildInputs ++ [
-        prev.libdatachannel
-      ];
-
-      cmakeFlags = previousAttrs.cmakeFlags ++ [
-        "-DENABLE_QSV11=OFF"
-      ];
-    });
 
     terraform_1_5_5 = prev.mkTerraform {
       version = "1.5.5";
