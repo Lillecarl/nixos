18e08ae4f76758ff52d23abd52d47f81d22fa49e
Carl Hjerpe <git@hjerpe.xyz>
2023-12-11 15:43:06 +0100

    keymapper: monitor script to restart hung proc
    Noticed that keymapper somehow was stuck in some infinite loop, I'm
    guessing the read loop got into a state where it werent blocking reads.

--- common/keymapper.nix
+++ common/keymapper.nix
@@ -1,4 +1,7 @@
-{ pkgs, ... }:
+{ pkgs
+, config
+, ...
+}:
 {
   services.keymapper = {
     enable = true;
@@ -16,4 +19,39 @@
       ACTION=="remove", KERNEL=="event*", ATTRS{name}=="Keymapper", RUN+="${coreutils}/bin/unlink /dev/input/keymapper_kb"
       ACTION=="remove", KERNEL=="mouse*", ATTRS{name}=="Keymapper", RUN+="${coreutils}/bin/unlink /dev/input/keymapper_mouse"
     '';
+
+  # Found that keymapperd can get stuck in a loop and eat up CPU
+  systemd = {
+    timers.keymapper-mon = {
+      enable = true;
+      wantedBy = ["keymapper.service"];
+
+      timerConfig = {
+        OnCalendar = "*-*-* *:*:00";
+        AccuracySec = "60";
+        Persistent = true;
+      };
+
+    };
+
+    services.keymapper-mon = {
+      enable = true;
+      wantedBy = ["keymapper.service"];
+
+      path = [
+        config.systemd.package
+        pkgs.procps
+        pkgs.bc
+        pkgs.coreutils
+        pkgs.gawk
+      ];
+
+      script = ''
+        cpu=$(ps -p $(pgrep keymapperd) -u --no-headers | awk '{ print $3 }' | bc)
+        if test $cpu -gt 10; then
+          systemctl restart keymapperd
+        fi
+      '';
+    };
+  };
 }
