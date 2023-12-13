e9a0f8c0bcb1e427c9df559abab9002ffe50d857
Carl Hjerpe <git@hjerpe.xyz>
2023-12-14 00:16:27 +0100

    wezterm: implement linked config
    Increases velocity and yada yaa whatever

---
+++ lillecarl/dotfiles/.config/wezterm/linked.lua
@@ -0,0 +1,27 @@
+local wezterm = require('wezterm');
+local act = wezterm.action
+
+return {
+  check_for_updates = false,
+  window_close_confirmation = 'NeverPrompt',
+  hide_mouse_cursor_when_typing = false,
+  hide_tab_bar_if_only_one_tab = true,
+  term = "wezterm",
+  enable_wayland = true,
+  set_environment_variables = {
+    WSLENV = 'TERMINFO_DIRS',
+  },
+  disable_default_key_bindings = true,
+  keys = {
+    {
+      key = "x",
+      mods = "ALT|SHIFT",
+      action = act.ActivateCopyMode,
+    },
+    {
+      key = "x",
+      mods = "ALT",
+      action = act.ActivateCopyMode,
+    },
+  },
+}
--- lillecarl/gui/wezterm.nix
+++ lillecarl/gui/wezterm.nix
@@ -15,42 +15,16 @@
 
     extraConfig = ''
       -- Indent fixer
-          local act = wezterm.action
 
-          return {
-            check_for_updates = false,
-            window_close_confirmation = 'NeverPrompt',
-            hide_mouse_cursor_when_typing = false,
-            hide_tab_bar_if_only_one_tab = true,
+          local nix_config = wezterm.config_builder()
+          nix_config = {
             default_prog = { '${bp pkgs.fish}' },
-            term = "wezterm",
-            enable_wayland = true,
-            set_environment_variables = {
-              WSLENV = 'TERMINFO_DIRS',
-            },
-            keys = {--[[
-              {
-                key = "h",
-                mods = "ALT",
-                action = act.ActivatePaneDirection 'Left',
-              },
-              {
-                key = "l",
-                mods = "ALT",
-                action = act.ActivatePaneDirection 'Right',
-              },
-              {
-                key = "k",
-                mods = "ALT",
-                action = act.ActivatePaneDirection 'Up',
-              },
-              {
-                key = "j",
-                mods = "ALT",
-                action = act.ActivatePaneDirection 'Down',
-              },--]]
-            },
           }
+          for key, value in pairs(require('linked')) do
+            nix_config[key] = value
+          end
+
+          return nix_config
     '';
   };
 }
