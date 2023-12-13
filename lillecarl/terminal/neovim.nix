e59829f70bea3ade1305b250d59b602203f0d5a1
Carl Hjerpe <git@hjerpe.xyz>
2023-12-14 00:42:00 +0100

    neovim: remove CoC

--- lillecarl/terminal/neovim.nix
+++ lillecarl/terminal/neovim.nix
@@ -43,35 +43,6 @@
       which-key-nvim
     ];
 
-    coc = {
-      enable = false;
-
-      settings = {
-        languageserver = {
-          nix = {
-            command = bp pkgs.nil;
-            filetypes = [ "nix" ];
-            rootPatterns = [ "flake.nix" ];
-            settings = {
-              nil = {
-                formatting = { command = [ (bp pkgs.nixpkgs-fmt) ]; };
-              };
-            };
-          };
-          terraform = {
-            command = bp pkgs.terraform-lsp;
-            filetypes = [ "terraform" ];
-            initializationOptions = { };
-          };
-          clangd = {
-            command = "${pkgs.clang-tools}/bin/clangd";
-            rootPatterns = [ "compile_flags.txt" "compile_commands.json" ];
-            filetypes = [ "c" "cc" "cpp" "c++" "objc" "objcpp" "h" "hpp" ];
-          };
-        };
-      };
-    };
-
     extraLuaConfig = ''${"\n"}
       require('user_config')
       require('catppuccin_config')
@@ -80,7 +51,6 @@
       require('telescope_config')
       require('toggleterm_config')
       require('lsp_config')
-      --require('coc_config')
     '';
   };
 }
