0bdd8bd44dce09e8e0362e6d9d1a91f5f673d33d
Carl Hjerpe <git@hjerpe.xyz>
2023-12-14 05:43:26 +0100

    neovim: move terminal config to toggleterm

--- lillecarl/dotfiles/.config/nvim/lua/toggleterm_config.lua
+++ lillecarl/dotfiles/.config/nvim/lua/toggleterm_config.lua
@@ -1,2 +1,27 @@
+local wk = require('which-key')
+
 require("toggleterm").setup({
 })
+
+-- Allow leaving terminal mode with <C-\>
+wk.register({
+  --["<C-h>"] = { [[<C-\><C-N><C-w>h]], "Terminal Left" },
+  --["<C-j>"] = { [[<C-\><C-N><C-w>j]], "Terminal Down" },
+  --["<C-k>"] = { [[<C-\><C-N><C-w>k]], "Terminal Up" },
+  --["<C-l>"] = { [[<C-\><C-N><C-w>l]], "Terminal Right" },
+  ["<C-\\>"] = { [[<C-\><C-N>]], "Terminal unfocus" },
+},
+{
+  mode = "t",
+})
+
+-- Disable line numbers and start insert mode when opening a terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  once = true,
+  callback = function(args)
+    vim.o.number = false
+    vim.o.relativenumber = false
+    vim.cmd('startinsert')
+  end,
+})
+
--- lillecarl/dotfiles/.config/nvim/lua/user_config.lua
+++ lillecarl/dotfiles/.config/nvim/lua/user_config.lua
@@ -62,27 +62,5 @@ end
 require('showmaps')
 wk.register({["\\m"] = { "<cmd>ShowMaps<cr>", "Show Maps" }}, { })
 
--- Allow leaving terminal mode with <C-\>
-wk.register({
-  --["<C-h>"] = { [[<C-\><C-N><C-w>h]], "Terminal Left" },
-  --["<C-j>"] = { [[<C-\><C-N><C-w>j]], "Terminal Down" },
-  --["<C-k>"] = { [[<C-\><C-N><C-w>k]], "Terminal Up" },
-  --["<C-l>"] = { [[<C-\><C-N><C-w>l]], "Terminal Right" },
-  ["<C-\\>"] = { [[<C-\><C-N>]], "Terminal unfocus" },
-},
-{
-  mode = "t",
-})
-
--- Disable line numbers and start insert mode when opening a terminal
-vim.api.nvim_create_autocmd('TermOpen', {
-  once = true,
-  callback = function(args)
-    vim.o.number = false
-    vim.o.relativenumber = false
-    vim.cmd('startinsert')
-  end,
-})
-
 -- indent blanklines
 require("ibl").setup()
