41e0fec86f9d9615db0185db4e020aafe8e43d2a
Carl Hjerpe <git@hjerpe.xyz>
2023-12-18 10:36:23 +0100

    neovim: try LazyVim
    I'm lazy.....ish.
    
    TODO: Look into a more integrated solution with HM and LazyVim.

---
+++ lillecarl/dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+      ---@type lspconfig.options
+      servers = {
+        terraformls = {},
+      },
+  },
+}
--- lillecarl/terminal/neovim.nix
+++ lillecarl/terminal/neovim.nix
@@ -19,6 +19,7 @@
       pkgs.fd # for telescope
       pkgs.ripgrep # for telescope
       pkgs.tree-sitter # silence tree-sitter warning
+      pkgs.fzf # fuzzy finder
     ] ++ [
       # lsp servers
       pkgs.clang-tools # C, C++
@@ -34,30 +35,33 @@
     ];
 
     plugins = with pkgs.vimPlugins; [
-      camelcasemotion
-      catppuccin-nvim
-      cmp-nvim-lsp # LSP source for cmp
-      copilot-lua
-      formatter-nvim
-      fugitive
-      indent-blankline-nvim
-      neodev-nvim
-      neogit
-      nvim-cmp
-      nvim-lspconfig
-      nvim-snippy
-      nvim-tree-lua
-      nvim-treesitter.withAllGrammars
-      surround
-      tabby-nvim
-      tabmerge
-      telescope-nvim
-      toggleterm-nvim
-      vim-airline
-      which-key-nvim
+      #camelcasemotion
+      #catppuccin-nvim
+      #cmp-nvim-lsp # LSP source for cmp
+      #copilot-lua
+      #formatter-nvim
+      #fugitive
+      #indent-blankline-nvim
+      #neodev-nvim
+      #neogit
+      #nvim-cmp
+      #nvim-lspconfig
+      #nvim-snippy
+      #nvim-tree-lua
+      #nvim-treesitter.withAllGrammars
+      #surround
+      #tabby-nvim
+      #tabmerge
+      #telescope-nvim
+      #toggleterm-nvim
+      #vim-airline
+      #which-key-nvim
+      #LazyVim
+      lazy-nvim
     ];
 
-    extraLuaConfig = /* lua */ ''${"\n"}
+    extraLuaConfig = /* lua */ ''
+      ${"\n"}--[[
       require('user_config')
       require('catppuccin_config')
       require('copilot_config')
@@ -68,6 +72,8 @@
       require('nvim-tree_config')
       require('formatter_config')
       require('treesitter_config')
+      require("config.lazy").setup('${pkgs.vimPlugins.LazyVim}')--]]
+      require("config.lazy")
     '';
   };
 }
