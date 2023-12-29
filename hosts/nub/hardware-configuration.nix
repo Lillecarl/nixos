6c12013c6c1a87a57d81e73ae8d63129f3407461
Carl Hjerpe <git@hjerpe.xyz>
2023-12-29 11:56:30 +0100

    power: enable more GPU tunables

--- hosts/nub/hardware-configuration.nix
+++ hosts/nub/hardware-configuration.nix
@@ -22,7 +22,21 @@
     kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
     extraModprobeConfig = "options kvm_amd nested=1";
-    kernelParams = [ "amd_pstate=guided" "iommu=soft" ];
+    kernelParams = [
+      "amd_pstate=guided"
+      "iommu=soft"
+      # Exposes all the available tunables to userspace as r/w rather than r/o.
+      # https://community.frame.work/t/tracking-ppd-v-tlp-for-amd-ryzen-7040/39423/29
+      "amdgpu.ppfeaturemask=0xffffffff"
+      #"amd_pstate=active"
+      #"amdgpu.ppfeaturemask=0xffffffff"
+      #"amdgpu.sg_display=0"
+      #"cpufreq.default_governor=powersave"
+      #"initcall_blacklist=cpufreq_gov_userspace_init,cpufreq_gov_performance_init"
+      #"pcie_aspm=force"
+      #"pc"
+      #"ie_aspm.policy=powersupersave"
+    ];
 
     loader.systemd-boot.enable = lib.mkForce false;
 
