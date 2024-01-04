b7d6e520cd5e33f6d3428ba03b9aea1d2743ab15
Carl Hjerpe <git@hjerpe.xyz>
2024-01-04 15:59:19 +0100

    home: add signal-desktop

--- lillecarl/gui/packages.nix
+++ lillecarl/gui/packages.nix
@@ -11,5 +11,6 @@
     vlc # VLC sucks in comparision to MPV
     wev # Like xev but for wayland, displays which keys you're pressing
     gitg # Git GUI for viewing branches
+    signal-desktop # Signal messenger, useful to copy-paste stuff to your phone
   ];
 }
