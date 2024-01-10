c94b8c9c9014a42de37fb55660632645321e8ae1
Carl Hjerpe <git@hjerpe.xyz>
2024-01-10 12:55:28 +0100

    fancontrol: simple off only

--- scripts/fancontrol2.py
+++ scripts/fancontrol2.py
@@ -2,23 +2,18 @@
 
 import json
 import re
-import sys
-import atexit
+from time import sleep
 
-from time import sleep, time
 from plumbum import local
 from psutil import cpu_percent
 
 sensors = local["sensors"]["-j"]
-
-hwmon_root = local.path("/sys/bus/platform/drivers/thinkpad_hwmon/thinkpad_hwmon/hwmon/hwmon1")
-hwmon_path = hwmon_root / "pwm1"
-
 fanpath = local.path("/proc/acpi/ibm/fan")
 
-def setwatchdog(sec: int):
-    print(f"setting watchdog to {sec}")
+
+def setwatchdog(sec: int = 120):
     fanpath.write(f"watchdog {sec}")
+
 
 def getlevel():
     faninfo: str = fanpath.read()
@@ -41,7 +36,7 @@ def getlevel():
     return 0
 
 
-def setlevel(level: int, force: bool = False):
+def setlevel(level: int):
     curlevel = getlevel()
 
     if level > 7:
@@ -49,63 +44,44 @@ def setlevel(level: int, force: bool = False):
     elif level < 0:
         level = 0
 
-    if curlevel > 1 and level == 0:
-        level = 1
-
-    if level == 0 and getspeed() == 0 and not force:
-        return False
-
-    if level == curlevel and not force:
-        return False
-
-    if level > 0:
+    if level != curlevel:
         print(f"setting fanlevel to {level}")
-        fanpath.write(f"level {level}")
-    else:
-        u8level = int((255 / 7) * level)
-        print(f"setting pwmlevel to {u8level}")
-        hwmon_path.write(str(u8level))
+    fanpath.write(f"level {level}")
 
-    return True
+
+def setauto():
+    fanpath.write(f"level auto")
+
 
 def gettemp():
     sensordata: dict[str, dict] = json.loads(sensors())
     return float(sensordata["thinkpad-isa-0000"]["CPU"]["temp1_input"])
 
+
 def getspeed():
     sensordata: dict[str, dict] = json.loads(sensors())
     return float(sensordata["thinkpad-isa-0000"]["fan1"]["fan1_input"])
 
+
 def setfan(cpu_avg: float):
-    level = getlevel()
     temp = gettemp()
 
     print("cpu_avg: {}".format(cpu_avg))
 
-    # Reset watchdog, we're in auto and must set a level to
-    # get into manual mode
-    if getlevel() < 0 and getspeed() > 0:
-        setlevel(1)
-        return 1
-
-    if temp < 55:
-        return setlevel(0)
-    elif temp > 55 and cpu_avg > 20:
-        return setlevel(level + 1)
-    elif temp > 65:
-        return setlevel(level + 1)
-    elif cpu_avg < 3:
-        return setlevel(level - 2)
+    if temp < 60:
+        setlevel(0)
     else:
-        return setlevel(level - 1)
+        setauto()
+
 
 def main():
-    setwatchdog(120)
-    setlevel(0, True)
+    interval: int = 5
 
     while True:
-        sleep(5)
-        setfan(cpu_percent(5))
+        setwatchdog(interval + 1)
+        setfan(cpu_percent(interval))
+        sleep(interval)
+
 
 if __name__ == "__main__":
-    exit(main())
+    main()
