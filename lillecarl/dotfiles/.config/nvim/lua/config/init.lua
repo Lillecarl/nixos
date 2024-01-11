8b7e59bc30701657c6737d2ca8e0bc517fdc9292
Carl Hjerpe <git@hjerpe.xyz>
2024-01-11 15:25:52 +0100

    neovim: ignorecase and smartcase
    fmt

--- lillecarl/dotfiles/.config/nvim/lua/config/init.lua
+++ lillecarl/dotfiles/.config/nvim/lua/config/init.lua
@@ -3,9 +3,12 @@ vim.g.mapleader = " "
 -- Something from LazyVim
 vim.g.maplocalleader = "\\"
 -- Copy to system clipboard by default
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 -- no idea, from nvim-tree config example
 vim.opt.termguicolors = true
+-- case insesitive search when searching with only lowercase letters
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
