aa64e2a84e19856709b088be4953b6e14e8777ca
Carl Hjerpe <git@hjerpe.xyz>
2024-01-17 18:24:29 +0100

    neovim: don't load plugins from vscode

--- lillecarl/dotfiles/.config/nvim/lua/plugins/init.lua
+++ lillecarl/dotfiles/.config/nvim/lua/plugins/init.lua
@@ -3,10 +3,10 @@ local M = {}
 function M.setup(_)
   if not vim.g.vscode then
     require("plugins.multiplug")
+    require("plugins.tree-sitter")
+    require("plugins.lspconfig")
+    require("plugins.cmp")
   end
-  require("plugins.tree-sitter")
-  require("plugins.lspconfig")
-  require("plugins.cmp")
 end
 
 return M
