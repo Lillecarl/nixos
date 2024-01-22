84bb62c27927e3eb14e9e705d209af3971316bcb
Carl Hjerpe <git@hjerpe.xyz>
2024-01-22 10:46:58 +0100

    neovim: init neoconf and neodev

--- lillecarl/dotfiles/.config/nvim/lua/plugins/multiplug.lua
+++ lillecarl/dotfiles/.config/nvim/lua/plugins/multiplug.lua
@@ -16,9 +16,20 @@ require("catppuccin").setup({
 })
 vim.cmd.colorscheme("catppuccin")
 
+
+require("neoconf").setup({
+})
 -- neodev
 -- https://github.com/folke/neodev.nvim
-require("neodev").setup({})
+require("neodev").setup({
+  pathStrict = false,
+  library = {
+    enabled = true,
+    runtime = true,
+    types = true,
+    plugins = true,
+  }
+})
 
 -- nvim-tree, tree plugin
 -- https://github.com/nvim-tree/nvim-tree.lua
@@ -56,11 +67,11 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
   },
 })
 
