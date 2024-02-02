1726f4deb073be76aaf325f8e64e683f66805609
Carl Hjerpe <git@hjerpe.xyz>
2024-02-02 01:49:48 +0100

    fish: bind for editing buffer in neovim

--- lillecarl/dotfiles/.config/fish/conf.d/linked.fish
+++ lillecarl/dotfiles/.config/fish/conf.d/linked.fish
@@ -50,6 +50,9 @@ bind \ce -M insert end-of-buffer
 bind \ce -M visual end-of-buffer
 # bind alt+l to complete whatever fish is suggesting
 bind -M insert \el forward-char
+# bind ctrl+shift+e to edit command buffer
+bind -M insert \e\[101\;6u edit_command_buffer
+bind -M visual \e\[101\;6u edit_command_buffer
 
 # function that is called on fish_preexec event (before executing something)
 function reload_awscreds -e fish_preexec
