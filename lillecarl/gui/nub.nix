d1002b4426f7c20c9113cbc8e96b6f1a7943d3a9
Carl Hjerpe <git@hjerpe.xyz>
2024-03-06 17:18:31 +0100

    hyprland: identify monitor by description

--- lillecarl/gui/nub.nix
+++ lillecarl/gui/nub.nix
@@ -15,7 +15,7 @@
           # Work external display
           monitor=desc:Dell Inc. DELL U3421WE HQDH753,3440x1440@60,0x0,1.0
           # Home external display
-          monitor=HDMI-A-1,2560x1440@144,0x0,1.0
+          monitor=desc:Huawei Technologies Co. Inc. XWU-CBA 0x00000001,2560x1440@144,0x0,1.0
           # Laptop integrated display
           monitor=desc:AU Optronics 0xFA9B,1920x1200@60,760x1440,1.0
         '';
