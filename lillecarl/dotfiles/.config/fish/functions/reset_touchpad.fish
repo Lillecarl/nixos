41504dbcc6a3dcf737367f66358b856fb36ae1a4
Carl Hjerpe <git@hjerpe.xyz>
2024-03-15 13:40:16 +0100

    fish: better touchpad reset function

--- lillecarl/dotfiles/.config/fish/functions/reset_touchpad.fish
+++ lillecarl/dotfiles/.config/fish/functions/reset_touchpad.fish
@@ -1,6 +1,7 @@
 function reset_touchpad
-    sudo rmmod i2c_hid_acpi
-    sudo rmmod i2c_hid
-    sudo rmmod i2c_dev
+    sudo modprobe -r i2c_hid_acpi
+    sudo modprobe -r i2c_hid
+    sudo modprobe -r i2c_dev
     sudo modprobe i2c_hid_acpi
+    sudo modprobe i2c-dev
 end
