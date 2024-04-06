f24269b11718005c7748e3bf4d01644a74f15b7b
Carl Hjerpe <git@hjerpe.xyz>
2024-04-06 04:24:25 +0200

    Remove old monitor config

--- lillecarl/flake-module.nix
+++ lillecarl/flake-module.nix
@@ -44,13 +44,6 @@ in
             ./gui/shitbox.nix
           ];
           extraSpecialArgs = {
-            monitorConfig = ''
-              monitor=DP-1,2560x1440@164.802002,1080x240,1.0
-              monitor=DP-2,1920x1080@143.996002,0x0,1.0
-              monitor=DP-2,transform,3
-              monitor=DP-3,1920x1080@143.996002,3640x0,1.0
-              monitor=DP-3,transform,1
-            '';
             nixosConfig = self.nixosConfigurations.shitbox.config;
           };
         });
@@ -60,14 +53,6 @@ in
             ./gui/nub.nix
           ];
           extraSpecialArgs = {
-            monitorConfig = ''
-              # Work external display
-              monitor=DP-1,3440x1440@60,0x0,1.0
-              # Home external display
-              monitor=HDMI-A-1,2560x1440@144,0x0,1.0
-              # Laptop integrated display
-              monitor=eDP-1,1920x1200@60,760x1440,1.0
-            '';
             nixosConfig = self.nixosConfigurations.nub.config;
           };
         });
