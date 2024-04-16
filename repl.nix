a37fe1dc0b4ca737ae8817b86960e48f236d168e
Carl Hjerpe <git@hjerpe.xyz>
2024-04-16 14:39:35 +0200

    nix/repl: initialize pkgs based off $HOST

--- repl.nix
+++ repl.nix
@@ -1,13 +1,28 @@
 let
+  host = builtins.getEnv "HOST";
   flake = builtins.getFlake (toString ./.);
-  pkgs = import <nixpkgs> { };
+  inherit (flake.nixosConfigurations) nub;
+  inherit (flake.nixosConfigurations) shitbox;
+  pkgs =
+    if
+      host == "nub" then nub.pkgs
+    else if
+      host == "shitbox" then shitbox.pkgs
+    else
+      throw "Unknown host ${host}";
   inherit (pkgs) lib;
   inherit (flake) nixosConfigurations;
 in
 {
-  inherit flake pkgs lib nixosConfigurations;
-  inherit (flake.nixosConfigurations) nub;
-  inherit (flake.nixosConfigurations) shitbox;
+  inherit
+    flake
+    pkgs
+    lib
+    nixosConfigurations
+    nub
+    shitbox
+    host;
+
   nub_home = flake.homeConfigurations."lillecarl@nub";
   shitbox_home = flake.homeConfigurations."lillecarl@shitbox";
 }
