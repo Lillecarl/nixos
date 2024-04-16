aef1ca4f0e465de470f8b98047747b1ccc9f8653
Carl Hjerpe <git@hjerpe.xyz>
2024-04-16 13:20:31 +0200

    qutebrowser: config import exception handling

--- lillecarl/dotfiles/.config/qutebrowser/linked.py
+++ lillecarl/dotfiles/.config/qutebrowser/linked.py
@@ -1,6 +1,16 @@
 #! /usr/bin/env python3
+
 c = c  # type: ignore
 config = config # type: ignore
+
+try:
+    from qutebrowser.config.configfiles import ConfigAPI
+    from qutebrowser.config.config import ConfigContainer
+    config: ConfigAPI = config
+    c: ConfigContainer = c
+except:
+    pass
+
 
 c.editor.command = ["kitty", "vim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
 c.tabs.position = "left"
