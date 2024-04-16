d83700552e25f7feddd0a430c6732a8ec93898d3
Carl Hjerpe <git@hjerpe.xyz>
2024-04-16 12:22:13 +0200

    qutebrowser: configuration
    Configure editor command
    Move tabs to the left
    Bind zoom in and out

--- lillecarl/dotfiles/.config/qutebrowser/linked.py
+++ lillecarl/dotfiles/.config/qutebrowser/linked.py
@@ -1,3 +1,9 @@
 #! /usr/bin/env python3
 c = c  # type: ignore
 config = config # type: ignore
+
+c.editor.command = ["kitty", "vim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
+c.tabs.position = "left"
+
+config.bind("<ctrl-j>", "zoom-out")
+config.bind("<ctrl-k>", "zoom-in")
