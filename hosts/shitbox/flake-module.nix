6d37c1a8b310ff89f1c7f275b50b5221976281c1
Carl Hjerpe <git@hjerpe.xyz>
2024-04-17 16:02:42 +0200

    nix: flake refactoring step one of N

--- hosts/shitbox/flake-module.nix
+++ hosts/shitbox/flake-module.nix
@@ -1,47 +1,47 @@
-{ inputs
-, flakeloc
-, self
-, pkgs
+{ self
+, inputs
+, withSystem
 , ...
-}:
+}@top:
 let
   system = "x86_64-linux";
 in
 {
   flake = {
-    nixosConfigurations.shitbox = inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      pkgs = pkgs.${system};
-      modules = [
-        ../../common
-        ../../common/acme.nix
-        ../../common/fish.nix
-        ../../common/hyprland.nix
-        ../../common/nix.nix
-        ../../common/stylix.nix
-        ../../common/users.nix
-        ../../common/verycommon.nix
-        ../../common/xdg.nix
-        ./default.nix
-        inputs.agenix.nixosModules.default
-        inputs.disko.nixosModules.disko
-        inputs.flake-programs-sqlite.nixosModules.programs-sqlite
-        inputs.niri.nixosModules.niri
-        inputs.nixos-hardware.nixosModules.common-cpu-amd
-        inputs.nixos-hardware.nixosModules.common-gpu-intel
-        inputs.nixos-hardware.nixosModules.common-pc
-        inputs.nixos-hardware.nixosModules.common-pc-ssd
-        inputs.stylix.nixosModules.stylix
-        ({ pkgs, ... }: {
-          programs.niri = {
-            enable = true;
-            package = pkgs.niri-unstable;
+    nixosConfigurations.shitbox =
+      withSystem system ({ pkgs, flakeloc, ... }@ctx:
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+          modules = [
+            ../../common
+            ../../common/acme.nix
+            ../../common/fish.nix
+            ../../common/hyprland.nix
+            ../../common/nix.nix
+            ../../common/stylix.nix
+            ../../common/users.nix
+            ../../common/verycommon.nix
+            ../../common/xdg.nix
+            ./default.nix
+            inputs.agenix.nixosModules.default
+            inputs.disko.nixosModules.disko
+            inputs.flake-programs-sqlite.nixosModules.programs-sqlite
+            inputs.niri.nixosModules.niri
+            inputs.nixos-hardware.nixosModules.common-cpu-amd
+            inputs.nixos-hardware.nixosModules.common-gpu-intel
+            inputs.nixos-hardware.nixosModules.common-pc
+            inputs.nixos-hardware.nixosModules.common-pc-ssd
+            inputs.stylix.nixosModules.stylix
+            ({ pkgs, ... }: {
+              programs.niri = {
+                enable = true;
+                package = pkgs.niri-unstable;
+              };
+            })
+          ];
+          specialArgs = {
+            inherit inputs flakeloc self;
           };
-        })
-      ];
-      specialArgs = {
-        inherit inputs self flakeloc;
-      };
-    };
+        });
   };
 }
