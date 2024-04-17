a5228ad050f807d0d3d62d54fa6fb590e0aee2a1
Carl Hjerpe <git@hjerpe.xyz>
2024-04-17 07:02:01 +0200

    rimport: rewrite

--- lib/default.nix
+++ lib/default.nix
@@ -2,39 +2,21 @@ lib:
 let
   bs = builtins;
 in
-{
-  rimport = sources: regexes:
+rec {
+  trace = trace: bs.trace trace trace;
+  # sources: a path or a list of paths
+  # regexes: a regex or a list of regexes
+  rimport1 = sources: regexes:
     let
-      regexList =
-        let
-          list = lib.toList regexes;
-        in
-        lib.throwIf (lib.any (regex: bs.typeOf regex != "string") list) "regexes must be a string or a list of string" list;
-    in
-    lib.pipe (lib.toList sources) [
-      # Make sure consumers don't stuff the wrong types
-      (sourcePaths: lib.throwIf (lib.any (path: bs.typeOf path != "path") sourcePaths) "sources must be path or list of path" sourcePaths)
-      # Bring all .nix files into the store
-      (sourcePaths: bs.map (path: lib.sources.sourceFilesBySuffices path [ ".nix" ]) sourcePaths)
-      # Get all "file store paths" from the folder store paths
-      (storePaths: bs.map (path: lib.filesystem.listFilesRecursive path) storePaths)
-      # Flatten "list of list of file store path" to "list of file store path"
-      (storeFilesLists: lib.flatten storeFilesLists)
-      # Filter out possible duplicates
-      (files: lib.unique files)
-      # Convert paths to strings for regex matching
-      (files: bs.map (path: "${path}") files)
-      # Filter out files that don't match the regex
-      (files: lib.filter (path: lib.any (re: bs.match re path != null) regexList) files)
-    ];
+      #storeMatch = str: builtins.match "^(/nix/store/.*-source/).*$" str;
+      regexList = lib.toList regexes;
+      sourceList = lib.toList sources;
 
-  strimport = source: regexes:
-    lib.pipe source [
-      (source: bs.path {
-        path = source;
-        recursive = true;
-        filter = (path: type: bs.trace "${path} ${type}" (bs.match "^.*flake-module.nix$" "${path}" != null));
-      })
-      (path: lib.filesystem.listFilesRecursive path)
-    ];
+      modulePaths = lib.flatten (bs.map (path: (lib.filesystem.listFilesRecursive path)) sourceList);
+      nixFiltered = lib.filter (path: bs.match ".*\.nix" (builtins.toString path) != null) modulePaths;
+      userFiltered = lib.filter (path: lib.any (re: bs.match re (builtins.toString path) != null) regexList) nixFiltered;
+    in
+    userFiltered;
+
+  rimport = rimport1;
 }
