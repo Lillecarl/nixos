f293171c157607be3af355d3ab04dee24ed37e33
Carl Hjerpe <git@hjerpe.xyz>
2024-04-23 01:24:35 +0200

    rimport: add discard information to paths before filtering

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,6 @@
 { outPath
 , lib
+, ...
 }:
 let
   bs = builtins;
@@ -7,13 +8,18 @@ let
     let
       basePath = bs.toString outPath;
     in
+    # listFilesRecursive returns absolute store paths
     lib.pipe (lib.filesystem.listFilesRecursive basePath) [
       (paths: bs.map
         (path: {
+          # Remove store prefix
           noPrefix = lib.removePrefix basePath path;
+          # Path as string
           sPath = bs.toString path;
         })
         paths)
+      # Only Nix files
+      (paths: lib.filter (path: lib.hasSuffix ".nix" path.sPath) paths)
     ];
 in
 rec {
@@ -23,9 +29,14 @@ rec {
     let
       pathInfo =
         rec {
+          # Path where we look for files
           sPath = bs.toString path;
+          # Root of the flake, used to replace store paths with absolute "disk paths"
           sFlakeRoot = (lib.filesystem.locateDominatingFile "flake.nix" sPath).path;
+          inherit outPath;
         };
+
+      discardFilter = dpaths: dfilter: dname: bs.map (dpath: dpath // { discarded = dpath.discarded ++ (if dfilter dpath then [ dname ] else [ ]); }) dpaths;
 
       regadds = lib.toList regadd;
       regdels = lib.toList regdel;
@@ -33,17 +44,19 @@ rec {
       filteredFiles = lib.pipe all_files [
         (paths: bs.map
           (path: path // {
+            # Replace store path with absolute "disk path"
             newPrefix = pathInfo.sFlakeRoot + path.noPrefix;
+            discarded = [ ];
           })
           paths)
-        # Only Nix files
-        (paths: lib.filter (path: lib.hasSuffix ".nix" path.sPath) paths)
-        # Match path location
-        (paths: lib.filter (path: lib.hasPrefix pathInfo.sPath path.newPrefix) paths)
+        # Match absolute "disk path" location with files absolute "disk path" location
+        (paths: discardFilter paths (path: !lib.hasPrefix pathInfo.sPath path.newPrefix) "prefix")
         # Match positive regex
-        (paths: lib.filter (path: lib.any (re: bs.match re path.newPrefix != null) regadds) paths)
+        #(paths: bs.map (path: path // { discarded = (path.discarded || !lib.any (re: bs.match re path.newPrefix != null) regadds); }) paths)
+        (paths: discardFilter paths (path: lib.all (re: bs.match re path.newPrefix == null) regadds) "regadd")
         # Match negative regex
-        (paths: lib.filter (path: lib.all (re: bs.match re path.newPrefix == null) regdels) paths)
+        #(paths: bs.map (path: path // { discarded = (path.discarded || !lib.all (re: bs.match re path.newPrefix == null) regdels); }) paths)
+        (paths: discardFilter paths (path: lib.any (re: bs.match re path.newPrefix != null) regdels) "regdel")
       ];
     in
     {
@@ -73,6 +86,8 @@ rec {
       (res: bs.map (subres: subres.files) res)
       # Flatten the lists
       (res: lib.flatten res)
+      # Remove discarded files
+      (res: bs.filter (subres: bs.length subres.discarded == 0) res)
       # Extract the paths
       (res: bs.map (subres: subres.sPath) res)
     ];
