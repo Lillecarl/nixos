b082d6bc8231e3954b04ea45bb4688fe48ae8df0
Carl Hjerpe <git@hjerpe.xyz>
2024-04-23 03:08:49 +0200

    nix/hm/flake: remove old HM outputs

--- lillecarl/flake-module.nix
+++ lillecarl/flake-module.nix
@@ -1,117 +1,36 @@
 { self
 , inputs
 , withSystem
-, flakeloc
+#, flakeloc
 , __curPos ? __curPos
 , ...
 }:
-let
-  mkHome = system: customArgs:
-    withSystem system ({ pkgs, mpkgs, spkgs, ... }:
-      inputs.home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs =
-          {
-            inherit self inputs flakeloc mpkgs spkgs;
-          } // customArgs.extraSpecialArgs;
-
-        modules = [
-          inputs.agenix.homeManagerModules.default
-          inputs.stylix.homeManagerModules.stylix
-          "${self}/common/stylix.nix"
-          "${self}/modules/hm"
-          ./default.nix
-          ./secrets.nix
-          ./moduleOverrides.nix
-        ] ++ customArgs.modules;
-      });
-in
 {
   flake = {
     homeConfigurations =
       let
-        guibase = {
-          extraSpecialArgs = { };
-          modules = [
-            ./gui
-            ./terminal
-            inputs.nix-flatpak.homeManagerModules.nix-flatpak
-            inputs.niri.homeModules.niri
-          ];
-        };
+        workstation = excludeName:
+          withSystem "x86_64-linux" ({ pkgs, mpkgs, spkgs, flakeloc, ... }:
+            inputs.home-manager.lib.homeManagerConfiguration {
+              inherit pkgs;
+              extraSpecialArgs =
+                {
+                  inherit self inputs flakeloc mpkgs spkgs;
+                  nixosConfig = self.nixosConfigurations.shitbox.config;
+                };
+              modules = [
+                (self + "/stylix.nix")
+                inputs.agenix.homeManagerModules.default
+                inputs.niri.homeModules.niri
+                inputs.nix-flatpak.homeManagerModules.nix-flatpak
+                inputs.stylix.homeManagerModules.stylix
+              ]
+              ++ pkgs.lib.rimport { path = [ ./. ../modules/hm ]; regdel = [ __curPos.file ".*${excludeName}.*" ]; };
+            });
       in
       {
-        "lillecarl@shitbox_old" = mkHome "x86_64-linux" (guibase // {
-          modules = guibase.modules ++ [
-            ./gui/shitbox.nix
-          ];
-          extraSpecialArgs = {
-            nixosConfig = self.nixosConfigurations.shitbox.config;
-          };
-        });
-        "lillecarl@shitbox" = withSystem "x86_64-linux" ({ pkgs, mpkgs, spkgs, flakeloc, ... }:
-          inputs.home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs =
-              {
-                inherit self inputs flakeloc mpkgs spkgs;
-                nixosConfig = self.nixosConfigurations.shitbox.config;
-              };
-            modules = [
-              (self + "/stylix.nix")
-              inputs.agenix.homeManagerModules.default
-              inputs.niri.homeModules.niri
-              inputs.nix-flatpak.homeManagerModules.nix-flatpak
-              inputs.stylix.homeManagerModules.stylix
-            ]
-            ++ pkgs.lib.rimport { path = [ ./. ../modules/hm ]; regdel = [ __curPos.file ".*nub.*" ]; };
-          });
-        "lillecarl@nub_old" = mkHome "x86_64-linux" (guibase // {
-          modules = guibase.modules ++ [
-            "${self}/modules/hm"
-            ./gui/nub.nix
-          ];
-          extraSpecialArgs = {
-            nixosConfig = self.nixosConfigurations.nub.config;
-          };
-        });
-        "lillecarl@nub" = withSystem "x86_64-linux" ({ pkgs, mpkgs, spkgs, flakeloc, ... }:
-          inputs.home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            extraSpecialArgs =
-              {
-                inherit self inputs flakeloc mpkgs spkgs;
-                nixosConfig = self.nixosConfigurations.nub.config;
-              };
-            modules = [
-              (self + "/stylix.nix")
-              inputs.agenix.homeManagerModules.default
-              inputs.niri.homeModules.niri
-              inputs.nix-flatpak.homeManagerModules.nix-flatpak
-              inputs.stylix.homeManagerModules.stylix
-            ]
-            ++ pkgs.lib.rimport { path = [ ./. ../modules/hm ]; regdel = [ __curPos.file ".*shitbox.*" ]; };
-          });
-        "lillecarl@wsl" = mkHome "x86_64-linux" {
-          extraSpecialArgs = { };
-          modules = [
-            ./terminal
-          ];
-        };
-        "lillecarl@pi" = mkHome "aarch64-linux" {
-          extraSpecialArgs = {
-            nixosConfig = self.nixosConfigurations.pi.config;
-          };
-          modules = [
-            ./terminal
-          ];
-        };
-        lillecarl-term = mkHome "x86_64-linux" {
-          extraSpecialArgs = { };
-          modules = [
-            ./terminal
-          ];
-        };
+        "lillecarl@shitbox" = workstation "nub";
+        "lillecarl@nub" = workstation "shitbox";
       };
   };
 }
