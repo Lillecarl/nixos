8bd2ed91e288bb33516a58a52550a66b88897dbc
Carl Hjerpe <git@hjerpe.xyz>
2024-04-25 19:13:11 +0200

    nix/repl: Make repl nicer

--- repl.nix
+++ repl.nix
@@ -1,33 +1,27 @@
 let
   host = builtins.getEnv "HOST";
-  _flakePath = /home/lillecarl/Code/nixos;
-  flake = builtins.getFlake (toString _flakePath);
-  inherit (flake.nixosConfigurations) nub;
-  inherit (flake.nixosConfigurations) shitbox;
-  bs = builtins;
-  pkgs =
-    if
-      host == "nub" then nub.pkgs
-    else if
-      host == "shitbox" then shitbox.pkgs
-    else
-      throw "Unknown host ${host}";
-  inherit (pkgs) lib;
-  inherit (flake) nixosConfigurations;
+  user = builtins.getEnv "USER";
+  self = builtins.getFlake (toString ./.);
+
+  out = self // rec {
+    bs = builtins;
+
+    os = self.nixosConfigurations.${host};
+    home = self.homeConfigurations.${"${user}@${host}"};
+
+    pkgs = self.nixosConfigurations.${host}.pkgs;
+    config = self.nixosConfigurations.${host}.config;
+    lib = self.inputs.nixpkgs.lib;
+    fsys = lib.filesystem;
+    fset = lib.filesets;
+
+    slib = import ./lib { inherit (self) outPath; inherit lib; };
+
+    nub_home = self.homeConfigurations."lillecarl@nub";
+    shitbox_home = self.homeConfigurations."lillecarl@shitbox";
+  };
 in
-rec {
-  inherit
-    flake
-    pkgs
-    lib
-    nixosConfigurations
-    nub
-    shitbox
-    host;
-
-  self = flake;
-
-  slib = import ./lib { inherit (self) outPath; inherit (flake.inputs.nixpkgs) lib; };
-  nub_home = flake.homeConfigurations."lillecarl@nub";
-  shitbox_home = flake.homeConfigurations."lillecarl@shitbox";
+out //
+{
+  tmp = import ./repl_tmp.nix out;
 }
--- repl_tmp.nix
+++ repl_tmp.nix
@@ -1,4 +1,5 @@
 { lib, ... }@ctx:
 {
-
+  hyprscroller = lib.elemAt ctx.home.config.wayland.windowManager.hyprland.plugins 0;
+  hyprland = ctx.home.config.wayland.windowManager.hyprland.package;
 }
