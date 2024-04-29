f11a4f4011eb4ef6491bdb4e9f3d1375adb812f5
Carl Hjerpe <git@hjerpe.xyz>
2024-04-29 23:23:47 +0200

    fish: bind ctrl + z to interactive zoxide

--- lillecarl/dotfiles/.config/fish/conf.d/linked.fish
+++ lillecarl/dotfiles/.config/fish/conf.d/linked.fish
@@ -68,6 +68,8 @@ bind -M visual / _atuin_search
 # bind ctrl+shift+d to scroll down
 bind -M insert \e\[100\;6u scrolldown
 bind -M visual \e\[100\;6u scrolldown
+# bind ctrl+z to interactive zoxide
+bind \cZ __zi_drop
 
 function set_lastpath -e fish_postexec
     if not set -q TMUX
