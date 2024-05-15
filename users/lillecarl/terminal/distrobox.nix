d79dc84a1cceaa18e0c3181049380d3e6aa93dcb
Carl Hjerpe <git@hjerpe.xyz>
2024-05-15 20:28:22 +0200

    distrobox: add zoxide and fd to distrobox PATH

--- users/lillecarl/terminal/distrobox.nix
+++ users/lillecarl/terminal/distrobox.nix
@@ -5,9 +5,11 @@
     paths = with config; [
       nix.package
       programs.atuin.package
+      programs.fd.package
       programs.fish.package
       programs.ripgrep.package
       programs.starship.package
+      programs.zoxide.package
       wayland.windowManager.hyprland.package
     ];
   };
