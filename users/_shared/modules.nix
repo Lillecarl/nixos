a9055d85cdfd41aa7fdf1b8a71d831985d84dd23
Carl Hjerpe <git@hjerpe.xyz>
2024-05-17 10:09:05 +0200

    disable nix-flatpak

--- users/_shared/modules.nix
+++ users/_shared/modules.nix
@@ -3,7 +3,7 @@
   imports = [
     (self + "/stylix.nix")
     inputs.agenix.homeManagerModules.default
-    inputs.nix-flatpak.homeManagerModules.nix-flatpak
+    #inputs.nix-flatpak.homeManagerModules.nix-flatpak
     inputs.stylix.homeManagerModules.stylix
     inputs.catppuccin-nix.homeManagerModules.catppuccin
   ];
