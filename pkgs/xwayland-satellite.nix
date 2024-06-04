d49f605f7060376b726b68bfd69cd1dfa035894e
Carl Hjerpe <git@hjerpe.xyz>
2024-06-04 08:49:03 +0200

    xwayland-satellite: use GitHub version

--- pkgs/xwayland-satellite.nix
+++ pkgs/xwayland-satellite.nix
@@ -1,13 +1,19 @@
 { lib
+, fetchFromGitHub
 , rustPlatform
 , xcb-util-cursor
 , ...
 }:
 rustPlatform.buildRustPackage rec {
   pname = "xwayland-satellite";
-  version = "git";
+  version = "0.3";
 
-  src = ./.;
+  src = fetchFromGitHub {
+    owner = "Supreeeme";
+    repo = "xwayland-satellite";
+    rev = "v${version}";
+    sha256 = "sha256-0nvFpHzkho7ATwvceDNdlk3CULVRP4VgKJjEO/7SCi4=";
+  };
 
   nativeBuildInputs = [
     rustPlatform.bindgenHook # Find libclang.so
@@ -25,6 +31,7 @@ rustPlatform.buildRustPackage rec {
   };
 
   meta = {
+    mainProgram = pname;
     description = "Xwayland outside your wayland";
     homepage = "https://github.com/${src.owner}/${src.repo}";
     license = lib.licenses.mspl;
