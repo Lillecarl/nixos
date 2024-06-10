f8d8263d4444cfa87104363af5b75aec16bd4b93
Carl Hjerpe <git@hjerpe.xyz>
2024-06-10 04:50:44 +0200

    Fix deprecated option

--- hosts/_shared/default.nix
+++ hosts/_shared/default.nix
@@ -159,7 +159,7 @@
       pinentryPackage = pkgs.pinentry-qt;
     };
     # Bash autocomplete
-    bash.enableCompletion = true;
+    bash.completion.enable = true;
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
     mtr.enable = true;
