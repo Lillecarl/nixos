99ca7e38580b01b76363e02670581aebe4e3f0d5
Carl Hjerpe <git@hjerpe.xyz>
2024-06-12 22:42:05 +0200

    nix-cd: allow arbitary locations

--- users/lillecarl/dotfiles/.config/fish/functions/nix-cd.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix-cd.fish
@@ -1,6 +1,6 @@
 function nix-cd
-	set -f pkg $argv[1]
-	set -f shell_path (nix shell "$FLAKE#$pkg" --command sh -c 'echo $PATH')
+	set -f flakearg $argv[1]
+	set -f shell_path (nix shell "$flakearg" --command sh -c 'echo $PATH')
 	set -f shell_path_list (echo $shell_path | string split ':')
 	cd $shell_path_list[1] > /dev/null || cd (dirname $shell_path_list[1]) > /dev/null || echo "Wtf"
 end
