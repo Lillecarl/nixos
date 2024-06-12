20bee63a00884be02ae73bdb75471ca603735464
Carl Hjerpe <git@hjerpe.xyz>
2024-06-12 22:41:15 +0200

    unset sourced environment before Niri launch

--- users/lillecarl/dotfiles/.config/fish/final/90-start_niri.fish
+++ users/lillecarl/dotfiles/.config/fish/final/90-start_niri.fish
@@ -1,4 +1,8 @@
 if status --is-login && test "$XDG_VTNR" = 2
     set -x NIRI_LOGFILE /run/user/$(id -u)/niri.log
+
+    set -e __HM_SESS_VARS_SOURCED
+    set -e __NIXOS_SET_ENVIRONMENT_DONE
+
     exec sh -c "SHLVL=0 niri --session > $NIRI_LOGFILE"
 end
