73fc7df6ee66189e6a4d37f0f54c2313d5d36145
Carl Hjerpe <git@hjerpe.xyz>
2024-06-18 18:26:56 +0200

    nix-cd: Don't display errors for missing paths

--- users/lillecarl/dotfiles/.config/fish/functions/nix-cd.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix-cd.fish
@@ -2,5 +2,5 @@ function nix-cd
 	set -f flakearg $argv[1]
 	set -f shell_path (nix shell "$flakearg" --command sh -c 'echo $PATH')
 	set -f shell_path_list (echo $shell_path | string split ':')
-	cd $shell_path_list[1] > /dev/null || cd (dirname $shell_path_list[1]) > /dev/null || echo "Wtf"
+	cd $shell_path_list[1] &> /dev/null || cd (dirname $shell_path_list[1]) &> /dev/null || echo "Wtf"
 end
