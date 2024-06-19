cf1158c34412c38ff2b3da471ef8975a78c2f345
Carl Hjerpe <git@hjerpe.xyz>
2024-06-19 11:47:43 +0200

    rust-overlay: Remove flake-utils override

--- flake.nix
+++ flake.nix
@@ -127,7 +127,6 @@
     };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-wsl = {
