5f6f416de4161d007658ab74c8957624610c1702
Carl Hjerpe <git@hjerpe.xyz>
2024-07-01 17:18:34 +0200

    gpg-agent: disable keyboard grabbing, set pinentry package

--- users/lillecarl/terminal/gpg.nix
+++ users/lillecarl/terminal/gpg.nix
@@ -1,10 +1,13 @@
 { config
+, pkgs
 , ...
 }: {
   services.gpg-agent = {
     enable = true;
     enableSshSupport = false; # gnome-keyring
     enableExtraSocket = true;
+    pinentryPackage = pkgs.pinentry-qt;
+    grabKeyboardAndMouse = false;
   };
 
   programs.gpg = {
