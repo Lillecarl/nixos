afcc7ecda23bdc1ae11bbeeced74f6bbffdbd55e
Carl Hjerpe <git@hjerpe.xyz>
2024-07-01 17:18:02 +0200

    helix: lsp(s)

--- users/lillecarl/terminal/helix.nix
+++ users/lillecarl/terminal/helix.nix
@@ -7,18 +7,31 @@
     enable = true;
 
     languages = {
-      language-server = {
-        nix = {
-          command = lib.getExe pkgs.nil;
-          config.nil = {
-            formatting.command = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ];
-            nix.flake.autoEvalInputs = true;
-          };
+      language-server.nil = {
+        command = lib.getExe pkgs.nil;
+        config.nil = {
+          formatting.command = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ];
+          nix.flake.autoEvalInputs = true;
         };
+      };
+      language-server.gpt = {
+        command = "${lib.getExe pkgs.helix-gpt} --handler copilot";
+      };
+      language-server.luals = {
+        command = "${lib.getExe pkgs.lua-language-server}";
+      };
+      language-server.pylsp = {
+        command = lib.getExe pkgs.python3Packages.python-lsp-server;
+      };
+      language-server.yamlls = {
+        command = lib.getExe pkgs.yaml-language-server;
       };
 
       language = [
-        { name = "nix"; language-servers = [ "nix" ]; }
+        { name = "nix"; language-servers = [ "nil" "gpt" ]; }
+        { name = "python"; language-servers = [ "pylsp" "gpt" ]; }
+        { name = "lua"; language-servers = [ "luals" "gpt" ]; }
+        { name = "yaml"; language-servers = [ "yamlls" "gpt" ]; }
       ];
     };
   };
