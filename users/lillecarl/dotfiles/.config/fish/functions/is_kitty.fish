d706e41b7f16f8ce756315fd1d06ec9213b175f8
Carl Hjerpe <git@hjerpe.xyz>
2024-08-09 03:09:11 +0200

    improve is_kitty

--- users/lillecarl/dotfiles/.config/fish/functions/is_kitty.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/is_kitty.fish
@@ -1,4 +1,8 @@
+# check if the terminal is kitty, but not really
 function is_kitty
+    # zellij and tmux can't handle kitty escape codes
     set -q ZELLIJ && return 1
+    set -q TMUX && return 1
+    # is kitty if $TERM is xterm-kitty
     test $TERM = xterm-kitty && return 0 || return 1
 end
