411d4e554e2cb360c3a62860893420d758bb1438
Carl Hjerpe <git@hjerpe.xyz>
2024-08-09 05:47:34 +0200

    netfilter: move rules into nix files

--- hosts/_shared/networking.nix
+++ hosts/_shared/networking.nix
@@ -10,13 +10,25 @@
     nftables = {
       enable = true;
 
-      tables.carl = {
+      tables.masq = {
         family = "inet";
-        content = builtins.readFile ./carl.nft;
+        content = ''
+          chain nat {
+            type nat hook postrouting priority mangle;
+            ip saddr == 10.13.37.0/24 ip daddr != 10.13.37.0/24 masquerade comment "Masquerade VM traffic"
+            iifname == br13 oifname != br13 masquerade comment "Masquerade VM traffic"
+          }
+        '';
       };
     };
     firewall = {
       enable = true;
+      allowPing = true;
+
+      extraInputRules = ''
+        ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert, mld-listener-query, nd-router-solicit } accept comment "Allow icmpv6 identification"
+        ip protocol icmp icmp type { destination-unreachable, router-advertisement, time-exceeded, parameter-problem } accept comment "Allow icmpv4 identification"
+      '';
     };
     networkmanager.unmanaged = lib.attrNames config.systemd.network.netdevs;
   };
@@ -28,23 +40,26 @@
       # Dummy interface to add to bridge(s) that we always want up.
       dummy0 = {
         netdevConfig = {
-          Kind = "dummy";
           Name = "dummy0";
+          Kind = "dummy";
         };
       };
 
       br13 = {
         netdevConfig = {
-          Kind = "bridge";
           Name = "br13";
+          Kind = "bridge";
         };
       };
     };
+
     networks = {
       # Dummy interface to add to bridge(s) that we always want up.
       dummy0 = {
+        # Match with dummy netdev by name
         matchConfig.Name = config.systemd.network.netdevs.dummy0.netdevConfig.Name;
         # Bridge(s) to add this interface to.
+        # br13 is our bridge for VM's
         bridge = [
           config.systemd.network.netdevs.br13.netdevConfig.Name
         ];
