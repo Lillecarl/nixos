ba9fcf69907dc482cb8309d84b7e40fdb1e5e7c0
Carl Hjerpe <git@hjerpe.xyz>
2024-08-13 01:40:28 +0200

    users/postgres: add lillecarl to postgres group

--- hosts/_shared/users.nix
+++ hosts/_shared/users.nix
@@ -33,6 +33,7 @@ in
               "uinput"
               "input"
               "kubernetes"
+              "postgres"
             ];
             openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAHZ3pA0vIXiKQuwfM1ks8TipeOxfDT9fgo4xMi9iiWr lillecarl@lillecarl.com"
