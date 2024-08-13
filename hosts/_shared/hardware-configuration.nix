d8856b26963773974d6de14f82d3e6893fa0049e
Carl Hjerpe <git@hjerpe.xyz>
2024-08-13 17:20:42 +0200

    hardware: enable zramSwap

--- hosts/_shared/hardware-configuration.nix
+++ hosts/_shared/hardware-configuration.nix
@@ -1,4 +1,7 @@
-{ lib, ... }:
+{ config
+, lib
+, ...
+}:
 {
   boot = {
     kernelParams = [
@@ -8,7 +11,11 @@
     kernel.sysctl = {
       "kernel.task_delayacct" = 1;
       "net.ipv4.ip_forward" = lib.mkDefault 1;
-      "vm.swappiness" = 1;
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
+      "vm.min_free_kbytes" = 151041; # 1% of RAM in nub
     };
   };
 
@@ -20,4 +27,11 @@
       thin_pool_discards = 1
     }
   '';
+
+  zramSwap = {
+    enable = true;
+    memoryPercent = if config.networking.hostName == "nub" then 90 else 50;
+    priority = config.disko.devices.lvm_vg.pool.lvs.swap.content.priority * 2;
+    writebackDevice = "/dev/pool/${config.disko.devices.lvm_vg.pool.lvs.zram-writeback.name}";
+  };
 }
