6a1b6cf9fdbe7a1aa5a4c3850e17e2de176017af
Carl Hjerpe <git@hjerpe.xyz>
2024-08-13 02:44:07 +0200

    postgres: package pg_jsonschema from Supabase

---
+++ pkgs/pg_jsonschema.nix
@@ -0,0 +1,34 @@
+{ buildPgrxExtension
+, fetchFromGitHub
+, pkg-config
+, postgresql
+}:
+let
+  name = "pg_jsonschema";
+  version = "0.3.1";
+
+  src = fetchFromGitHub {
+    owner = "supabase";
+    repo = name;
+    rev = "v${version}";
+    sha256 = "sha256-YdKpOEiDIz60xE7C+EzpYjBcH0HabnDbtZl23CYls6g=";
+  };
+in
+buildPgrxExtension {
+  inherit postgresql src version;
+
+  pname = name;
+
+  nativeBuildInputs = [ pkg-config ];
+
+  doCheck = false;
+
+  cargoLock = {
+    lockFile = "${src}/Cargo.lock";
+    outputHashes = { };
+  };
+
+  postPatch = /* bash */ ''
+    substituteInPlace ./${name}.control --subst-var-by CARGO_VERSION ${version}
+  '';
+}
