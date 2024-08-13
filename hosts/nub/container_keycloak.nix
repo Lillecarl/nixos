c4784172789f7914faa727acb201ca991a15dcf0
Carl Hjerpe <git@hjerpe.xyz>
2024-08-13 17:28:00 +0200

    nub: keycloak

--- hosts/nub/container_keycloak.nix
+++ hosts/nub/container_keycloak.nix
@@ -5,10 +5,19 @@ let
   domain = "${alias}.${topDomain}";
 
   certPath = config.security.acme.certs.${alias}.directory;
-  httpPort = 20080;
   httpsPort = 20443;
+  pgport = 5433;
+  username = "keycloak";
+  dbname = username;
 in
-if false then { } else {
+if true then { } else {
+  disabledModules = [
+    "services/web-apps/keycloak.nix"
+  ];
+  imports = [
+    "${self}/modules/nixos/keycloak.nix"
+  ];
+
   containers.keycloak = {
     autoStart = true;
     specialArgs = {
@@ -27,11 +36,53 @@ if false then { } else {
       ];
 
       environment.systemPackages = [
-        pkgs.kitty.terminfo
-        config.services.keycloak.package
+        pkgs.kitty.terminfo # Terminal acts all weird otherwise
+        pkgs.binutils # strings
       ];
 
+      passthru.kc = lib.lists.head (lib.filter (x: (x.pname or "") == "keycloak") config.environment.systemPackages);
+
       services.openssh.ports = [ 20022 ];
+      users = {
+        users.keycloak = {
+          isNormalUser = true;
+          group = username;
+        };
+        groups.keycloak = { };
+      };
+      security.wrappers."kc.sh" =
+        let
+          envWrap = pkgs.writeScript "kc_env_wrap" /*bash*/''
+            #!/usr/bin/env bash
+            export KC_HOME_DIR="/run/keycloak"
+            export KC_CONF_DIR="/run/keycloak/conf"
+            exec ${config.passthru.kc}/bin/kc.sh "$@"
+          '';
+        in
+        {
+          setuid = true;
+          setgid = true;
+          owner = username;
+          group = username;
+          source = "${envWrap}";
+        };
+
+      services.postgresql = {
+        enable = true;
+        settings = {
+          port = pgport;
+          log_connections = true;
+        };
+        #identMap = ''
+        #  # MAPNAME       SYSTEM-USERNAME   PG-USERNAME
+        #  keycloak        keycloak          keycloak
+        #'';
+        ensureUsers = [{
+          name = "keycloak";
+          ensureDBOwnership = true;
+        }];
+        ensureDatabases = [ "keycloak" ];
+      };
 
       services.keycloak = {
         enable = true;
@@ -41,16 +92,21 @@ if false then { } else {
 
         database = {
           type = "postgresql";
-          createLocally = true;
+          port = pgport;
+          createLocally = false;
 
-          username = "lillecarl";
+          username = "keycloak";
           passwordFile = "${pkgs.writeText "keycloak-db-password" "lillecarl"}";
         };
 
         plugins = [
-          (builtins.fetchurl {
+          (pkgs.fetchurl {
             url = "https://github.com/sweid4keycloak/bankid4keycloak/releases/download/v24.0.0/bankid4keycloak-24.0.0.jar";
             sha256 = "sha256:062mvnym9zi6c7ia3jjjpvqk5092pymfnp5wc86r49y8qi7qdrj0";
+          })
+          (builtins.fetchurl {
+            url = "https://github.com/kohlschutter/junixsocket/releases/download/junixsocket-2.9.1/junixsocket-selftest-2.9.1-jar-with-dependencies.jar";
+            sha256 = "sha256:0bcj37ccm3dim4zwa8wvb0vak8kcgqc6w5p03rc9d94nci93h3xr";
           })
         ];
 
@@ -72,10 +128,22 @@ if false then { } else {
         };
 
         settings = {
+          # I hate Java so much...
+          #db-password = lib.mkForce null;
+          #db-url = lib.mkForce "jdbc:postgresql://localhost/keycloak?socketFactory=org.newsclub.net.unix.AFUNIXSocketFactory$FactoryArg&socketFactoryArg=/var/run/postgresql/.s.PGSQL.${toString pgport}&sslmode=disable";
+          db-url-database = dbname;
+          db-url-host = "localhost";
+          db-url-port = pgport;
+          db-url-properties = "socketFactory=org.newsclub.net.unix.AFUNIXSocketFactory$FactoryArg&socketFactoryArg=/var/run/postgresql/.s.PGSQL.${toString pgport}&sslmode=disable";
+          db-username = "keycloak";
+          db-password = null;
+          #db-url-database = lib.mkForce null;
+          #db-url-host = lib.mkForce null;
+          #db-url-port = lib.mkForce null;
+          #db-url-properties = lib.mkForce null;
+          #db-username = lib.mkForce null;
           health-enabled = true;
           hostname = "";
-          #hostname = domain;
-          #hostname-admin = domain;
           hostname-debug = true;
           hostname-strict = false;
           #hostname-strict-backchannel = false;
@@ -83,9 +151,10 @@ if false then { } else {
           http-enabled = false;
           https-port = httpsPort;
           log = "console";
-          log-level = "trace";
+          log-level = "info";
           metrics-enabled = true;
           proxy-headers = "xforwarded";
+          "quarkus.transaction-manager.enable-recovery" = true;
         };
       };
       nixpkgs.pkgs = hostPkgs;
