85fce758521e63313616246f4aa9eccc55f0f3e8
Carl Hjerpe <git@hjerpe.xyz>
2024-08-16 07:25:06 +0200

    udiskie: start after waybar

--- users/_shared/gui/udiskie.nix
+++ users/_shared/gui/udiskie.nix
@@ -1,7 +1,17 @@
-_:
+{ lib, ... }:
 {
   services.udiskie = {
     enable = true;
     automount = false;
+  };
+
+  systemd.user.services.udiskie = {
+    Unit = {
+      After = lib.mkForce [
+        "graphical-session-pre.target"
+        "waybar.service"
+      ];
+      Requires = lib.mkForce [ "waybar.service" ];
+    };
   };
 }
