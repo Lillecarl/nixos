cb3f6cb05c37103ae6eea3cae20cb9b2859a5bfa
Carl Hjerpe <git@hjerpe.xyz>
2024-08-24 19:56:01 +0200

    Enable waydroid
    TODO: Fix waydroid networking starting it's own bridge, DHCP server and DNS server.

---
+++ hosts/_shared/waydroid.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  # How to Google apps:
+  # 1. Start Waydroid
+  # 2. Browse https://www.google.com/android/uncertified/
+  # 3. Run the sqlite command in 'waydroid shell' to get android_id
+  # 4. Register the id
+  # 5. Restart Waydroid
+  virtualisation.waydroid.enable = true;
+}
