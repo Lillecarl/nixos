ed1a37bada81c2167a47f4461a896ec1ab38b0c7
Carl Hjerpe <git@hjerpe.xyz>
2024-11-15 23:43:58 +0000

    hm: Create config for ChromeBook

---
+++ users/penguin/flake-module.nix
@@ -0,0 +1,26 @@
+{ self
+, inputs
+, withSystem
+, ...
+}:
+let
+  system = "aarch64-linux";
+in
+{
+  flake = {
+    homeConfigurations =
+      {
+        "lillecarl@penguin" = withSystem system ({ pkgs, mpkgs, spkgs, flakeloc, ... }:
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs =
+              {
+                inherit self inputs flakeloc mpkgs spkgs;
+              };
+            modules = [
+              ../lillecarl
+            ];
+          });
+      };
+  };
+}
