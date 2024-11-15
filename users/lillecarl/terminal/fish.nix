4af4769d6f6e88f548e23831b4e8f74a31079927
Carl Hjerpe <git@hjerpe.xyz>
2024-11-15 23:37:04 +0000

    fish: remove fishPlugins.json + mapper

--- users/lillecarl/terminal/fish.nix
+++ users/lillecarl/terminal/fish.nix
@@ -9,25 +9,12 @@
   programs.fish = {
     enable = true;
 
-    plugins =
-      (builtins.map
-        (
-          plug: {
-            inherit (plug) name;
-            src = pkgs.fetchFromGitHub plug.src;
-          }
-        )
-        (
-          builtins.fromJSON (builtins.readFile ./fishPlugins.json)
-        )
-      )
-      ++
-      (with pkgs.fishPlugins; [
-        {
-          name = "done";
-          inherit (done) src;
-        }
-      ]);
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "done";
+        inherit (done) src;
+      }
+    ];
 
     interactiveShellInit = /* fish */ ''
       ${lib.getExe pkgs.zoxide} init fish | source
