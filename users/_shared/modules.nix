899d7ee3eb3728c7e4ccf62de23f652ad6ccb74a
Carl Andersson <git@lillecarl.com>
2024-11-23 20:41:55 +0000

    Remove old code for overriding modules

--- users/_shared/modules.nix
+++ users/_shared/modules.nix
@@ -3,7 +3,6 @@
   imports = [
     (self + "/stylix.nix")
     inputs.agenix.homeManagerModules.default
-    #inputs.nix-flatpak.homeManagerModules.nix-flatpak
     inputs.stylix.homeManagerModules.stylix
     inputs.catppuccin-nix.homeManagerModules.catppuccin
   ];
