75753c4f4f788e31e6239e242aef906ba5c9ad52
Carl Andersson <git@lillecarl.com>
2024-11-24 20:30:39 +0000

    fish: Fix PATH when login shell
    When fish is among the first things to run on a non-nixos system we
    can't be sure that PATH is set correctly, check if PATH contains our
    configured values and if not add them.

--- users/lillecarl/terminal/fish.nix
+++ users/lillecarl/terminal/fish.nix
@@ -19,7 +19,20 @@
       #source $XDG_RUNTIME_DIR/agenix/copilot
     '';
 
-    shellInit = ''
+    # Prepend some Nix & HM paths if they're not already there.
+    # Required when using fish as login shell in Crostini.
+    shellInit = let
+      paths = [
+        "/nix/var/nix/profiles/default/bin"
+        "${config.home.homeDirectory}/.nix-profile/bin"
+        "${config.home.homeDirectory}/.local/bin"
+      ];
+      pathsStr = lib.pipe paths [
+        (paths: builtins.map (path: "contains ${path} $PATH || set --prepend PATH ${path}") paths)
+        (lines: lib.concatLines lines)
+      ];
+    in ''
+      ${pathsStr}
     '';
     shellInitLast = ''
       for line in $(find ${config.xdg.configHome}/fish/final/*.fish)
