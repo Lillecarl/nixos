df3f1fe5e4e5b406913e329982a46625cece70a2
Carl Andersson <git@lillecarl.com>
2024-12-09 01:55:50 +0000

    helix: implement helix wrapper to local build if exists

--- users/lillecarl/terminal/helix.nix
+++ users/lillecarl/terminal/helix.nix
@@ -1,8 +1,34 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   programs.helix = lib.mkIf config.ps.editors.enable {
     enable = config.ps.editors.enable;
     defaultEditor = true;
+    #package = (builtins.getFlake "/home/lillecarl/Code/helix").packages.${pkgs.system}.helix;
+    # If we have a Helix built with "cargo build" in $HOME/Code/helix we write a
+    # shell script that calls that one rather than using one from nixpkgs.
+    package =
+      let
+        repo = "/home/lillecarl/Code/helix";
+        runtime = "${repo}/runtime";
+        exe = "${repo}/target/debug/hx";
+      in
+      if builtins.pathExists exe && builtins.pathExists runtime then
+        pkgs.writeScriptBin "hx"
+          # bash
+          ''
+            #! ${pkgs.runtimeShell}
+            # Tells Helix where to look for runtime code (tree-sitter
+            # grammars, themes and stuff)
+            export HELIX_RUNTIME="${runtime}" 
+            exec ${exe} "$@"
+          ''
+      else
+        pkgs.helix;
 
     settings = {
       theme = if config.ps.terminal.true-color then "catppuccin_mocha" else "default";
