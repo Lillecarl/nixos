43027dde7e977789ae3f57c090c7fb5dc20c80f6
Carl Andersson <git@lillecarl.com>
2024-12-13 00:53:08 +0000

    Implement deploy-rs for hetzner VM

--- hosts/hetzner1/flake-module.nix
+++ hosts/hetzner1/flake-module.nix
@@ -9,41 +9,52 @@ let
   system = "aarch64-linux";
 in
 {
-  flake = {
-    nixosConfigurations.hetzner1 = withSystem system (
-      {
-        config,
-        pkgs,
-        flakeloc,
-        ...
-      }:
-      let
-        specialArgs = {
-          inherit inputs flakeloc self;
-        };
-      in
-      inputs.nixpkgs.lib.nixosSystem {
-        inherit pkgs;
-        modules = [
-          {
-            networking.hostName = "hetzner1";
-            networking.firewall.enable = false;
-            services.openssh.enable = true;
-          }
-          ./default.nix
-        ];
-        inherit specialArgs;
-      }
-    );
-    deploy.nodes.some-random-system = {
-        hostname = "some-random-system";
+  flake =
+    let
+      hostname = "hetzner1";
+    in
+    {
+      nixosConfigurations.${hostname} = withSystem system (
+        {
+          config,
+          pkgs,
+          flakeloc,
+          ...
+        }:
+        let
+          specialArgs = {
+            inherit inputs flakeloc self;
+          };
+        in
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit pkgs;
+          modules = [
+            {
+              networking.hostName = hostname;
+              networking.firewall.enable = false;
+              services.openssh.enable = true;
+              programs.git.enable = true;
+              environment.systemPackages = [
+                pkgs.gitui
+              ];
+            }
+            ./default.nix
+          ];
+          inherit specialArgs;
+        }
+      );
+      deploy.nodes.${hostname} = {
+        hostname = "65.21.63.133";
         profiles.system = {
           user = "root";
-          path = inputs.deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.hetzner1;
+          sshUser = "root";
+          path = inputs.deploy-rs.lib.${system}.activate.nixos self.nixosConfigurations.${hostname};
         };
-    };
+      };
 
-    # This is highly advised, and will prevent many possible mistakes
-    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) inputs.deploy-rs.lib;
-  };
+      # This is highly advised, and will prevent many possible mistakes
+      # checks = builtins.mapAttrs (
+      #   system: deployLib: deployLib.deployChecks self.deploy
+      # ) inputs.deploy-rs.lib;
+    };
 }
