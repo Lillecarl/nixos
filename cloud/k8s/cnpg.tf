a52e85495ca1b6976842cdd947696074b9290b04
Carl Andersson <git@lillecarl.com>
2024-12-15 22:20:15 +0100

    k8s tf lab

---
+++ cloud/k8s/cnpg.tf
@@ -0,0 +1,106 @@
+locals {
+  kust_path = "${path.module}/rendered-kustomize"
+}
+
+data "http" "manifest" {
+  url = "https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/releases/cnpg-1.25.0-rc1.yaml"
+}
+
+resource "local_file" "cnpg-release" {
+  content  = data.http.manifest.response_body
+  filename = "${local.kust_path}/cnpg/operator.yaml"
+}
+
+resource "local_file" "cnpg-kustomize" {
+  content = yamlencode({
+    resources = [
+      "./operator.yaml"
+    ]
+  })
+  filename = "${local.kust_path}/cnpg/kustomization.yaml"
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k ${path.module}/cnpg/ --server-side"
+  }
+}
+
+resource "kubectl_manifest" "cnpg-cluster" {
+    yaml_body = <<YAML
+apiVersion: postgresql.cnpg.io/v1
+kind: Cluster
+metadata:
+  name: cnpg-cluster
+spec:
+  instances: 1
+
+  storage:
+    size: 1Gi
+YAML
+}
+
+# data "kubectl_kustomize_documents" "cnpg-manifests" {
+#     target = path.module
+
+#     depends_on = [ local_file.cnpg-kustomize ]
+# }
+
+# resource "kubectl_manifest" "cnpg" {
+#     count     = length(data.kubectl_kustomize_documents.cnpg-manifests.documents)
+#     yaml_body = element(data.kubectl_kustomize_documents.cnpg-manifests.documents, count.index)
+# }
+
+# resource "kubectl_manifest" "test" {
+#     yaml_body = data.http.manifest.response_body
+#     server_side_apply = true
+# }
+
+# data "kustomization_overlay" "cnpg" {
+#   resources = [
+#     "${path.module}/cnpg.yaml"
+#   ]
+# }
+
+# # first loop through resources in ids_prio[0]
+# resource "kustomization_resource" "p0" {
+#   for_each = data.kustomization_overlay.cnpg.ids_prio[0]
+
+#   manifest = (
+#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
+#     : data.kustomization_overlay.cnpg.manifests[each.value]
+#   )
+# }
+
+# # then loop through resources in ids_prio[1]
+# # and set an explicit depends_on on kustomization_resource.p0
+# # wait 2 minutes for any deployment or daemonset to become ready
+# resource "kustomization_resource" "p1" {
+#   for_each = data.kustomization_overlay.cnpg.ids_prio[1]
+
+#   manifest = (
+#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
+#     : data.kustomization_overlay.cnpg.manifests[each.value]
+#   )
+#   wait = true
+#   timeouts {
+#     create = "2m"
+#     update = "2m"
+#   }
+
+#   depends_on = [kustomization_resource.p0]
+# }
+
+# # finally, loop through resources in ids_prio[2]
+# # and set an explicit depends_on on kustomization_resource.p1
+# resource "kustomization_resource" "p2" {
+#   for_each = data.kustomization_overlay.cnpg.ids_prio[2]
+
+#   manifest = (
+#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
+#     : data.kustomization_overlay.cnpg.manifests[each.value]
+#   )
+
+#   depends_on = [kustomization_resource.p1]
+# }
---
+++ cloud/k8s/providers.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    kustomization = {
+      source = "kbst/kustomization"
+      version = "0.9.6"
+    }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = "1.18.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.35.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "3.4.5"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
+
+provider "kustomization" {
+  # Configuration options
+}
+provider "kubernetes" {
+  # Configuration options
+}
+provider "kubectl" {
+  # Configuration options
+}
+provider "http" {
+  # Configuration options
+}
+provider "local" {
+  # Configuration options
+}
