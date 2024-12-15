e3f1015c3e00804313d1c112638490d0e26d831f
Carl Andersson <git@lillecarl.com>
2024-12-15 17:20:23 +0100

    disko: Use local disk as swap

--- hosts/hetzner1/disko.nix
+++ hosts/hetzner1/disko.nix
@@ -1,27 +1,79 @@
 {
   disko.devices = {
     disk = {
-      sda = {
-        device = "/dev/sda";
+      local = {
         type = "disk";
+        device = "/dev/sda";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              size = "1G";
               type = "EF00";
-              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
-            root = {
+            plainSwap = {
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "swap";
+                discardPolicy = "both";
+                priority = 3; # 5 is default for zramSwap
+              };
+            };
+          };
+        };
+      };
+      remote = {
+        type = "disk";
+        device = "/dev/sdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            primary = {
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "volumepool";
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      volumepool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ]; # Override existing partition
+              # Subvolumes must set a mountpoint in order to be mounted,
+              # unless their parent is mounted
+              subvolumes = {
+                # Subvolume name is different from mountpoint
+                "/rootfs" = {
+                  mountpoint = "/";
+                };
+                # Subvolume name is the same as the mountpoint
+                "/home" = {
+                  mountOptions = [ "compress=zstd" ];
+                  mountpoint = "/home";
+                };
+                # Parent is not mounted so the mountpoint must be set
+                "/nix" = {
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                  mountpoint = "/nix";
+                };
               };
             };
           };
