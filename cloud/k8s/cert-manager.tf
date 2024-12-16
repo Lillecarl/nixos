7941eec221ef80e57f85bd0c74cce882dcedbe4a
Carl Andersson <git@lillecarl.com>
2024-12-17 00:09:41 +0100

    Better kustomize render and application
    Use kbst/kustomize to render the kustomization in Terraform
    Use gavinbunney/kubectl to server-side apply YAML documents exposed by
    kbst/kustomize.
    
    The kbst/kustomize provider is nicer than the gavinbunney/kubectl for
    splitting kustomizations since it splits them in 3 steps.
    See https://registry.terraform.io/providers/kbst/kustomization/latest/docs/data-sources/overlay#ids_prio-6

--- cloud/k8s/cert-manager.tf
+++ cloud/k8s/cert-manager.tf
@@ -1,6 +1,7 @@
 locals {
   cm_path = "${local.kust_path}/cm"
 }
+variable "CF_DNS_TOKEN" {}
 
 data "http" "cm-manifest" {
   url = "https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml"
@@ -11,20 +12,43 @@ resource "local_file" "cm-release" {
   filename = "${local.cm_path}/operator.yaml"
 }
 
-resource "local_file" "cm-kustomize" {
-  content = yamlencode({
-    resources = [
-      "./operator.yaml"
-    ]
-  })
-  filename = "${local.cm_path}/kustomization.yaml"
-
-  provisioner "local-exec" {
-    command = "kubectl apply -k ${local.cm_path} --server-side"
-  }
+resource "local_file" "cm-extras" {
+  content  = <<YAML
+---
+YAML
+  filename = "${local.cm_path}/extras.yaml"
 }
 
-variable "CF_DNS_TOKEN" {}
+data "kustomization_overlay" "cm-manifests" {
+  resources = [
+    "${local_file.cm-release.filename}",
+    "${local_file.cm-extras.filename}",
+  ]
+} 
+
+resource "kubectl_manifest" "cm0" {
+  for_each = data.kustomization_overlay.cm-manifests.ids_prio[0]
+
+  yaml_body = data.kustomization_overlay.cm-manifests.manifests[each.value]
+  server_side_apply = true
+}
+
+resource "kubectl_manifest" "cm1" {
+  for_each = data.kustomization_overlay.cm-manifests.ids_prio[1]
+
+  yaml_body = data.kustomization_overlay.cm-manifests.manifests[each.value]
+  server_side_apply = true
+  depends_on = [kubectl_manifest.cm0]
+}
+
+resource "kubectl_manifest" "cm2" {
+  for_each = data.kustomization_overlay.cm-manifests.ids_prio[2]
+
+  yaml_body = data.kustomization_overlay.cm-manifests.manifests[each.value]
+  server_side_apply = true
+  depends_on = [kubectl_manifest.cm1]
+}
+
 resource "kubectl_manifest" "cm-cf-secret" {
     yaml_body = <<YAML
 apiVersion: v1
@@ -36,6 +60,11 @@ type: Opaque
 stringData:
   api-token: ${var.CF_DNS_TOKEN}
 YAML
+  depends_on = [
+    kubectl_manifest.cm0,
+    kubectl_manifest.cm1,
+    kubectl_manifest.cm2,
+  ]
 }
 
 resource "kubectl_manifest" "cm-cf-issuer" {
@@ -57,6 +86,9 @@ spec:
             name: cloudflare-api-token-secret
             key: api-token
 YAML
+  depends_on = [
+    kubectl_manifest.cm-cf-secret,
+  ]
 }
 
 resource "kubectl_manifest" "cm-cert-lillecarl-com" {
@@ -81,4 +113,7 @@ spec:
     name: letsencrypt-staging
     kind: ClusterIssuer
 YAML
+  depends_on = [
+    kubectl_manifest.cm-cf-issuer,
+  ]
 }
--- cloud/k8s/cnpg.tf
+++ cloud/k8s/cnpg.tf
@@ -12,17 +12,41 @@ resource "local_file" "cnpg-release" {
   filename = "${local.cnpg_path}/operator.yaml"
 }
 
-resource "local_file" "cnpg-kustomize" {
-  content = yamlencode({
-    resources = [
-      "./operator.yaml"
-    ]
-  })
-  filename = "${local.cnpg_path}/kustomization.yaml"
+resource "local_file" "cnpg-extras" {
+  content  = <<YAML
+---
+YAML
+  filename = "${local.cnpg_path}/extras.yaml"
+}
 
-  provisioner "local-exec" {
-    command = "kubectl apply -k ${local.cnpg_path} --server-side"
-  }
+data "kustomization_overlay" "cnpg-manifests" {
+  resources = [
+    "${local_file.cnpg-release.filename}",
+    "${local_file.cnpg-extras.filename}",
+  ]
+} 
+
+resource "kubectl_manifest" "cnpg0" {
+  for_each = data.kustomization_overlay.cnpg-manifests.ids_prio[0]
+
+  yaml_body = data.kustomization_overlay.cnpg-manifests.manifests[each.value]
+  server_side_apply = true
+}
+
+resource "kubectl_manifest" "cnpg1" {
+  for_each = data.kustomization_overlay.cnpg-manifests.ids_prio[1]
+
+  yaml_body = data.kustomization_overlay.cnpg-manifests.manifests[each.value]
+  server_side_apply = true
+  depends_on = [kubectl_manifest.cnpg0]
+}
+
+resource "kubectl_manifest" "cnpg2" {
+  for_each = data.kustomization_overlay.cnpg-manifests.ids_prio[2]
+
+  yaml_body = data.kustomization_overlay.cnpg-manifests.manifests[each.value]
+  server_side_apply = true
+  depends_on = [kubectl_manifest.cnpg1]
 }
 
 resource "kubectl_manifest" "cnpg-cluster" {
@@ -39,73 +63,8 @@ spec:
 YAML
 
   depends_on = [
-    local_file.cnpg-kustomize
+    kubectl_manifest.cnpg0,
+    kubectl_manifest.cnpg1,
+    kubectl_manifest.cnpg2,
   ]
 }
-
-# data "kubectl_kustomize_documents" "cnpg-manifests" {
-#     target = path.module
-
-#     depends_on = [ local_file.cnpg-kustomize ]
-# }
-
-# resource "kubectl_manifest" "cnpg" {
-#     count     = length(data.kubectl_kustomize_documents.cnpg-manifests.documents)
-#     yaml_body = element(data.kubectl_kustomize_documents.cnpg-manifests.documents, count.index)
-# }
-
-# resource "kubectl_manifest" "test" {
-#     yaml_body = data.http.manifest.response_body
-#     server_side_apply = true
-# }
-
-# data "kustomization_overlay" "cnpg" {
-#   resources = [
-#     "${path.module}/cnpg.yaml"
-#   ]
-# }
-
-# # first loop through resources in ids_prio[0]
-# resource "kustomization_resource" "p0" {
-#   for_each = data.kustomization_overlay.cnpg.ids_prio[0]
-
-#   manifest = (
-#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
-#     : data.kustomization_overlay.cnpg.manifests[each.value]
-#   )
-# }
-
-# # then loop through resources in ids_prio[1]
-# # and set an explicit depends_on on kustomization_resource.p0
-# # wait 2 minutes for any deployment or daemonset to become ready
-# resource "kustomization_resource" "p1" {
-#   for_each = data.kustomization_overlay.cnpg.ids_prio[1]
-
-#   manifest = (
-#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
-#     : data.kustomization_overlay.cnpg.manifests[each.value]
-#   )
-#   wait = true
-#   timeouts {
-#     create = "2m"
-#     update = "2m"
-#   }
-
-#   depends_on = [kustomization_resource.p0]
-# }
-
-# # finally, loop through resources in ids_prio[2]
-# # and set an explicit depends_on on kustomization_resource.p1
-# resource "kustomization_resource" "p2" {
-#   for_each = data.kustomization_overlay.cnpg.ids_prio[2]
-
-#   manifest = (
-#     contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
-#     ? sensitive(data.kustomization_overlay.cnpg.manifests[each.value])
-#     : data.kustomization_overlay.cnpg.manifests[each.value]
-#   )
-
-#   depends_on = [kustomization_resource.p1]
-# }
