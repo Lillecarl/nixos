6dfe9194b714205002db151d3f85a616f8de262f
Carl Andersson <git@lillecarl.com>
2024-12-18 09:22:05 +0100

    zellisesh: erase vars instead of unexporting

--- users/lillecarl/dotfiles/.config/fish/functions/zellisesh.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/zellisesh.fish
@@ -1,11 +1,11 @@
 function zellisesh
     # Make sure to source all configs on new shell instances from zellij
-    set -u __HM_SESS_VARS_SOURCED
-    set -u __fish_home_manager_config_sourced
+    set -e __HM_SESS_VARS_SOURCED
+    set -e __fish_home_manager_config_sourced
     # Set WAYLAND_DISPLAY as a universal variable so it's shared between all
     # fish instances.
     set WAYLAND_DISPLAY_ORIG $WAYLAND_DISPLAY
-    set -u WAYLAND_DISPLAY
+    set -e WAYLAND_DISPLAY
     set -Ux WAYLAND_DISPLAY $WAYLAND_DISPLAY_ORIG
     # Attach to zellij or create a new session
     zellij attach || zellij
