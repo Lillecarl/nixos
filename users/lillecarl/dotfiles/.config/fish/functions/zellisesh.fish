3acafc16e815a783ac167c0c3dac7afb465df370
Carl Andersson <git@lillecarl.com>
2024-12-18 09:07:05 +0100

    zellisesh: unset one more config source gatekeeper variable

--- users/lillecarl/dotfiles/.config/fish/functions/zellisesh.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/zellisesh.fish
@@ -1,6 +1,7 @@
 function zellisesh
-    # Unset __HM_SESS_VARS_SOURCED so new shells get updated variables
+    # Make sure to source all configs on new shell instances from zellij
     set -u __HM_SESS_VARS_SOURCED
+    set -u __fish_home_manager_config_sourced
     # Set WAYLAND_DISPLAY as a universal variable so it's shared between all
     # fish instances.
     set WAYLAND_DISPLAY_ORIG $WAYLAND_DISPLAY
