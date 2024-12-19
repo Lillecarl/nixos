597af9f4f6149f4fb428415c27b3a927044cc17b
Carl Andersson <git@lillecarl.com>
2024-12-19 04:49:40 +0100

    HM: enable zoxide

--- users/lillecarl/terminal/default.nix
+++ users/lillecarl/terminal/default.nix
@@ -38,6 +38,7 @@
     ./tealdeer.nix
     ./template.nix
     ./tmux.nix
+    ./zoxide.nix
     ./zsh.nix
   ];
 
---
+++ users/lillecarl/terminal/zoxide.nix
@@ -0,0 +1,20 @@
+{ lib, config, ... }:
+let
+  modName = "zoxide";
+  cfg = config.ps.${modName};
+in
+{
+  options.ps = {
+    ${modName} = {
+      enable = lib.mkOption {
+        default = config.ps.terminal.enable;
+        description = "Whether to enable ${modName}.";
+      };
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    programs.zoxide = {
+      enable = true;
+    };
+  };
+}
