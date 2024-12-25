36388f257c0ccda35ac478f33b2ba223533f038c
Carl Andersson <git@lillecarl.com>
2024-12-25 20:57:45 +0100

    k8s: Deploy keycloak with Kustomize
    Render chart with Kustomize and apply individual manifests

--- cloud/k8s/keycloak.tf
+++ cloud/k8s/keycloak.tf
@@ -7,15 +7,15 @@ metadata:
 YAML
 }
 
-resource "helm_release" "keycloak" {
-  name       = "keycloak"
-  namespace  = "keycloak"
-  repository = "oci://registry-1.docker.io/bitnamicharts/"
-  chart      = "keycloak"
-  version    = "24.3.1"
-
-  values = [
-    (<<YAML
+data "kustomization_overlay" "keycloak-chart" {
+  helm_charts {
+    name          = "keycloak"
+    namespace     = "keycloak"
+    repo          = "oci://registry-1.docker.io/bitnamicharts/"
+    release_name  = "keycloak"
+    version       = "24.3.1"
+    include_crds  = true
+    values_inline = <<YAML
 postgresql:
   enabled: false
 auth:
@@ -42,10 +42,34 @@ metrics:
   prometheusRule:
     enabled: true
 YAML
-    )
-  ]
+  }
+  kustomize_options {
+    load_restrictor = "none"
+    enable_helm     = true
+    helm_path       = local.helm_path
+  }
+}
+resource "kubectl_manifest" "keycloak-chart0" {
+  for_each          = data.kustomization_overlay.keycloak-chart.ids_prio[0]
+  yaml_body         = data.kustomization_overlay.keycloak-chart.manifests[each.value]
+  server_side_apply = true
+  wait              = true
   depends_on = [
     kubectl_manifest.keycloak-namespace,
-    kubectl_manifest.cnpg_resource,
+    kubectl_manifest.cnpg_database,
   ]
+}
+resource "kubectl_manifest" "keycloak-chart1" {
+  for_each          = data.kustomization_overlay.keycloak-chart.ids_prio[1]
+  yaml_body         = data.kustomization_overlay.keycloak-chart.manifests[each.value]
+  server_side_apply = true
+  wait              = true
+  depends_on        = [kubectl_manifest.keycloak-chart0]
+}
+resource "kubectl_manifest" "keycloak-chart2" {
+  for_each          = data.kustomization_overlay.keycloak-chart.ids_prio[2]
+  yaml_body         = data.kustomization_overlay.keycloak-chart.manifests[each.value]
+  server_side_apply = true
+  wait              = true
+  depends_on        = [kubectl_manifest.keycloak-chart1]
 }
