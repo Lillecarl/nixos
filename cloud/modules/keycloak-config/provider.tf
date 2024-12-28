752e5cb5abde9715c001c80df06792a52b2bcef1
Carl Andersson <git@lillecarl.com>
2024-12-28 20:09:36 +0100

    Remove old tf modules

--- cloud/modules/keycloak-config/provider.tf
+++
@@ -1,8 +0,0 @@
-variable "keycloak_admin_pass" {}
-provider "keycloak" {
-  url                      = "https://keycloak.lillecarl.com"
-  client_id                = "admin-cli"
-  username                 = "superadmin"
-  password                 = var.keycloak_admin_pass
-  tls_insecure_skip_verify = true
-}
