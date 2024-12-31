3b4cc548261f6bde949c5af0e9f588e22582cc81
Carl Andersson <git@lillecarl.com>
2024-12-31 23:24:51 +0100

    helix style commenting

--- users/lillecarl/dotfiles/.config/nvim/lua/plugins/mini.lua
+++ users/lillecarl/dotfiles/.config/nvim/lua/plugins/mini.lua
@@ -7,18 +7,8 @@ function M.setup(cfg)
     after = function()
       require("mini.comment").setup({
         mappings = {
-          comment_visual = "c",
-        },
-      })
-      require("which-key").add({
-        {
-          mode = { "n" },
-          "<leader>c",
-          function()
-            local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-            MiniComment.toggle_lines(row, row, {})
-          end,
-          desc = "Comment cur line",
+          comment_visual = "<leader>c",
+          comment_line = "<leader>c",
         },
       })
     end,
