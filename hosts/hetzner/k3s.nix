f426cb99c47c80ee20a1582a724265d05a359b5a
Carl Andersson <git@lillecarl.com>
2024-12-31 12:49:02 +0100

    Use k3s cloud control manager again?
    Honestly I have no idea about cloud-controller-manager nor kube-proxy really
    TODO: Look into cloud-controller-manager and friends

--- hosts/hetzner/k3s.nix
+++ hosts/hetzner/k3s.nix
@@ -25,12 +25,17 @@ in
       extraFlags = [
         "--disable=traefik"
         "--disable=coredns"
+
+        # "--disable-kube-proxy"
+
         "--flannel-backend=none"
         "--disable-network-policy"
+
         # "--no-deploy servicelb"
-        "--disable-cloud-controller"
-        "--kubelet-arg=cloud-provider=external"
+        # "--disable-cloud-controller"
+        # "--kubelet-arg=cloud-provider=external"
         "--kubelet-arg=provider-id=hcloud://${config.lib.hetzAttrs.id}"
+
         "--kube-apiserver-arg=oidc-issuer-url=https://keycloak.lillecarl.com/realms/master"
         "--kube-apiserver-arg=oidc-client-id=kubernetes"
         "--kube-apiserver-arg=oidc-username-claim=sub" # usename is less fluid
