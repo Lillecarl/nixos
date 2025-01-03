49051d67aad1256cfef97f37fb83ef4b747533ec
Carl Andersson <git@lillecarl.com>
2025-01-03 22:09:17 +0100

    increase inotify and stuff

--- hosts/hetzner/k3s.nix
+++ hosts/hetzner/k3s.nix
@@ -51,5 +51,15 @@ in
     environment.systemPackages = [
       pkgs.cilium-cli
     ];
+    boot.kernel.sysctl = { # virtualisation.lxd.recommendedSysctlSettings
+      "fs.inotify.max_queued_events" = 1048576;
+      "fs.inotify.max_user_instances" = 1048576;
+      "fs.inotify.max_user_watches" = 1048576;
+      "vm.max_map_count" = 262144;
+      "kernel.dmesg_restrict" = 1;
+      "net.ipv4.neigh.default.gc_thresh3" = 8192;
+      "net.ipv6.neigh.default.gc_thresh3" = 8192;
+      "kernel.keys.maxkeys" = 2000;
+    };
   };
 }
