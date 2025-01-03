e8cb43ec056fb4e1f7f8d16415b6207137357dc0
Carl Andersson <git@lillecarl.com>
2025-01-03 03:33:57 +0100

    dragonflydb: init

---
+++ cloud/dragonflydb/main.tf
@@ -0,0 +1,74 @@
+variable "paths" { type = map(string) }
+variable "k8s_force" { type = bool }
+variable "deploy" { type = bool }
+locals {
+  ids-this-stage0 = data.kustomization_overlay.this.ids_prio[0]
+  ids-this-stage1 = toset(var.deploy ? [
+    for i in data.kustomization_overlay.this.ids_prio[1] :
+    # Exclude test-connection that's broken in upstream chart
+    i if i != "_/Pod/dragonfly-system/dragonfly-operator-test-connection"
+  ] : [])
+  ids-this-stage2 = var.deploy ? data.kustomization_overlay.this.ids_prio[2] : []
+  helm_values = {
+    serviceMonitor = {
+      enabled = true
+    }
+    grafanaDashboard = {
+      enabled = true
+    }
+  }
+  namespace = "dragonfly-system"
+}
+data "kustomization_overlay" "this" {
+  resources = [for file in tolist(fileset(path.module, "*.yaml")) : "${path.module}/${file}"]
+  namespace = local.namespace
+  helm_charts {
+    name          = "dragonfly-operator"
+    release_name  = "dragonfly-operator"
+    namespace     = local.namespace
+    include_crds  = true
+    values_inline = <<YAML
+YAML
+  }
+  helm_globals {
+    # Set chart_home to the path above our predownloaded chart. This somehow
+    # makes Kustomize use the local chart.
+    chart_home = var.paths.dragonfly-charts
+  }
+  kustomize_options {
+    load_restrictor = "none"
+    enable_helm     = true
+    helm_path       = var.paths.helm-path
+  }
+}
+resource "kubectl_manifest" "stage0" {
+  for_each   = local.ids-this-stage0
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = []
+
+  force_conflicts   = var.k8s_force
+  apply_only        = true
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage1" {
+  for_each   = local.ids-this-stage1
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage0]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage2" {
+  for_each   = local.ids-this-stage2
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage1]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
