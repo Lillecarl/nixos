f82abf4064ebe9a3d4f5694742fd255c5229add5
Carl Andersson <git@lillecarl.com>
2025-01-04 08:00:11 +0100

    Deploy reloader
    Rerolls different kinds of deployments if secrets or configmaps
    they depend on are updated.

---
+++ cloud/reloader/main.tf
@@ -0,0 +1,58 @@
+variable "paths" { type = map(string) }
+variable "k8s_force" { type = bool }
+variable "deploy" { type = bool }
+locals {
+  ids-this-stage0 = data.kustomization_overlay.this.ids_prio[0]
+  ids-this-stage1 = var.deploy ? data.kustomization_overlay.this.ids_prio[1] : []
+  ids-this-stage2 = var.deploy ? data.kustomization_overlay.this.ids_prio[2] : []
+  helm_values = {
+  }
+}
+data "kustomization_overlay" "this" {
+  resources = [for file in tolist(fileset(path.module, "*.yaml")) : "${path.module}/${file}"]
+  helm_charts {
+    name          = "reloader"
+    namespace     = "reloader"
+    repo          = "https://stakater.github.io/stakater-charts"
+    release_name  = "reloader"
+    version       = "1.2.0"
+    include_crds  = true
+    values_inline = yamlencode(local.helm_values)
+  }
+  kustomize_options {
+    load_restrictor = "none"
+    enable_helm     = true
+    helm_path       = var.paths.helm-path
+  }
+}
+resource "kubectl_manifest" "stage0" {
+  for_each   = local.ids-this-stage0
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = []
+
+  force_conflicts   = var.k8s_force
+  apply_only        = true
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage1" {
+  for_each   = local.ids-this-stage1
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage0]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage2" {
+  for_each   = local.ids-this-stage2
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage1]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
