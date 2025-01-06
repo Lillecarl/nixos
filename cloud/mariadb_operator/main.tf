5238d236f26309d2051ddc48abb425aa821807bd
Carl Andersson <git@lillecarl.com>
2025-01-06 10:11:21 +0100

    Deploy MariaDB operator

---
+++ cloud/mariadb_operator/main.tf
@@ -0,0 +1,62 @@
+variable "paths" { type = map(string) }
+variable "k8s_force" { type = bool }
+variable "deploy" { type = bool }
+locals {
+  ids-this-stage0 = data.kustomization_overlay.this.ids_prio[0]
+  ids-this-stage1 = var.deploy ? data.kustomization_overlay.this.ids_prio[1] : []
+  ids-this-stage2 = var.deploy ? data.kustomization_overlay.this.ids_prio[2] : []
+  helm_values = {
+    crds        = { enabled = true }
+    clusterName = "k8s.lillecarl.com"
+  }
+}
+data "kustomization_overlay" "this" {
+  resources = [for file in tolist(fileset(path.module, "*.yaml")) : "${path.module}/${file}"]
+  namespace = "mariadb-operator"
+  helm_charts {
+    name          = "mariadb-operator"
+    release_name  = "mariadb-operator"
+    namespace     = "mariadb-operator"
+    include_crds  = true
+    values_inline = yamlencode(local.helm_values)
+  }
+  helm_globals {
+    chart_home = var.paths.charts
+  }
+  kustomize_options {
+    load_restrictor = "none"
+    enable_helm     = true
+    helm_path       = var.paths.helm-path
+  }
+}
+resource "kubectl_manifest" "stage0" {
+  for_each   = local.ids-this-stage0
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = []
+
+  force_conflicts   = var.k8s_force
+  apply_only        = true
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage1" {
+  for_each   = local.ids-this-stage1
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage0]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
+resource "kubectl_manifest" "stage2" {
+  for_each   = local.ids-this-stage2
+  yaml_body  = data.kustomization_overlay.this.manifests[each.value]
+  depends_on = [kubectl_manifest.stage0]
+
+  force_conflicts   = var.k8s_force
+  server_side_apply = true
+  wait              = true
+  timeouts { create = "1m" }
+}
