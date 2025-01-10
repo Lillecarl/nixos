d944a2cbe27b00106d3cb0a75bc4562a9b05ac10
Carl Andersson <git@lillecarl.com>
2025-01-10 14:32:41 +0100

    nvim

--- users/lillecarl/terminal/neovim.nix
+++ users/lillecarl/terminal/neovim.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 let
   modName = "neovim";
   cfg = config.ps.${modName};
@@ -18,6 +23,48 @@ in
       viAlias = true;
       vimAlias = true;
       vimdiffAlias = true;
+
+      extraPackages = config.programs.helix.extraPackages ++ [
+        pkgs.nodejs # Copilot
+        pkgs.lua-language-server
+        pkgs.basedpyright
+      ];
+
+      plugins = with pkgs.vimPlugins; [
+        lze # Lazy loader that quacks a bit like lazy.nvim
+        multicursors-nvim # Feeling a bit heelixy?
+        which-key-nvim # Show keybindings
+        nvim-treesitter.withAllGrammars # Syntax highlighting for humans
+        catppuccin-nvim # Theme
+        mini-nvim # Collection of essential features
+        indent-blankline-nvim # Indent guides
+        nvim-web-devicons # Icons
+        conform-nvim # Formatting master
+        nvim-lspconfig # LSP configuration tool
+        copilot-lua # GH Copilot for Neovim
+        CopilotChat-nvim # GH Copilot chat for Neovim
+        plenary-nvim # curl, log, async
+        gitsigns-nvim # Git gutters
+        luasnip # Snippets
+        nvim-cmp # Completion
+        cmp-nvim-lsp
+        cmp-git
+        cmp-path
+        cmp-buffer
+        cmp-cmdline
+        cmp_luasnip
+      ];
+
+      extraLuaConfig =
+        let
+          luaConfig = {
+
+          };
+        in
+        # lua
+        ''
+          require("luainit").setup(${lib.generators.toLua { } luaConfig})
+        '';
     };
   };
 }
