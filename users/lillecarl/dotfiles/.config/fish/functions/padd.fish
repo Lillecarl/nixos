1eb15d20c07b2f91403437a70863f459a0ad7e57
Carl Andersson <git@lillecarl.com>
2025-01-24 00:17:29 +0100

    Improve padd
    Better error and success reporting
    Only evaluate package.out

--- users/lillecarl/dotfiles/.config/fish/functions/padd.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/padd.fish
@@ -1,9 +1,15 @@
 function padd
-    for path in $(nix build nixpkgs#$argv[1] --print-out-paths --no-link)
-        set bin "$path/bin"
-        if test -d $bin
-            echo "Added $bin to PATH"
-            set --prepend --global PATH $bin
+    for package in $argv
+        for path in $(nix build nixpkgs#$package.out --print-out-paths --no-link)
+            set bin "$path/bin"
+            if ! test -d $bin
+                echo "$bin doesn't exist"
+            else if contains $PATH $bin
+                echo "$bin is already on PATH"
+            else
+                echo "Added $bin to PATH"
+                set --prepend --global PATH $bin
+            end
         end
     end
 end
