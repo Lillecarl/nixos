93e7ffa3551d783522ccedd3e8b9a79dcc467875
Carl Andersson <git@lillecarl.com>
2025-03-02 20:50:49 +0100

    only do the WAYLAND_DISPLAY dance in SSH sessions

--- users/lillecarl/dotfiles/.config/fish/final/20-waydisplay.fish
+++ users/lillecarl/dotfiles/.config/fish/final/20-waydisplay.fish
@@ -1,4 +1,5 @@
 function waydisplay --on-event fish_preexec
+    set -q SSH_CLIENT || return 0 # Only set WAYLAND_DISPLAY in SSH connections
     set WAYDISPLAY (fd --base-directory $XDG_RUNTIME_DIR wayland)[1]
     if test -S "$XDG_RUNTIME_DIR/$WAYDISPLAY"
         set -gx WAYLAND_DISPLAY $WAYDISPLAY
