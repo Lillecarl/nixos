b7b070c350aeead83df42751d48bf8e955d5e966
Carl Andersson <git@lillecarl.com>
2025-03-24 22:01:29 +0100

    FRR configuration
    Configure BGP from shitbox to home router
    Configure RFC1918 filters
    Configure home filter
    Configure IPv4 redistribution

--- hosts/_shared/frr.nix
+++ hosts/_shared/frr.nix
@@ -22,25 +22,69 @@ in
       };
     };
   };
-  config = lib.mkIf cfg.enable {
-    services.frr = {
-      bgpd.enable = true;
-      config = ''
+  config =
+    let
+      routeMaps = ''
+        route-map home permit 10
+          match ip address prefix-list home
+        exit
+        route-map 1918 permit 10
+          match ip address prefix-list 1918
+        exit
+      '';
+      prefixLists = ''
+        ip prefix-list 1918 seq 10 permit 10.0.0.0/8      ge 8
+        ip prefix-list 1918 seq 20 permit 172.16.0.0/12   ge 12
+        ip prefix-list 1918 seq 30 permit 192.168.0.0/16  ge 16
+        ip prefix-list 1918 seq 40 deny   0.0.0.0/0       le 32
+
+        ip prefix-list home seq 10 permit 192.168.88.0/24 ge 24
+        ip prefix-list home seq 20 deny   0.0.0.0/0       le 32
+      '';
+      hostSpecific =
+        if config.networking.hostName == "shitbox" then
+          ''
+            neighbor 10.44.33.1 peer-group hub
+            neighbor 192.168.88.1 remote-as external
+            address-family ipv4 unicast
+              redistribute connected
+              neighbor spoke route-map home out
+              neighbor spoke route-map 1918 in
+
+              neighbor 192.168.88.1 route-map 1918 out
+              neighbor 192.168.88.1 route-map 1918 in
+            exit-address-family
+          ''
+        else if config.networking.hostName == "hetzner1" then
+          ''
+            bgp listen range 10.44.33.0/24 peer-group spoke
+            address-family ipv4 unicast
+              neighbor hub route-map 1918 out
+              neighbor hub route-map 1918 in
+            exit-address-family
+          ''
+        else
+          '''';
+      frrConfig = ''
         frr defaults datacenter
+
+        ${prefixLists}
+        ${routeMaps}
+
         router bgp ${toString cfg.ASN}
           neighbor spoke peer-group
           neighbor spoke remote-as external
           neighbor hub peer-group
           neighbor hub remote-as external
-          ${
-            if cfg.isHub then
-              "bgp listen range 10.44.33.0/24 peer-group spoke"
-            else
-              "neighbor 10.44.33.1 peer-group hub"
-          }
+          ${hostSpecific}
         exit
         end
       '';
+    in
+    lib.mkIf cfg.enable {
+      services.frr = {
+        bgpd.enable = true;
+        config = frrConfig;
+      };
     };
-  };
 }
