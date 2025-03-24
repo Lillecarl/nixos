01ae74517514f8f7603a634ff85db33d247cccaf
Carl Andersson <git@lillecarl.com>
2025-03-24 18:07:16 +0100

    Make `nix repl ...` run normal Nix command with args

--- users/lillecarl/dotfiles/.config/fish/functions/nix.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix.fish
@@ -2,10 +2,11 @@ function nix --wraps nix
     set command $argv[1]
     set args $argv
     set --erase args[1]
+    echo $args
 
     if test $command = build
         command nix build $args --no-link --print-out-paths --impure
-    else if test $command = repl
+    else if test $command = repl && test -z "$args"
         command nix repl --file $FLAKE
     else if test $command = run
         command nix run $args --impure
