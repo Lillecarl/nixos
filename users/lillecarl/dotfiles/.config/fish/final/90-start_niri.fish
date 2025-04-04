e5e992372892ddc5c6cfa984b153d9ffe2f0c05d
Carl Andersson <git@lillecarl.com>
2025-04-04 19:49:45 +0200

    Clean fc-cache before starting niri

--- users/lillecarl/dotfiles/.config/fish/final/90-start_niri.fish
+++ users/lillecarl/dotfiles/.config/fish/final/90-start_niri.fish
@@ -4,6 +4,7 @@ if status --is-login && test "$XDG_VTNR" = 1
     set -e __HM_SESS_VARS_SOURCED
     set -e __NIXOS_SET_ENVIRONMENT_DONE
 
+    fc-cache -rv # Clean fontcache
     env --unset=SHLVL niri --session >$NIRI_LOGFILE
     systemctl --user stop niri-session.target
     exec echo "niri session exited, goodbye"
