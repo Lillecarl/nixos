8047e0e51fee2f7a4c4d377b01fbf3664d3447e8
Carl Andersson <git@lillecarl.com>
2025-04-23 18:59:18 +0200

    Cleanup git and nix wrappers a bit

--- users/lillecarl/dotfiles/.config/fish/functions/git.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/git.fish
@@ -1,14 +1,14 @@
 function git --wraps=git
     set command $argv[1]
-    set args $argv
-    set --erase args[1]
+    set --erase argv[1]
 
     if test $command = push
         command git push $args
-        command git remote | rg -q forgejo && command git push forgejo $args
-    else if test $command = something
-        command git something $args
+
+        if command git remote | rg -q forgejo
+            command git push forgejo $args
+        end
     else
-        command git $argv
+        command git $command $argv
     end
 end
--- users/lillecarl/dotfiles/.config/fish/functions/nix.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix.fish
@@ -1,16 +1,15 @@
-function nix --wraps nix
+function nix
+    # Extract nix subcommand
     set command $argv[1]
-    set args $argv
-    set --erase args[1]
-    echo $args
+    set --erase argv[1]
 
     if test $command = build
-        command nix build $args --no-link --print-out-paths --impure
-    else if test $command = repl && test -z "$args"
+        command nix build $argv --no-link --print-out-paths --impure
+    else if test $command = repl && test -z "$argv"
         command nix repl --file $FLAKE
     else if test $command = run
-        command nix run $args --impure
+        command nix run $argv --impure
     else
-        command nix $argv
+        command nix $command $argv
     end
 end
