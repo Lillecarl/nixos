0cd25a6656976b10b9caf49b4dd637d91b5245f5
Carl Andersson <git@lillecarl.com>
2025-04-24 00:22:11 +0200

    nix wrapper: move --impure before own args

--- users/lillecarl/dotfiles/.config/fish/functions/nix.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix.fish
@@ -1,14 +1,14 @@
-function nix
+function nix --wraps nix
     # Extract nix subcommand
     set command $argv[1]
     set --erase argv[1]
 
     if test $command = build
-        command nix build $argv --no-link --print-out-paths --impure
+        command nix build --no-link --print-out-paths --impure $argv
     else if test $command = repl && test -z "$argv"
         command nix repl --file $FLAKE
     else if test $command = run
-        command nix run $argv --impure
+        command nix run --impure $argv
     else
         command nix $command $argv
     end
