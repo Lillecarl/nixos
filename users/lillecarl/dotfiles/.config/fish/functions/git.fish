696884903e0d5a16ce5d1c76e83b6f9edc19786a
Carl Andersson <git@lillecarl.com>
2025-05-07 21:04:59 +0200

    fix git push function logic

--- users/lillecarl/dotfiles/.config/fish/functions/git.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/git.fish
@@ -2,11 +2,11 @@ function git --wraps=git
     set command $argv[1]
     set --erase argv[1]
 
-    if test $command = push
-        command git push $args
+    if test $command = push && test -z "$argv"
+        command git push $argv
 
         if command git remote | rg -q forgejo
-            command git push forgejo $args
+            command git push forgejo $argv
         end
     else
         command git $command $argv
