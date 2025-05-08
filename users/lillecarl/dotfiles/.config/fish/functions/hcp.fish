6da0884734fc7c2374e2b2d9a165dba223af5bed
Carl Andersson <git@lillecarl.com>
2025-05-09 00:29:00 +0200

    fix hcp (history copy)

--- users/lillecarl/dotfiles/.config/fish/functions/hcp.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/hcp.fish
@@ -4,8 +4,7 @@ function hcp
     end
 
     set result (atuin search -i 3>&1 1>&2 2>&3 | string collect)
-    echo $result &>3
-    echo $result | wl-copy -n
+    echo $result | copy
 
     if is_kitty
         set post_height (kitty @ kitten get_cursor.py | jq '.cursor.height')
@@ -14,8 +13,6 @@ function hcp
 
     if is_kitty && test $drop -gt 0
         set drop (math $drop - 2)
-        # This scrolls the commandline to the bottom after an atuin search
-        echo "Dropping $drop" >&3
         echo -en '\e['$drop'+T'
         echo -en '\e['$drop'B'
     end
