8dc6555103926b58a4cf421e99036b5b9f5370bf
Carl Andersson <git@lillecarl.com>
2025-05-19 22:18:53 +0200

    Implement nix getrev

--- users/lillecarl/dotfiles/.config/fish/functions/nix.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix.fish
@@ -9,6 +9,12 @@ function nix --wraps nix
         command nix repl --file $FLAKE
     else if test $command = run
         command nix run --impure $argv
+    else if test $command = getrev
+        set input nixpkgs
+        if test -n "$argv[1]"
+            set input $argv[1]
+        end
+        command nix eval --expr "(import $FLAKE).inputs.$input.rev" --impure --raw
     else
         command nix $command $argv
     end
