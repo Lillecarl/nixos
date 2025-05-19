c9b1c78214189f72d0347398c877c77c9318996a
Carl Andersson <git@lillecarl.com>
2025-05-19 22:18:24 +0200

    libvirt: Define our binaries earlier

--- hosts/_shared/libvirt.nix
+++ hosts/_shared/libvirt.nix
@@ -74,9 +74,18 @@ in
           in
           {
             ${gamingvm.name} =
+              let
+                awk = lib.getExe pkgs.gawk;
+                fish = lib.getExe pkgs.fish;
+                renice = "${pkgs.util-linux}/bin/renice";
+                sponge = "${pkgs.moreutils}/bin/sponge";
+                sysctl = lib.getExe pkgs.sysctl;
+                systemctl = "${config.systemd.package}/bin/systemctl";
+                virsh = "${pkgs.libvirt}/bin/virsh";
+              in
               pkgs.writeScript "${gamingvm.name}-hook" # fish
                 ''
-                  #!${lib.getExe pkgs.fish}
+                  #!${fish}
                   # See https://libvirt.org/hooks.html#etc-libvirt-hooks-qemu for hook documentation
 
                   set vmname $argv[1] # vm name is passed as first argument
@@ -90,41 +99,45 @@ in
                   # Get the process id of the VM
                   set vmpid $(cat /var/run/libvirt/qemu/$vmname.pid)
                   # Get the process group id of the VM so we can renice all threads
-                  set vmpgid $(ps -o pgid --no-heading $vmpid | awk '{print $1}')
+                  set vmpgid $(ps -o pgid --no-heading $vmpid | ${awk} '{print $1}')
 
                   if test $event = "prepare"
                     # Prepare kernel memory for gaming VM hugepages
 
                     # Drop filesystem caches
-                    echo 3 > /proc/sys/vm/drop_caches
+                    echo 3 | ${sponge} /proc/sys/vm/drop_caches
+
                     # Compact memory to make hugepages available
-                    echo 1 > /proc/sys/vm/compact_memory
+                    echo 1 | ${sponge} /proc/sys/vm/compact_memory
 
                     # Allocate 2M hugepages for vm (count = VM MiB / 2)
-                    virsh allocpages 2M ${toString hugepagesWithOverhead} || begin
+                    ${virsh} allocpages 2M ${toString hugepagesWithOverhead} || begin
                       echo Failed to allocate hugepages for ${gamingvm.name}
                       exit 1
                     end
                   else if test $event = "started"
                     # Renice VM to -1
-                    renice -1 -g $vmpgid
-                    # Only schedule Linux stuff on CPU 5 and 11 (the 6th physical core)
-                    systemctl set-property --runtime -- init.scope AllowedCPUs=5,11
-                    systemctl set-property --runtime -- kubepods.scope AllowedCPUs=5,11
-                    systemctl set-property --runtime -- kubernetes.scope AllowedCPUs=5,11
-                    systemctl set-property --runtime -- system.slice AllowedCPUs=5,11
-                    systemctl set-property --runtime -- user.slice AllowedCPUs=5,11
-                  else if test $event = "release"
-                    # Release hugepages back to the system
+                    ${renice} -1 -g $vmpgid
 
-                    set reason $argv[4] # shutoff-reason is passed as fourth argument
-                    virsh allocpages 2M 0
+                    # Only schedule Linux stuff on CPU 5 and 11 (the 6th physical core)
+                    ${systemctl} set-property --runtime -- init.scope AllowedCPUs=5,11
+                    ${systemctl} set-property --runtime -- kubepods.scope AllowedCPUs=5,11
+                    ${systemctl} set-property --runtime -- kubernetes.scope AllowedCPUs=5,11
+                    ${systemctl} set-property --runtime -- system.slice AllowedCPUs=5,11
+                    ${systemctl} set-property --runtime -- user.slice AllowedCPUs=5,11
+                  else if test $event = "release"
+                    # shutoff-reason is passed as fourth argument
+                    set reason $argv[4]
+
+                    # Release hugepages back to the system
+                    ${virsh} allocpages 2M 0
+
                     # Reset scheduling back to use the entire CPU
-                    systemctl set-property --runtime -- init.scope AllowedCPUs=0-11
-                    systemctl set-property --runtime -- kubepods.scope AllowedCPUs=0-11
-                    systemctl set-property --runtime -- kubernetes.scope AllowedCPUs=0-11
-                    systemctl set-property --runtime -- system.slice AllowedCPUs=0-11
-                    systemctl set-property --runtime -- user.slice AllowedCPUs=0-11
+                    ${systemctl} set-property --runtime -- init.scope AllowedCPUs=0-11
+                    ${systemctl} set-property --runtime -- kubepods.scope AllowedCPUs=0-11
+                    ${systemctl} set-property --runtime -- kubernetes.scope AllowedCPUs=0-11
+                    ${systemctl} set-property --runtime -- system.slice AllowedCPUs=0-11
+                    ${systemctl} set-property --runtime -- user.slice AllowedCPUs=0-11
                   end
                 '';
           };
