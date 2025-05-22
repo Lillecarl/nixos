7e9e1cfdea63410a98e3fae15e447d97c918ed42
Carl Andersson <git@lillecarl.com>
2025-05-22 22:33:05 +0200

    fish/nix: set access-token with env variable

--- users/lillecarl/dotfiles/.config/fish/functions/nix.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/nix.fish
@@ -1,7 +1,10 @@
 function nix --wraps nix
     # Extract nix subcommand
     set command $argv[1]
+    set argvClone $argv
     set --erase argv[1]
+
+    set --export NIX_CONFIG "access-tokens = github.com=$(echo $PS_GHBASIC | base64 -d)"
 
     if test $command = build
         command nix build --no-link --print-out-paths --impure $argv
