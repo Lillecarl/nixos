b7c6094b49019e22b0aad4e2ffce816d541cd5d3
Carl Andersson <git@lillecarl.com>
2025-08-01 07:19:39 +0200

    Remove loads of programs I don't use

--- hosts/_shared/workstation.nix
+++ hosts/_shared/workstation.nix
@@ -51,29 +51,22 @@ in
       # Commandline tools (CLI)
       virtiofsd # VirtIO filesystem daemon
       home-manager # Tool to build your home environment in a reproducible fashion, anywhere with Nix!
-      rmtrash # rm compatible remove tool
       inetutils # Common internet utilities
       tcpdump # Network packet analyzer / cap tool
       nmap # Network scanner
       execline # Tools for dbus n stuff
-      handlr # xdg-open alternative
       asciinema # Terminal session recorder
-      wireguard-tools # Wireguard tools
       pmutils # Suspend tools
       inotify-tools # CLI tools for inotify in Linux
       inotify-info # inotify troubleshooter helper
       sshfs # Mount SFTP as filesystem
       rclone # rsync for clouds (+ loads of other cool things)
-      gnumake # make
       sipcalc # Subnet calculator
       dogdns # dig without poop
       whois # whois command
-      (lowPrio wireshark-cli) # Wireshark CLI
-      (lowPrio termshark) # Wireshark TUI?
       entr # Run commands when files change
       pv # Monitor pipe progress
       libnotify # Cli utils for sending KDE notifications
-      vim # Modal CLI text editor
       neovim # Modal CLI text editor, modern version of Vim
       lsof # Check who uses file
       wget # Fetch things quickly with HTTP
@@ -82,42 +75,28 @@ in
       curl # All things HTTP and other web transfer protocols
       tmux # terminal multiplexer
       htop # NCurses "task manager"
-      bottom # Task Manager written in Rust
-      powertop # See power information
       iotop # See disk IO information
       nixpkgs-fmt # Format Nix like the Nix project wants
       manix # Nix documentation search tool
-      nix-index # Files database for nixpkgs (find uninstalled commands)
       nix-tree # visualize the Nix store interactively
-      nix-update # Tool to help updating nix packages
-      nix-prefetch # nix prefetcher
-      nix-prefetch-github # fetch nix package from github
       pijul # Patch based git alternative (Similar to darcs but written in rust)
       gitui # Git TUI, rust
       wl-clipboard # Wayland clipboard manipulation
       iftop # Show interface throughput and which IP's
       smartmontools # Read SSD SMART information
-      glances # Fancy system monitoring TUI
       lm_sensors # Read sensors
       file # Show information about files
-      desktop-file-utils # Required for VS Code live share
       ncdu # NCurses Disk Utility (TUI way of finding big files and folders)
-      scrot # Commandline print-screen tool, use with GnuPG for automatic screenshotting
       pulseaudio # For pactl
       dmidecode # system information
       pciutils # PCI(e) utilities (lspci for example)
       usbutils # USB utils
       bridge-utils # Bridge utils
-      xdotool # Tools to automate mouse and keyboard in X
       wtype # Wayland version of xdotools
-      libsForQt5.qt5.qttools # qdbus command comes from here
       tree # Show things as a CLI tree in folders
-      neofetch # Tool for fancy print-screens
       bwm_ng # Check current network throughput by reading interface values
-      killall # Kill processes by name
       swtpm # needed to emulate TPM on QEMU
       gnupg # PGP implementation
-      nnn # file manager
       unipicker # unicode char finder
       waypipe # Wayland forwarding (Like X11 forwarding, but for Wayland)
       libinput # Libinput CLI tooling
@@ -128,7 +107,6 @@ in
       unzip # Decompress zip files
       unrar # Decompress rar files
       libressl # Crypto library, userspace tools
-      fsql # Query the filesystem with SQL
       pstree # Show process tree as a tree
       gist # Tool to post files to gist.github.com straight away
 
