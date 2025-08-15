4841db3154284e7ccdc366b83df3d18045913602
Carl Andersson <git@lillecarl.com>
2025-08-15 13:48:53 +0200

    Enable distrobox and podman

--- users/lillecarl/terminal/distrobox.nix
+++ users/lillecarl/terminal/distrobox.nix
@@ -18,6 +18,12 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    # Enable distrobox
+    programs.distrobox.enable = true;
+    # distrobox needs Podman
+    services.podman.enable = true;
+    # Create a link farm with common packages required to get anything
+    # in my $HOME going.
     xdg.configFile."distrobox_bin" = {
       source = pkgs.symlinkJoin {
         name = "distrobox_bin";
