bc275eb8847e103816a64d322af7c5e1d52b9295
Carl Andersson <git@lillecarl.com>
2025-08-15 15:51:38 +0200

    edit flake. -> edit flake.nix
    Common mildly irritating common typo, fixed with fish magic

--- users/lillecarl/dotfiles/.config/fish/functions/edit.fish
+++ users/lillecarl/dotfiles/.config/fish/functions/edit.fish
@@ -1,3 +1,8 @@
 function edit --wraps=$EDITOR
+    # I commonly tab flake because it's always accompanied by flake.lock
+    if test "$argv[1]" = "flake."
+        $EDITOR flake.nix
+        return 0
+    end
     $EDITOR $argv
 end
