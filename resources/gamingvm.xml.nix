7f147db53383374d5ec4529de44ecd7272886c22
Carl Andersson <git@lillecarl.com>
2025-08-23 16:26:59 +0200

    optimize gamingvm

--- resources/gamingvm.xml.nix
+++ resources/gamingvm.xml.nix
@@ -81,20 +81,20 @@ in
       <acpi/>
       <apic/>
       <hyperv mode='passthrough'/>
+      <ps2 state='off'/>
       <kvm>
         <hint-dedicated state='on'/>
         <poll-control state='on'/>
+        <pv-ipi state='on'/>
       </kvm>
       <vmport state='off'/>
-      <!-- Set this once libvirt 10.7.0 hits nixpkgs
-      <ps2 state='off'/>
-      -->
     </features>
     <clock offset='localtime'>
       <timer name='rtc' tickpolicy='catchup'/>
       <timer name='pit' tickpolicy='delay'/>
       <timer name='hpet' present='no'/>
       <timer name='hypervclock' present='yes'/>
+      <timer name='tsc' present='yes' mode='native'/>
     </clock>
     <on_poweroff>destroy</on_poweroff>
     <on_reboot>restart</on_reboot>
@@ -107,7 +107,6 @@ in
       <sound model='ich9'>
         <codec type='micro'/>
         <audio id='1'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x1b' function='0x0'/>
       </sound>
       <audio id='1' type='pipewire' runtimeDir='/run'>
         <input mixingEngine='no' streamName='vminput'/>
@@ -119,105 +118,51 @@ in
         <source dev='/dev/pool/winthin'/>
         <target dev='vda' bus='virtio'/>
         <boot order='1'/>
-        <address type='pci' domain='0x0000' bus='0x04' slot='0x00' function='0x0'/>
       </disk>
       <disk type='file' device='cdrom'>
         <driver name='qemu' type='raw'/>
         <target dev='sdb' bus='sata'/>
         <readonly/>
-        <address type='drive' controller='0' bus='0' target='0' unit='1'/>
       </disk>
       <controller type='usb' index='0' model='qemu-xhci' ports='15'>
-        <address type='pci' domain='0x0000' bus='0x02' slot='0x00' function='0x0'/>
       </controller>
       <controller type='pci' index='0' model='pcie-root'/>
       <controller type='pci' index='1' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='1' port='0x10'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0' multifunction='on'/>
       </controller>
       <controller type='pci' index='2' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='2' port='0x11'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x1'/>
       </controller>
       <controller type='pci' index='3' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='3' port='0x12'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x2'/>
       </controller>
       <controller type='pci' index='4' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='4' port='0x13'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x3'/>
       </controller>
       <controller type='pci' index='5' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='5' port='0x14'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x4'/>
       </controller>
       <controller type='pci' index='6' model='pcie-root-port'>
         <model name='pcie-root-port'/>
         <target chassis='6' port='0x15'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x5'/>
-      </controller>
-      <controller type='pci' index='7' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='7' port='0x16'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x6'/>
-      </controller>
-      <controller type='pci' index='8' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='8' port='0x17'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x7'/>
-      </controller>
-      <controller type='pci' index='9' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='9' port='0x18'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0' multifunction='on'/>
-      </controller>
-      <controller type='pci' index='10' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='10' port='0x19'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x1'/>
-      </controller>
-      <controller type='pci' index='11' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='11' port='0x1a'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x2'/>
-      </controller>
-      <controller type='pci' index='12' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='12' port='0x1b'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x3'/>
-      </controller>
-      <controller type='pci' index='13' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='13' port='0x1c'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x4'/>
-      </controller>
-      <controller type='pci' index='14' model='pcie-root-port'>
-        <model name='pcie-root-port'/>
-        <target chassis='14' port='0x1d'/>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x5'/>
       </controller>
       <controller type='sata' index='0'>
-        <address type='pci' domain='0x0000' bus='0x00' slot='0x1f' function='0x2'/>
       </controller>
       <filesystem type='mount' accessmode='passthrough'>
         <driver type='virtiofs'/>
         <source dir='/home/lillecarl'/>
         <target dir='/lillecarl'/>
-        <address type='pci' domain='0x0000' bus='0x03' slot='0x00' function='0x0'/>
       </filesystem>
       <interface type='bridge'>
         <mac address='02:00:00:12:34:57'/>
         <source bridge='br0'/>
         <model type='virtio'/>
-        <address type='pci' domain='0x0000' bus='0x01' slot='0x00' function='0x0'/>
       </interface>
-      <input type='mouse' bus='ps2'/>
-      <input type='keyboard' bus='ps2'/>
       <tpm model='tpm-crb'>
         <backend type='emulator' version='2.0'/>
       </tpm>
@@ -226,17 +171,16 @@ in
         <source>
           <address domain='0x0000' bus='0x09' slot='0x00' function='0x0'/>
         </source>
+        <address type='pci' domain='0x0000' bus='0x01' slot='0x00' function='0x0'/>
         <rom bar='on'/>
-        <address type='pci' domain='0x0000' bus='0x06' slot='0x00' function='0x0'/>
       </hostdev>
       <!-- PCI-E passthrough NVIDIA soundcard -->
       <hostdev mode='subsystem' type='pci' managed='yes'>
         <source>
           <address domain='0x0000' bus='0x09' slot='0x00' function='0x1'/>
         </source>
-        <address type='pci' domain='0x0000' bus='0x07' slot='0x00' function='0x0'/>
+        <address type='pci' domain='0x0000' bus='0x02' slot='0x00' function='0x0'/>
       </hostdev>
-      <watchdog model='itco' action='reset'/>
     </devices>
   </domain>
 ''
