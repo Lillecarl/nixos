462f0ff521433da71262e6d582a513e0b23368d4
Carl Andersson <git@lillecarl.com>
2025-09-11 13:28:41 +0200

    Remove the waydisplay experiment

--- users/lillecarl/dotfiles/.config/fish/final/20-waydisplay.fish
+++
@@ -1,7 +0,0 @@
-function waydisplay --on-event fish_preexec
-    set -q SSH_CLIENT || return 0 # Only set WAYLAND_DISPLAY in SSH connections
-    set WAYDISPLAY (fd --base-directory $XDG_RUNTIME_DIR wayland)[1]
-    if test -S "$XDG_RUNTIME_DIR/$WAYDISPLAY"
-        set -gx WAYLAND_DISPLAY $WAYDISPLAY
-    end
-end
