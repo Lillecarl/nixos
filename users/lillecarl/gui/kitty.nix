bc295cf15968d6070d02bf9ce2c343f8ec24d2d6
Carl Andersson <git@lillecarl.com>
2025-09-22 18:30:41 +0200

    Better kitty configuration

--- users/lillecarl/gui/kitty.nix
+++ users/lillecarl/gui/kitty.nix
@@ -4,45 +4,47 @@
     programs.kitty = {
       enable = true;
 
-      extraConfig = ''
-        include linked.conf
-        update_check_interval 0
-        confirm_os_window_close 0
-        scrollback_fill_enlarged_window yes
-        scrollback_pager nvimpager
-        notify_on_cmd_finish invisible 30
-        allow_remote_control yes
-        listen_on unix:@kitty
-        scrollback_lines 25000
-        tab_bar_align center
-        tab_bar_edge top
-        tab_bar_min_tabs 1
-        tab_bar_style separator
-        tab_separator " |"
-        tab_activity_symbol 🔔
-      '';
+      settings = {
+        # Include a git symlinked config for easier impure edits
+        include = "linked.conf";
+        # Borders
+        active_border_color = "#74c7ec";
+        inactive_border_color = "#585b70";
+        border_width = 1.0;
+        draw_minimal_borders = "no";
 
-      keybindings =
-        let
-          vimcmd = "vim -c 'set ft=man buftype=nofile' -c '$'-";
-        in
-        {
-          "alt+f1" = "launch --stdin-source=@last_cmd_output --type=overlay ${vimcmd}";
-          "alt+f2" = "launch --stdin-source=@screen --type=overlay ${vimcmd}";
-          "alt+f3" = "launch --stdin-source=@screen_scrollback --type=overlay ${vimcmd}";
-          "ctrl+tab" = "next_tab";
-          "ctrl+shift+tab" = "previous_tab";
-          "ctrl+shift+0" = "change_font_size all 0";
-          "ctrl+shift+1" = "change_font_size all -1.0";
-          "ctrl+shift+2" = "change_font_size all +1.0";
-          "ctrl+shift+3" = "no_op";
-          "ctrl+shift+4" = "no_op";
-          "ctrl+shift+5" = "no_op";
-          "ctrl+shift+6" = "no_op";
-          "ctrl+shift+7" = "no_op";
-          "ctrl+shift+8" = "no_op";
-          "ctrl+shift+9" = "no_op";
-        };
+        inactive_text_alpha = 0.7;
+
+        update_check_interval = 0;
+        confirm_os_window_close = 0;
+        scrollback_fill_enlarged_window = "yes";
+        scrollback_pager = "less";
+        notify_on_cmd_finish = "invisible 15 notify";
+        allow_remote_control = "yes";
+        listen_on = "unix:@kitty";
+        scrollback_lines = 25000;
+        tab_bar_align = "center";
+        tab_bar_edge = "top";
+        tab_bar_min_tabs = 2;
+        tab_bar_style = "separator";
+        tab_separator = " |";
+        tab_activity_symbol = "🔔";
+      };
+
+      keybindings = {
+        "ctrl+tab" = "next_tab";
+        "ctrl+shift+tab" = "previous_tab";
+        "ctrl+shift+0" = "change_font_size all 0";
+        "ctrl+shift+1" = "change_font_size all -1.0";
+        "ctrl+shift+2" = "change_font_size all +1.0";
+        "ctrl+shift+3" = "no_op";
+        "ctrl+shift+4" = "no_op";
+        "ctrl+shift+5" = "no_op";
+        "ctrl+shift+6" = "no_op";
+        "ctrl+shift+7" = "no_op";
+        "ctrl+shift+8" = "no_op";
+        "ctrl+shift+9" = "no_op";
+      };
     };
   };
 }
