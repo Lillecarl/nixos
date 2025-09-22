ae5411a95911715ad3c75ba11888d6cd01bd4ef8
Carl Andersson <git@lillecarl.com>
2025-09-22 18:28:44 +0200

    Use flake-compatish for default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,9 @@
 let
   # flake-compat = import /home/lillecarl/Code/flake-compat;
   flake-compat = import (builtins.fetchGit {
-    url = "https://git.lix.systems/lix-project/flake-compat.git";
+    url = "https://github.com/lillecarl/flake-compatish.git";
     ref = "main";
   });
-  flake = flake-compat {
-    src = (builtins.toString ./.);
-    copySourceTreeToStore = false;
-  };
+  flake = flake-compat ./.;
 in
-flake.outputs
+flake.outputs // { inherit (flake) impure; }
