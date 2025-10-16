6dc663aae5a01692b59ce143c423d4f8e5b7314c
Carl Andersson <git@lillecarl.com>
2025-10-16 11:07:14 +0200

    shitbox: disable fail2ban

--- hosts/shitbox/flake-module.nix
+++ hosts/shitbox/flake-module.nix
@@ -23,45 +23,45 @@ in
       in
       inputs.nixpkgs.lib.nixosSystem {
         inherit pkgs;
-        modules =
-          [
-            {
-              ps.frr.ASN = 65001;
-              ps.networking.enable = true;
-              ps.workstation.enable = true;
-              ps.libvirt.enable = true;
-              ps.kea.enable = true;
-              ps.btrfs.enable = true;
-              ps.guix.enable = true;
-              ps.kerneltune.enable = true;
-              ps.acme.enable = true;
-              ps.secrets.enable = true;
-              ps.k3s.enable = false;
-            }
-            inputs.agenix.nixosModules.default
-            inputs.disko.nixosModules.disko
-            inputs.niri.nixosModules.niri
-            inputs.nixos-hardware.nixosModules.common-cpu-amd
-            inputs.nixos-hardware.nixosModules.common-gpu-intel
-            inputs.nixos-hardware.nixosModules.common-pc
-            inputs.nixos-hardware.nixosModules.common-pc-ssd
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = specialArgs;
-            }
-            ./default.nix
-          ]
-          ++ pkgs.lib.rimport {
-            path = [
-              ../_shared
-            ];
-            regdel = [
-              __curPos.file
-              ".*disko\.nix"
-            ];
-          };
+        modules = [
+          {
+            ps.frr.ASN = 65001;
+            ps.networking.enable = true;
+            ps.workstation.enable = true;
+            ps.libvirt.enable = true;
+            ps.kea.enable = true;
+            ps.btrfs.enable = true;
+            ps.guix.enable = true;
+            ps.kerneltune.enable = true;
+            ps.acme.enable = true;
+            ps.secrets.enable = true;
+            ps.k3s.enable = false;
+            ps.fail2ban.enable = false;
+          }
+          inputs.agenix.nixosModules.default
+          inputs.disko.nixosModules.disko
+          inputs.niri.nixosModules.niri
+          inputs.nixos-hardware.nixosModules.common-cpu-amd
+          inputs.nixos-hardware.nixosModules.common-gpu-intel
+          inputs.nixos-hardware.nixosModules.common-pc
+          inputs.nixos-hardware.nixosModules.common-pc-ssd
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = specialArgs;
+          }
+          ./default.nix
+        ]
+        ++ pkgs.lib.rimport {
+          path = [
+            ../_shared
+          ];
+          regdel = [
+            __curPos.file
+            ".*disko\.nix"
+          ];
+        };
         inherit specialArgs;
       }
     );
